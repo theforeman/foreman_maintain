227d0591b11016347947854f0c9b0c50f3f81b76
Ivan Nečas <inecas@redhat.com>
2017-04-05 22:05:53 +0200

    Ignore some tasks that are known to not cause issues when paused

--- definitions/checks/foreman_tasks/not_paused.rb
+++ definitions/checks/foreman_tasks/not_paused.rb
@@ -7,12 +7,23 @@ module Checks::ForemanTasks
     end
 
     def run
-      paused_tasks_count = feature(:foreman_tasks).paused_tasks_count
+      paused_tasks_count = feature(:foreman_tasks).paused_tasks_count(ignored_tasks)
       assert(paused_tasks_count == 0,
              "There are currently #{paused_tasks_count} paused tasks in the system",
              :next_steps =>
                [Procedures::ForemanTasks::Resume.new,
-                Procedures::ForemanTasks::UiInvestigate.new('search_query' => 'state = paused')])
+                Procedures::ForemanTasks::UiInvestigate.new('search_query' => scoped_search_query)])
+    end
+
+    # Note: this is for UI link generation only: we are not using scoped search for querying
+    # the tasks itself as we use direct SQL instead
+    def scoped_search_query
+      "state = paused AND label !^(#{ignored_tasks.join(' ')})"
+    end
+
+    def ignored_tasks
+      %w[Actions::Candlepin::ListenOnCandlepinEvents
+         Actions::Katello::EventQueue::Monitor]
     end
   end
 end
