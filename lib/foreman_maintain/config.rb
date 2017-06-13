b8212e0a1fe4c7d9440792e39ccf643da674f652
Kavita Gaikwad <kavitagaikwad103@gmail.com>
2017-06-13 05:06:31 -0400

    Newly added config constants for packaging.

--- lib/foreman_maintain/config.rb
+++ lib/foreman_maintain/config.rb
@@ -2,18 +2,22 @@ require 'fileutils'
 module ForemanMaintain
   class Config
     attr_accessor :pre_setup_log_messages,
-                  :config_file, :definitions_dirs, :log_level, :log_dir, :storage_file
+                  :config_file, :definitions_dirs, :log_level, :log_dir, :storage_file,
+                  :backup_dir
 
     def initialize(options)
       @pre_setup_log_messages = []
-      @config_file = options.fetch(:config_file, default_config_file)
+      @config_file = options.fetch(:config_file, config_file_path)
       @options = load_config
       @definitions_dirs = @options.fetch(:definitions_dirs,
                                          [File.join(source_path, 'definitions')])
 
       @log_level = @options.fetch(:log_level, ::Logger::DEBUG)
-      @log_dir = find_log_dir_path(@options.fetch(:log_dir, 'log'))
+      @log_dir = find_dir_path(@options.fetch(:log_dir, 'log'))
       @storage_file = @options.fetch(:storage_file, 'data.yml')
+      @backup_dir = find_dir_path(
+        @options.fetch(:backup_dir, '/lib/foreman-maintain')
+      )
     end
 
     private
@@ -30,23 +34,23 @@ module ForemanMaintain
       raise "Couldn't load configuration file. Error: #{e.message}"
     end
 
-    def default_config_file
-      File.join(source_path, 'config/foreman_maintain.yml')
+    def config_file_path
+      File.exist?(CONFIG_FILE) ? CONFIG_FILE : 'config/foreman_maintain.yml'
     end
 
     def source_path
       File.expand_path('../../..', __FILE__)
     end
 
-    def find_log_dir_path(log_dir)
-      log_dir_path = File.expand_path(log_dir)
+    def find_dir_path(dir_path_str)
+      dir_path = File.expand_path(dir_path_str)
       begin
-        FileUtils.mkdir_p(log_dir_path, :mode => 0o750) unless File.exist?(log_dir_path)
+        FileUtils.mkdir_p(dir_path, :mode => 0o750) unless File.exist?(dir_path)
       rescue => e
-        $stderr.puts "No permissions to create log dir #{log_dir}"
+        $stderr.puts "No permissions to create dir #{dir_path_str}"
         $stderr.puts e.message.inspect
       end
-      log_dir_path
+      dir_path
     end
   end
 end
