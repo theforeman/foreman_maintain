ee07a21f93aa42a04c9506eed1541eb18288ebdb
Amit Upadhye <upadhyeammit@gmail.com>
2017-12-04 22:18:27 +0530

    Fixes #20156 - EPEL repository check

---
+++ definitions/checks/check_epel_repository.rb
@@ -0,0 +1,21 @@
+class Checks::CheckEpelRepository < ForemanMaintain::Check
+  metadata do
+    label :check_epel_repository
+    description 'Check if EPEL repository enabled on system'
+    tags :pre_upgrade
+    confine do
+      feature(:downstream)
+    end
+  end
+
+  def run
+    with_spinner('Checking for presence of EPEL repository') do
+      assert(!epel_enabled?, 'System is subscribed to EPEL repository')
+    end
+  end
+
+  def epel_enabled?
+    system_repos = execute("yum repolist enabled -d 6 -e 0| grep -E 'Repo-baseurl|Repo-id'")
+    system_repos.to_s.match(/\bepel\b/i)
+  end
+end
