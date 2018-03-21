e56255a2e7238b498e0f96c664cf860d612cf9d3
Sean O'Keeffe <seanokeeffe797@gmail.com>
2018-03-21 15:40:37 +0000

    Fixes #22976, #22977 - foreman-installer doesnt take --upgrade
    and katello no longers uses katello-installer

--- definitions/procedures/installer/upgrade.rb
+++ definitions/procedures/installer/upgrade.rb
@@ -1,16 +1,24 @@
 module Procedures::Installer
   class Upgrade < ForemanMaintain::Procedure
     def run
-      execute!("LANG=en_US.utf-8 #{installer_command} --upgrade", :interactive => true)
+      execute!("LANG=en_US.utf-8 #{installer_command} #{upgrade_command?}", :interactive => true)
     end
 
     private
 
+    def upgrade_command?
+      if package_version('satellite-installer') || package_version('katello-installer')
+        '--upgrade'
+      end
+    end
+
     def installer_command
-      if package_version('satellite-installer')
-        'satellite-installer'
-      elsif package_version('katello-installer')
-        'katello-installer'
+      if feature(:downstream)
+        if feature(:downstream).current_minor_version <= '6.1'
+          'katello-installer'
+        else
+          'satellite-installer'
+        end
       else
         'foreman-installer'
       end
