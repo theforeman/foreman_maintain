28654d6638f5cae1f7f95621364743253da4c1aa
kgaikwad <kavitagaikwad103@gmail.com>
2018-03-21 06:30:57 -0400

    Fixes #22945 - show failure if DB service stopped

--- definitions/procedures/candlepin/delete_orphaned_records_from_env_content.rb
+++ definitions/procedures/candlepin/delete_orphaned_records_from_env_content.rb
@@ -7,28 +7,35 @@ module Procedures::Candlepin
       confine do
         feature(:candlepin_database) &&
           feature(:downstream) &&
-          feature(:downstream).current_minor_version == '6.2' &&
-          feature(:candlepin_database).table_exist?('cp_env_content')
+          feature(:downstream).current_minor_version == '6.2'
       end
     end
 
     def run
-      with_spinner('Deleting cp_env_content record(s) with unresolvable content') do |spinner|
-        spinner.update 'Finding cp_env_content records with unresolvable content'
-        env_content_ids = feature(:candlepin_database).env_content_ids_with_null_content
-        if env_content_ids.empty?
-          spinner.update 'No orphaned records found'
-        else
-          spinner.update 'Taking a backup of the candlepin database'
-          feature(:candlepin_database).perform_backup
-
-          puts "Total #{env_content_ids.length} records with unresolvable content"
-          spinner.update 'Deleting record(s) from cp_env_content table'
-          feature(:candlepin_database).delete_records_by_ids(
-            'cp_env_content', env_content_ids
-          )
+      if feature(:candlepin_database).table_exist?('cp_env_content')
+        with_spinner('Deleting cp_env_content record(s) with unresolvable content') do |spinner|
+          spinner.update 'Finding cp_env_content records with unresolvable content'
+          env_content_ids = feature(:candlepin_database).env_content_ids_with_null_content
+          if env_content_ids.empty?
+            spinner.update 'No orphaned records found'
+          else
+            backup_and_delete_records(spinner, env_content_ids)
+          end
         end
       end
+    end
+
+    private
+
+    def backup_and_delete_records(spinner, env_content_ids)
+      spinner.update 'Taking a backup of the candlepin database'
+      feature(:candlepin_database).perform_backup
+
+      puts "Total #{env_content_ids.length} records with unresolvable content"
+      spinner.update 'Deleting record(s) from cp_env_content table'
+      feature(:candlepin_database).delete_records_by_ids(
+        'cp_env_content', env_content_ids
+      )
     end
   end
 end
