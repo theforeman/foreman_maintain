5e800fdecb8b6f359dea076b98a6393264ef7efd
Kavita Gaikwad <kavitagaikwad103@gmail.com>
2018-04-05 03:02:04 -0400

    Fixes #22771, #22772 - make generic configs for foreman_proxy

--- definitions/checks/foreman_proxy/verify_dhcp_config_syntax.rb
+++ definitions/checks/foreman_proxy/verify_dhcp_config_syntax.rb
@@ -1,17 +1,24 @@
 module Checks::ForemanProxy
   class VerifyDhcpConfigSyntax < ForemanMaintain::Check
     metadata do
-      for_feature :foreman_proxy
       description 'Check for verifying syntax for ISP DHCP configurations'
       tags :default
       confine do
-        file_exists?('/etc/dhcp/dhcpd.conf')
+        feature(:foreman_proxy)
       end
     end
 
     def run
-      success = feature(:foreman_proxy).valid_dhcp_configs?
-      assert(success, 'Please check and verify DHCP configurations.')
+      if feature(:foreman_proxy).features.include?('dhcp')
+        if feature(:foreman_proxy).dhcpd_conf_exist?
+          success = feature(:foreman_proxy).valid_dhcp_configs?
+          assert(success, 'Please check and verify DHCP configurations.')
+        else
+          fail! "Couldn't find configuration file at #{feature(:foreman_proxy).dhcpd_config_file}"
+        end
+      else
+        skip 'DHCP feature is not enabled'
+      end
     end
   end
 end
