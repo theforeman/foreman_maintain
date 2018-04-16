1adafb6ca5658a0798c4f4a3ae8981f1dae51dde
John Mitsch <jomitsch@redhat.com>
2018-04-16 14:04:06 +0000

    Fixes #23289 - Add puppet-server to feature check
    both 'puppetserver' and 'puppet-server' are puppet server
    packages depending on puppet 3/4

--- definitions/features/puppet_server.rb
+++ definitions/features/puppet_server.rb
@@ -2,6 +2,9 @@ class Features::PuppetServer < ForemanMaintain::Feature
   metadata do
     label :puppet_server
 
+    # We only check puppetserver and not puppet-server, as puppet-server
+    # is a part of httpd and relies on httpd service to restart, therefore
+    # not requiring a separate service to restart
     confine do
       find_package('puppetserver')
     end
