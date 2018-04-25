ba886a28b939e5b9823215b51f981a5a758119dc
Ivan Nečas <inecas@redhat.com>
2018-04-25 09:23:31 +0200

    Fixes #23388 - enable ansible repos for Sat 6.4

--- definitions/features/downstream.rb
+++ definitions/features/downstream.rb
@@ -64,11 +64,13 @@ class Features::Downstream < ForemanMaintain::Feature
       sat_repo_id = "rhel-server-#{rh_version_major}-satellite-6-beta-rpms"
     end
 
-    ["rhel-#{rh_version_major}-server-rpms",
-     "rhel-server-rhscl-#{rh_version_major}-rpms",
-     "rhel-#{rh_version_major}-server-satellite-maintenance-6-rpms",
-     sat_tools_repo_id,
-     sat_repo_id]
+    rh_repos = ["rhel-#{rh_version_major}-server-rpms",
+                "rhel-server-rhscl-#{rh_version_major}-rpms",
+                "rhel-#{rh_version_major}-server-satellite-maintenance-6-rpms",
+                sat_tools_repo_id,
+                sat_repo_id]
+    rh_repos << 'rhel-7-server-ansible-2-rpms' if sat_version.to_s == '6.4'
+    rh_repos
   end
 
   def version_from_source
