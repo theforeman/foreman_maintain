c530535f4e0a39d27daabb79185e9acc9fdf2e02
kgaikwad <kavitagaikwad103@gmail.com>
2018-06-11 09:53:42 -0400

    Fixes #23819 - upgrade guide for puppet4 if sat 6.3 & puppet3

--- definitions/procedures/knowledge_base_article.rb
+++ definitions/procedures/knowledge_base_article.rb
@@ -23,7 +23,8 @@ class Procedures::KnowledgeBaseArticle < ForemanMaintain::Procedure
 
   def kcs_documents
     {
-      'fix_cpdb_validate_failure' => 'https://access.redhat.com/solutions/3362821'
+      'fix_cpdb_validate_failure' => 'https://access.redhat.com/solutions/3362821',
+      'upgrade_puppet_guide_for_sat63' => 'https://access.redhat.com/documentation/en-us/red_hat_satellite/6.3/html/upgrading_and_updating_red_hat_satellite/upgrading_puppet-1'
     }
   end
 end
