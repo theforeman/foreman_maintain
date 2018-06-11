1e651891b1ccbff6167d7330c0f91a60e477c1d5
kgaikwad <kavitagaikwad103@gmail.com>
2018-06-11 04:36:35 -0400

    Fixes #23856 - on sat 6.3 & puppet4, enable  puppet4 repo
    (cherry picked from commit 1ce820360124ac8db5e62700946b6b615c739d59)

---
+++ definitions/features/puppet_server.rb
@@ -0,0 +1,20 @@
+class Features::PuppetServer < ForemanMaintain::Feature
+  metadata do
+    label :puppet_server
+
+    # We only check puppetserver and not puppet-server, as puppet-server
+    # is a part of httpd and relies on httpd service to restart, therefore
+    # not requiring a separate service to restart
+    confine do
+      find_package('puppetserver') || find_package('puppet')
+    end
+  end
+
+  def services
+    find_package('puppetserver') ? { 'puppetserver' => 30 } : {}
+  end
+
+  def puppet_version
+    version(execute!('puppet --version'))
+  end
+end
