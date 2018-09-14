43798835d1b07b0baf4c67fea1781376a1d4d9da
Martin Bačovský <mbacovsk@redhat.com>
2018-09-14 15:25:21 +0000

    Refs #24793 - fixed condition

--- lib/foreman_maintain/utils/service.rb
+++ lib/foreman_maintain/utils/service.rb
@@ -5,7 +5,7 @@ require 'foreman_maintain/utils/service/remote_db'
 module ForemanMaintain::Utils
   def self.system_service(name, priority, options = {})
     db_feature = options.fetch(:db_feature, nil)
-    if name =~ /^(postgresql|.*mongod)$/ && db_feature && db_feature.local? == false
+    if name =~ /^(postgresql|.*mongod)$/ && db_feature && !db_feature.local?
       Service::RemoteDB.new(name, priority, options)
     else
       Service::Systemd.new(name, priority, options)
