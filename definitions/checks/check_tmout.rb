f3772a45be95e767c431591a3449a72427c36832
Amit Upadhye <upadhyeammit@gmail.com>
2018-11-22 10:07:38 +0530

    Refs #23430 - added env and improved condition

--- definitions/checks/check_tmout.rb
+++ definitions/checks/check_tmout.rb
@@ -6,11 +6,15 @@ class Checks::CheckTmout < ForemanMaintain::Check
   end
 
   def run
-    assert(tmout_set?, 'The TMOUT environment variable is set on system.'\
+    assert(tmout_set?, "The TMOUT environment variable is set with value #{tmout_env}."\
           " Run 'unset TMOUT' command to unset this variable.")
   end
 
   def tmout_set?
-    execute('printenv TMOUT').empty?
+    tmout_env == '0' || tmout_env == '' || tmout_env.nil?
+  end
+
+  def tmout_env
+    ENV['TMOUT']
   end
 end
