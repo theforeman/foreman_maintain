196c9fe4ebc0302a37e85e92689895bf15cc3e5e
Martin Bačovský <martin.bacovsky@gmail.com>
2019-07-12 14:51:10 +0000

    Fixes #27291 - Make help tests whitespace insensitive
    New version of clamp (1.3.1) changed help formating which makes our
    output tests fail. assert_cmd was changed to optionaly ignore whitespace
    to make the tests pass regardless of what clamp version is used.
    
    (cherry picked from commit ac76f4c482e760761a3cff2d3be0a58ba8f1c752)

--- test/lib/cli_test.rb
+++ test/lib/cli_test.rb
@@ -11,7 +11,7 @@ module ForemanMaintain
     end
 
     it 'prints help' do
-      assert_cmd <<-OUTPUT.strip_heredoc
+      assert_cmd <<-OUTPUT.strip_heredoc, :ignore_whitespace => true
         Usage:
             foreman-maintain [OPTIONS] SUBCOMMAND [ARG] ...
 
