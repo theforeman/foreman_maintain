4804de4614e96b1747d9634d7b5c0bdc5dd8725e
Suraj Patil <patilsuraj767@gmail.com>
2019-10-14 12:26:21 +0000

    Fixes #26009 - fixed syntax

--- definitions/checks/foreman/verify_login_session_count.rb
+++ definitions/checks/foreman/verify_login_session_count.rb
@@ -1,28 +1,32 @@
-class Checks::VerifyLoginSessionCount < ForemanMaintain::Check
-  metadata do
-    label :foreman_login_session
-    tags :default
-    description 'Check the login session count'
-    for_feature :foreman_database
-  end
+module Checks
+  module Foreman
+    class Checks::VerifyLoginSessionCount < ForemanMaintain::Check
+      metadata do
+        label :foreman_login_session
+        tags :default
+        description 'Check the login session count'
+        for_feature :foreman_database
+      end
 
-  # The max id value that we can't go beyond is 2151782969.
-  # Therefore setting MAX_SESSION_ID = 2151782969/2
-  MAX_SESSION_ID = 1_075_891_484
+      # The max id value that we can't go beyond is 2151782969.
+      # Therefore setting MAX_SESSION_ID = 2151782969/2
+      MAX_SESSION_ID = 1_075_891_484
 
-  def run
-    login_session_count = count
-    assert(login_session_count <= MAX_SESSION_ID,
-           "Login session count reached to #{login_session_count} i.e greater than"\
-           " #{MAX_SESSION_ID} which will cause a problem while creating a new session."\
-           ' It needs to reset login session sequence',
-           :next_steps => Procedures::ResetLoginSessionIds.new)
-  end
+      def run
+        login_session_count = count
+        assert(login_session_count <= MAX_SESSION_ID,
+               "Login session count reached to #{login_session_count} i.e greater than"\
+               " #{MAX_SESSION_ID} which will cause a problem while creating a new session."\
+               ' It needs to reset login session sequence',
+               :next_steps => Procedures::ResetLoginSessionIds.new)
+      end
 
-  def count
-    sql = <<-SQL
-    SELECT last_value FROM sessions_id_seq
-    SQL
-    feature(:foreman_database).query(sql).first['last_value'].to_i
+      def count
+        sql = <<-SQL
+        SELECT last_value FROM sessions_id_seq
+        SQL
+        feature(:foreman_database).query(sql).first['last_value'].to_i
+      end
+    end
   end
 end
--- definitions/procedures/foreman/reset_login_session_id_sequence.rb
+++ definitions/procedures/foreman/reset_login_session_id_sequence.rb
@@ -1,21 +1,23 @@
-class Procedures::ResetLoginSessionIds < ForemanMaintain::Procedure
-  metadata do
-    description 'Reset Login Session Ids'
-    for_feature :foreman_database
-  end
-
-  def run
-    with_spinner('Reset the login session sequence:') do |spinner|
-      spinner.update 'Reseting sessions id sequence'
-      alter_sessions_id_seq
+module Procedures::Foreman
+  class Procedures::ResetLoginSessionIds < ForemanMaintain::Procedure
+    metadata do
+      description 'Reset Login Session Ids'
+      for_feature :foreman_database
     end
-  end
 
-  def alter_sessions_id_seq
-    feature(:foreman_database).psql(<<-SQL)
-    BEGIN;
-      ALTER SEQUENCE sessions_id_seq RESTART WITH 1;
-    COMMIT;
-    SQL
+    def run
+      with_spinner('Reset the login session sequence:') do |spinner|
+        spinner.update 'Reseting sessions id sequence'
+        alter_sessions_id_seq
+      end
+    end
+
+    def alter_sessions_id_seq
+      feature(:foreman_database).psql(<<-SQL)
+      BEGIN;
+        ALTER SEQUENCE sessions_id_seq RESTART WITH 1;
+      COMMIT;
+      SQL
+    end
   end
 end
