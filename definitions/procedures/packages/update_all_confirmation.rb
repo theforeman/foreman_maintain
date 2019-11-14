0b1b8ae6e17996f96839ca55f531d3c78dc0a779
Martin Bačovský <martin.bacovsky@gmail.com>
2019-11-14 09:12:54 +0000

    Refs #28097 - Disable --assumeyes

--- definitions/procedures/packages/update_all_confirmation.rb
+++ definitions/procedures/packages/update_all_confirmation.rb
@@ -8,14 +8,15 @@ module Procedures::Packages
 
     def run
       if @packages.nil? || @packages.empty?
-        question = "\nWARNING: No speciffic packages to update were provided\n" \
+        question = "\nWARNING: No specific packages to update were provided\n" \
           "so we are going to update all available packages.\n" \
           "It is recommended to update everything only as part of upgrade\n" \
           "of the #{feature(:instance).product_name} to the next version. \n" \
           "To Upgrade to next version use 'foreman-maintain upgrade'.\n\n" \
+          "NOTE: --assumeyes is not applicable for this check\n\n" \
           "Do you want to proceed with update of everything regardless\n" \
           'of the recommendations?'
-        answer = ask_decision(question, 'y(yes), q(quit)')
+        answer = ask_decision(question, 'y(yes), q(quit)', ignore_assumeyes: true)
         abort! unless answer == :yes
       end
     end
