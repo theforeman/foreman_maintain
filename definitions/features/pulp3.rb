9b7e76daed067fe82997ad7badac9e0457f60fd7
John Mitsch <jomitsch@redhat.com>
2019-12-05 01:52:16 +0000

    Refs #28193 - Skip pulpcore_workers on enable/disable

--- definitions/features/pulp3.rb
+++ definitions/features/pulp3.rb
@@ -14,7 +14,7 @@ class Features::Pulp3 < ForemanMaintain::Feature
       system_service('pulpcore-api', 10),
       system_service('pulpcore-content', 10),
       system_service('pulpcore-resource-manager', 10),
-      system_service('pulpcore-worker@*', 20, :all => true),
+      system_service('pulpcore-worker@*', 20, :all => true, :skip_enablement => true),
       system_service('redis', 30),
       system_service('httpd', 30)
     ]
