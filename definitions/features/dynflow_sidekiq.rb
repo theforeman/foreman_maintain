64146d77d1f2ea3921c40906f6dee29f7222c631
Adam Ruzicka <aruzicka@redhat.com>
2020-01-20 15:21:18 +0100

    Fixes #28329 - Make rubocop happy

--- definitions/features/dynflow_sidekiq.rb
+++ definitions/features/dynflow_sidekiq.rb
@@ -5,32 +5,35 @@ class Features::DynflowSidekiq < ForemanMaintain::Feature
     confine do
       server? && find_package('foreman-dynflow-sidekiq')
     end
+  end
 
-    def services
-      service_names.map { |service| system_service service, instance_priority(service) }
+  def services
+    service_names.map { |service| system_service service, instance_priority(service) }
+  end
+
+  def config_files
+    # Workaround until foreman-installer can deploy scaled workers
+    service_symlinks = configured_services.map do |service|
+      "/etc/systemd/system/multi-user.target.wants/#{service}.service"
     end
+    [
+      '/etc/foreman/dynflow',
+      service_symlinks
+    ].flatten
+  end
 
-    def config_files
-      [
-        '/etc/foreman/dynflow',
-        # Workaround until foreman-installer can deploy scaled workers
-        configured_services.map { |service| "/etc/systemd/system/multi-user.target.wants/#{service}.service" }
-      ].flatten
-    end
+  private
 
-    private
+  def instance_priority(instance)
+    # Orchestrator should be started before the workers are
+    instance.end_with?('@orchestrator') ? 30 : 31
+  end
 
-    def instance_priority(instance)
-      # Orchestrator should be started before the workers are
-      instance.end_with?('@orchestrator') ? 30 : 31
-    end
+  def service_names
+    configured_instances.map { |instance| "dynflow-sidekiq@#{instance}" }
+  end
 
-    def service_names
-      configured_instances.map { |instance| "dynflow-sidekiq@#{instance}" }
-    end
-
-    def configured_instances
-      Dir["/etc/foreman/dynflow/*"].map { |config| File.basename(config).gsub(/\.yml$/, '') }
-    end
+  def configured_instances
+    Dir['/etc/foreman/dynflow/*'].map { |config| File.basename(config).gsub(/\.yml$/, '') }
   end
 end
