1f24eb632fdc8620f7fc81129ae618a5ed1b48a9
Adam Ruzicka <aruzicka@redhat.com>
2020-01-21 09:25:54 +0100

    Fixes #28329 - Turn constants into methods
    This should allow us to modify them more easily when running on different
    platforms.

--- definitions/features/redis.rb
+++ definitions/features/redis.rb
@@ -1,22 +1,31 @@
 class Features::Redis < ForemanMaintain::Feature
-  SCL_PREFIX = 'rh-redis5'.freeze
-  SERVICE_NAME = "#{SCL_PREFIX}-redis".freeze
+  SCL_NAME = 'rh-redis5'.freeze
 
   metadata do
     label :redis
 
     confine do
       # Luckily, the service name is the same as the package providing it
-      server? && find_package(SERVICE_NAME)
+      server? && find_package(service_name)
     end
+  end
 
-    def services
-      [system_service(SERVICE_NAME, 10)]
-    end
+  def services
+    [system_service(service_name, 10)]
+  end
 
-    def config_files
-      ["/etc/opt/rh/#{SCL_PREFIX}/redis",
-       "/etc/opt/rh/#{SCL_PREFIX}/redis.conf"]
-    end
+  def config_files
+    ["/etc/opt/rh/#{SCL_NAME}/redis",
+     "/etc/opt/rh/#{SCL_NAME}/redis.conf"]
+  end
+
+  private
+
+  def scl_prefix
+    "#{SCL_NAME}-"
+  end
+
+  def service_name
+    "#{scl_prefix}redis"
   end
 end
