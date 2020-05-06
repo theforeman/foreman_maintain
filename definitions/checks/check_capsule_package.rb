15d1c501e59207fef224bf4c48852f93c42d8926
Suraj Patil <patilsuraj767@gmail.com>
2020-05-06 11:45:08 +0530

    Fixes #29579 - removed procedure remove_capsule_package.rb

--- definitions/checks/check_capsule_package.rb
+++ definitions/checks/check_capsule_package.rb
@@ -4,20 +4,15 @@ module Checks
       metadata do
         label :check_capsule_package
         description 'Check if satellite-capsule package installed in satellite server'
-        tags :pre_upgrade
+        tags :default
         confine do
           feature(:satellite) && feature(:capsule)
         end
       end
 
       def run
-        packages = find_package('satellite-capsule')
-        assert(
-          !packages,
-          'There are both satellite and satellite-capsule' \
-          ' packages installed in the system',
-          :next_steps => Procedures::Foreman::RemoveCapsulePackage.new
-        )
+        fail!('There are both satellite and satellite-capsule ' \
+              'packages installed in the system.')
       end
     end
   end
