0d59a37105f6b864fb94a7abbc11edf65eb138a7
Suraj Patil <patilsuraj767@gmail.com>
2020-05-20 17:04:21 +0530

    Fixes #29727 - Restricted check only for satellite

--- definitions/checks/foreman_proxy/check_tftp_storage.rb
+++ definitions/checks/foreman_proxy/check_tftp_storage.rb
@@ -5,46 +5,39 @@ module Checks::ForemanProxy
       description 'Clean old Kernel and initramfs files from tftp-boot'
       tags :default
       confine do
-        feature(:foreman_proxy)
+        feature(:satellite)
       end
     end
 
-    # rubocop:disable Metrics/MethodLength
     def run
       tftp_boot_dir = feature(:foreman_proxy).tftp_root_directory + '/boot/'
-      if feature(:foreman_proxy).features.include?('tftp')
-        if Dir.exist?(tftp_boot_dir)
-          if token_duration != 0
-            files = files_to_delete(tftp_boot_dir)
-            assert(files.empty?,
-                   'There are old initrd and vmlinuz files present in tftp',
-                   :next_steps => Procedures::Files::Remove.new(:files => files))
-          else
-            skip 'Provisioning token duration is set to 0'
-          end
-        else
-          fail! "TFTP root directory #{tftp_boot_dir} does not exist."
-        end
+      if !feature(:foreman_proxy).features.include?('tftp') || token_duration == 0
+        skip 'Skipping the check as TFTP feature is disabled or token duration is zero'
+      elsif Dir.exist?(tftp_boot_dir)
+        files = files_to_delete(tftp_boot_dir)
+        assert(files.empty?,
+               'There are old initrd and vmlinuz files present in tftp',
+               :next_steps => Procedures::Files::Remove.new(:files => files))
       else
-        skip 'TFTP feature is not enabled'
+        warn! "TFTP root directory #{tftp_boot_dir} does not exist."
       end
     end
-    # rubocop:enable Metrics/MethodLength
 
     def files_to_delete(tftp_boot_dir)
-      files = list_files_in_directory(tftp_boot_dir). \
-              map { |file| tftp_boot_dir + file }
-      files.map do |file|
-        file if File.mtime(file) + (token_duration * 60) < Time.now
+      list_files_in_directory(tftp_boot_dir).map do |file|
+        tftp_boot_dir + file if File.mtime(tftp_boot_dir + file) + (token_duration * 60) < Time.now
       end.compact
     end
 
     def token_duration
+      @token_duration ||= lookup_token_duration
+    end
+
+    def lookup_token_duration
       data = feature(:foreman_database). \
              query("select * from settings \
                     where category = 'Setting::Provisioning' and name = 'token_duration'")
-      param = data[0]['value'].nil? ? 'default' : 'value'
-      YAML.load(data[0][param])
+      YAML.load(data[0]['value'] || data[0]['default'])
     end
   end
 end
