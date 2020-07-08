2ad7cb175429eda06f2381fb51cf4bac62c404b2
Bernhard Suttner <sbernhard@users.noreply.github.com>
2020-07-08 09:26:52 +0200

    Fixes #30324 - check for http(s) proxy

---
+++ definitions/checks/env_proxy.rb
@@ -0,0 +1,13 @@
+class Checks::EnvProxy < ForemanMaintain::Check
+  metadata do
+    label :env_proxy
+    tags :env_proxy
+    description 'Check to make sure no HTTP(S) proxy set in ENV'
+  end
+
+  def run
+    variables = %w[http_proxy https_proxy HTTP_PROXY HTTPS_PROXY]
+    has_proxy_set = true if variables.map { |variable| ENV[variable] }.compact.any?
+    assert(has_proxy_set, 'Global HTTP(S) proxy in environment (env) is set. Please unset first!')
+  end
+end
