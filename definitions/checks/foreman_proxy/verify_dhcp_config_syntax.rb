6592c699020fba21d67fd00d22a886105953e9e2
Suraj Patil <31805557+patilsuraj767@users.noreply.github.com>
2020-07-29 16:17:07 +0530

    Fixes #30140 - check dhcp config syntax only for dhcp_isc provider
    (cherry picked from commit 244fc5ab7059dd5420dd6de0c4527483b004c6ec)

--- definitions/checks/foreman_proxy/verify_dhcp_config_syntax.rb
+++ definitions/checks/foreman_proxy/verify_dhcp_config_syntax.rb
@@ -4,20 +4,18 @@ module Checks::ForemanProxy
       description 'Check for verifying syntax for ISP DHCP configurations'
       tags :default
       confine do
-        feature(:foreman_proxy)
+        feature(:foreman_proxy) &&
+          feature(:foreman_proxy).features.include?('dhcp') &&
+          feature(:foreman_proxy).dhcp_isc_provider?
       end
     end
 
     def run
-      if feature(:foreman_proxy).features.include?('dhcp')
-        if feature(:foreman_proxy).dhcpd_conf_exist?
-          success = feature(:foreman_proxy).valid_dhcp_configs?
-          assert(success, 'Please check and verify DHCP configurations.')
-        else
-          fail! "Couldn't find configuration file at #{feature(:foreman_proxy).dhcpd_config_file}"
-        end
+      if feature(:foreman_proxy).dhcpd_conf_exist?
+        success = feature(:foreman_proxy).valid_dhcp_configs?
+        assert(success, 'Please check and verify DHCP configurations.')
       else
-        skip 'DHCP feature is not enabled'
+        fail! "Couldn't find configuration file at #{feature(:foreman_proxy).dhcpd_config_file}"
       end
     end
   end
