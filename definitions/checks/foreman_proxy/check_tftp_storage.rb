d2f0977b1fa616c776d2bfbabc59cca7cbddf4e1
Suraj Patil <31805557+patilsuraj767@users.noreply.github.com>
2020-08-19 13:39:10 +0530

    Fixes #30640 - fix tftp storage check
    (cherry picked from commit 40a60a68eb34bc5de57e7a8069fc55245ba4cd00)

--- definitions/checks/foreman_proxy/check_tftp_storage.rb
+++ definitions/checks/foreman_proxy/check_tftp_storage.rb
@@ -22,10 +22,9 @@ module Checks::ForemanProxy
     end
 
     def old_files_from_tftp_boot
-      Dir.entries(tftp_boot_directory).map do |file|
+      Dir.glob("#{tftp_boot_directory}*-{vmlinuz,initrd.img}").map do |file|
         unless File.directory?(file)
-          file_path =  tftp_boot_directory + file
-          file_path if File.mtime(file_path) + (token_duration * 60) < Time.now
+          file if File.mtime(file) + (token_duration * 60) < Time.now
         end
       end.compact
     end
