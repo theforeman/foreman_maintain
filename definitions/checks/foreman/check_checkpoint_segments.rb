2cae2c4f6572dfa02be8551242ed5b21961d3b20
Suraj Patil <patilsuraj767@gmail.com>
2020-09-02 16:04:21 +0530

    Refs #30729 - Removed postgresql config check

--- definitions/checks/foreman/check_checkpoint_segments.rb
+++ definitions/checks/foreman/check_checkpoint_segments.rb
@@ -10,47 +10,33 @@ module Checks
       end
 
       def run
-        files = []
-        files << check_postgres_config
-        files << check_custom_hiera
-        unless files.compact.empty?
-          failure_message = <<-MESSAGE.strip_heredoc
-          ERROR: Tuning option 'checkpoint_segments' found.
-          This option is no longer valid for PostgreSQL 9.5 and onwards.
-          Please remove it from following files and re-run the command.
-          #{files.join("\n")}
-          MESSAGE
-          fail! failure_message
-        end
+        failure_message = check_custom_hiera
+        fail! failure_message if failure_message
       end
 
+      # rubocop:disable Metrics/MethodLength
       def check_custom_hiera
         hiera_file = feature(:installer) ? feature(:installer).custom_hiera_file : nil
         return unless hiera_file
         if File.exist?(hiera_file) && (config = YAML.load_file(hiera_file)) &&
            config.key?('postgresql::server::config_entries')
-          if !config['postgresql::server::config_entries'].nil? &&
-             config['postgresql::server::config_entries'].key?('checkpoint_segments')
-            return hiera_file
+          if config['postgresql::server::config_entries'].nil?
+            return <<-MESSAGE.strip_heredoc
+            ERROR: 'postgresql::server::config_entries' cannot be null.
+            Please remove it from following file and re-run the command.
+            - #{hiera_file}
+            MESSAGE
+          elsif config['postgresql::server::config_entries'].key?('checkpoint_segments')
+            return <<-MESSAGE.strip_heredoc
+            ERROR: Tuning option 'checkpoint_segments' found.
+            This option is no longer valid for PostgreSQL 9.5 and onwards.
+            Please remove it from following file and re-run the command.
+            - #{hiera_file}
+            MESSAGE
           end
         end
       end
-
-      def check_postgres_config
-        param = /(?<!#)checkpoint_segments/
-        config_file = postgresql_config_file
-        if File.exist?(config_file) && (config = File.read(config_file))
-          return config_file if config.match(param)
-        end
-      end
-
-      def postgresql_config_file
-        if find_package('postgresql-server')
-          '/var/lib/pgsql/data/postgresql.conf'
-        else
-          '/var/opt/rh/rh-postgresql12/lib/pgsql/data/postgresql.conf'
-        end
-      end
+      # rubocop:enable Metrics/MethodLength
     end
   end
 end
