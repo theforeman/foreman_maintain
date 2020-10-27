6b19ba0347bc37690a695ee0c5cb4bb689df4292
Ian Ballou <ianballou67@gmail.com>
2020-10-27 15:47:15 +0000

    Fixes #31172 - content prepare on sat 6.9 must handle pulpcore services

--- definitions/features/pulpcore.rb
+++ definitions/features/pulpcore.rb
@@ -11,19 +11,35 @@ class Features::Pulpcore < ForemanMaintain::Feature
   end
 
   def services
-    [
-      system_service('rh-redis5-redis', 5),
-      system_service('pulpcore-api', 10),
-      system_service('pulpcore-content', 10),
-      system_service('pulpcore-resource-manager', 10),
+    pulpcore_common_services + [
       system_service('pulpcore-worker@*', 20, :all => true, :skip_enablement => true),
       system_service('httpd', 30)
+    ]
+  end
+
+  def pulpcore_migration_services
+    pulpcore_common_services + [
+      system_service('pulpcore-worker@1', 20),
+      system_service('pulpcore-worker@2', 20),
+      system_service('pulpcore-worker@3', 20),
+      system_service('pulpcore-worker@4', 20)
     ]
   end
 
   def config_files
     [
       '/etc/pulp/settings.py'
+    ]
+  end
+
+  private
+
+  def pulpcore_common_services
+    [
+      system_service('rh-redis5-redis', 5),
+      system_service('pulpcore-api', 10),
+      system_service('pulpcore-content', 10),
+      system_service('pulpcore-resource-manager', 10)
     ]
   end
 end
--- definitions/scenarios/content.rb
+++ definitions/scenarios/content.rb
@@ -10,7 +10,30 @@ module ForemanMaintain::Scenarios
       def compose
         # FIXME: remove this condition on next downstream upgrade scenario
         if Procedures::Content::Prepare.present?
+          enable_and_start_services
           add_step(Procedures::Content::Prepare)
+          disable_and_stop_services
+        end
+      end
+
+      private
+
+      def enable_and_start_services
+        add_step(Procedures::Service::Start)
+        if feature(:satellite) && feature(:satellite).at_least_version?('6.9')
+          add_step(Procedures::Service::Enable.
+                   new(:only => feature(:pulpcore).pulpcore_migration_services))
+          add_step(Procedures::Service::Start.
+                   new(:only => feature(:pulpcore).pulpcore_migration_services))
+        end
+      end
+
+      def disable_and_stop_services
+        if feature(:satellite) && feature(:satellite).current_minor_version == '6.9'
+          add_step(Procedures::Service::Stop.
+                   new(:only => feature(:pulpcore).pulpcore_migration_services))
+          add_step(Procedures::Service::Disable.
+                   new(:only => feature(:pulpcore).pulpcore_migration_services))
         end
       end
     end
