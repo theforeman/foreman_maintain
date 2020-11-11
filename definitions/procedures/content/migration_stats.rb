ce8a54bf20e9147bc0d2277637de02dc237e199c
Ian Ballou <ianballou67@gmail.com>
2020-11-11 16:50:17 +0000

    Fixes #31194 - update confines for content prepare related commands

--- definitions/procedures/content/migration_stats.rb
+++ definitions/procedures/content/migration_stats.rb
@@ -3,6 +3,11 @@ module Procedures::Content
     metadata do
       description 'Retrieve Pulp 2 to Pulp 3 migration statistics'
       for_feature :pulpcore
+
+      confine do
+        # FIXME: remove this condition on next downstream upgrade scenario
+        !feature(:satellite) || feature(:satellite).at_least_version?('6.9')
+      end
     end
 
     def run
