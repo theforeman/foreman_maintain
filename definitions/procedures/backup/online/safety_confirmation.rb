64d27015c238f16f1a15e51e8a2776d15903f821
Amit Upadhye <upadhyeammit@gmail.com>
2020-11-18 17:21:27 +0530

    Fixes #31328 - fix online backup warning for include-db-dumps

--- definitions/procedures/backup/online/safety_confirmation.rb
+++ definitions/procedures/backup/online/safety_confirmation.rb
@@ -4,21 +4,25 @@ module Procedures::Backup
       metadata do
         description 'Data consistency warning'
         tags :backup
+        param :include_db_dumps
       end
 
       def run
-        answer = ask_decision(
-          "*** WARNING: The online backup is intended for making a copy of the data\n" \
+        answer = ask_decision(warning_message(@include_db_dumps), 'y(yes), q(quit)')
+        abort! unless answer == :yes
+      end
+
+      def warning_message(include_db_dumps)
+        substr = include_db_dumps ? 'database dump' : 'online backup'
+        "*** WARNING: The #{substr} is intended for making a copy of the data\n" \
           '*** for debugging purposes only.' \
           " The backup routine can not ensure 100% consistency while the\n" \
           "*** backup is taking place as there is a chance there may be data mismatch between\n" \
           '*** Mongo and Postgres databases while the services are live.' \
-          " If you wish to utilize the online backup\n" \
+          " If you wish to utilize the #{substr}\n" \
           '*** for production use you need to ensure that there are' \
           " no modifications occurring during\n" \
-          "*** your backup run.\n\nDo you want to proceed?", actions_msg: 'y(yes), q(quit)'
-        )
-        abort! unless answer == :yes
+          "*** your backup run.\n\nDo you want to proceed?"
       end
     end
   end
