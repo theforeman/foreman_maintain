3695a25611dbfc5f01df665537e52109370ee18d
Ewoud Kohl van Wijngaarden <ewoud@kohlvanwijngaarden.nl>
2021-04-30 17:23:03 +0200

    Refs #32491 - Match description to the actual command

--- definitions/procedures/prep_6_10_upgrade.rb
+++ definitions/procedures/prep_6_10_upgrade.rb
@@ -11,7 +11,7 @@ class Procedures::Prep610Upgrade < ForemanMaintain::Procedure
   def run
     puts time_warning
     with_spinner('Updating filesystem permissions for Pulp 3') do |spinner|
-      spinner.update('# chmod -R g+rwX /var/lib/pulp/content')
+      spinner.update('# chmod -R g=rwX /var/lib/pulp/content')
       FileUtils.chmod_R 'g=rwX', '/var/lib/pulp/content'
       spinner.update("# find /var/lib/pulp/content -type d -perm -g-s -exec chmod g+s {} \;")
       execute!('find /var/lib/pulp/content -type d -perm -g-s -exec chmod g+s {} \;')
