theforeman/foreman_maintain

commit 81f60e1508f940574d3d59c8b9ab0e87c00bd6d2

Fixes #33271 - check DHCP and DNS interfaces before the restore

lib/foreman_maintain/utils/backup.rb
@@ -287,6 +287,26 @@ module ForemanMaintain
         end
       end
 
+      def validate_interfaces
+        # I wanted to do `Socket.getifaddrs.map(&:name).uniq`,
+        # but this has to work with Ruby 2.0, and Socket.getifaddrs is 2.1+
+        errors = {}
+        system_interfaces = Dir.children('/sys/class/net')
+
+        proxy_config = metadata.fetch('proxy_config', {})
+
+        %w[dns dhcp].each do |feature|
+          next unless proxy_config.fetch(feature, false)
+
+          wanted_interface = proxy_config.fetch("#{feature}_interface", 'lo')
+          unless system_interfaces.include?(wanted_interface)
+            errors[feature] = { 'configured' => wanted_interface, 'available' => system_interfaces }
+          end
+        end
+
+        return errors
+      end
+
       def metadata
         if file_map[:metadata][:present]
           YAML.load_file(file_map[:metadata][:path])

test/lib/utils/backup_test.rb
@@ -336,5 +336,46 @@ module ForemanMaintain
       kat_stand_backup.stubs(:hostname).returns('sat-6.example.com')
       assert kat_stand_backup.validate_hostname?
     end
+
+    it 'accepts backup without proxy config in the metadata' do
+      backup = subject.new(katello_standard_pulp2)
+      Dir.stubs(:children).returns(%w[lo eth0])
+      assert backup.validate_interfaces.empty?
+    end
+
+    it 'accepts backup with proxy config and disabled DHCP/DNS in the metadata' do
+      backup = subject.new(katello_standard_pulp2)
+      Dir.stubs(:children).returns(%w[lo eth0])
+      backup.stubs(:metadata).returns('proxy_config' =>
+                                      { 'dhcp' => false,
+                                        'dhcp_interface' => 'eth0',
+                                        'dns' => false,
+                                        'dns_interface' => 'eth0' })
+      assert backup.validate_interfaces.empty?
+    end
+
+    it 'accepts backup when DHCP/DNS configured interfaces are found on system' do
+      backup = subject.new(katello_standard_pulp2)
+      Dir.stubs(:children).returns(%w[lo eth0])
+      backup.stubs(:metadata).returns('proxy_config' =>
+                                      { 'dhcp' => true,
+                                        'dhcp_interface' => 'eth0',
+                                        'dns' => true,
+                                        'dns_interface' => 'eth0' })
+      assert backup.validate_interfaces.empty?
+    end
+
+    it 'rejects backup when DHCP/DNS configured interfaces are not found on system' do
+      backup = subject.new(katello_standard_pulp2)
+      Dir.stubs(:children).returns(%w[lo eth1])
+      backup.stubs(:metadata).returns('proxy_config' =>
+                                      { 'dhcp' => true,
+                                        'dhcp_interface' => 'eth0',
+                                        'dns' => true,
+                                        'dns_interface' => 'eth0' })
+      refute backup.validate_interfaces.empty?
+      assert backup.validate_interfaces['dns']['configured'] = 'eth0'
+      assert backup.validate_interfaces['dhcp']['configured'] = 'eth0'
+    end
   end
 end

definitions/checks/restore/validate_interfaces.rb
@@ -1,0 +1,24 @@
+require 'foreman_maintain/utils/backup'
+
+module Checks::Restore
+  class ValidateInterfaces < ForemanMaintain::Check
+    metadata do
+      description 'Validate network interfaces match the backup'
+
+      param :backup_dir,
+            'Path to backup directory',
+            :required => true
+      manual_detection
+    end
+
+    def run
+      backup = ForemanMaintain::Utils::Backup.new(@backup_dir)
+      invalid_interfaces = backup.validate_interfaces
+      msg = 'The following features are enabled in the backup, '\
+        "\nbut the system does not have the interfaces used by these features: "
+      msg << invalid_interfaces.map { |k, v| "#{k} (#{v['configured']})" }.join(', ')
+      msg << '.'
+      assert(backup.validate_interfaces.empty?, msg)
+    end
+  end
+end

definitions/scenarios/restore.rb
@@ -18,6 +18,7 @@ module ForemanMaintain::Scenarios
       add_steps_with_context(Checks::Restore::ValidateBackup,
                              Procedures::Restore::Confirmation,
                              Checks::Restore::ValidateHostname,
+                             Checks::Restore::ValidateInterfaces,
                              Procedures::Selinux::SetFileSecurity,
                              Procedures::Restore::Configs)
       add_step_with_context(Procedures::Crond::Stop) if feature(:cron)
@@ -87,6 +88,7 @@ module ForemanMaintain::Scenarios
       context.map(:backup_dir,
                   Checks::Restore::ValidateBackup => :backup_dir,
                   Checks::Restore::ValidateHostname => :backup_dir,
+                  Checks::Restore::ValidateInterfaces => :backup_dir,
                   Procedures::Restore::Configs => :backup_dir,
                   Procedures::Restore::DropDatabases => :backup_dir,
                   Procedures::Restore::PgGlobalObjects => :backup_dir,

test/definitions/checks/restore/validate_interfaces_test.rb
@@ -1,0 +1,25 @@
+require 'test_helper'
+
+describe Checks::Restore::ValidateInterfaces do
+  include DefinitionsTestHelper
+
+  subject do
+    Checks::Restore::ValidateInterfaces.new(:backup_dir => '.')
+  end
+
+  it 'passes when no invalid interfaces found' do
+    ForemanMaintain::Utils::Backup.any_instance.stubs(:validate_interfaces).returns({})
+    result = run_check(subject)
+    assert result.success?, 'Check expected to succeed'
+  end
+
+  it 'fails when invalid interfaces found' do
+    invalid = { 'dhcp' => { 'configured' => 'eth0' } }
+    ForemanMaintain::Utils::Backup.any_instance.stubs(:validate_interfaces).returns(invalid)
+    result = run_check(subject)
+    refute result.success?, 'Check expected to fail'
+    expected = 'The following features are enabled in the backup, '\
+      "\nbut the system does not have the interfaces used by these features: dhcp (eth0)."
+    assert_equal result.output, expected
+  end
+end

commit 2a040a78108bc546d79b7c887de6bf8005c3afe9

Refs #33271 - Use Dir.entries instead of Dir.children
Dir.children is new in Ruby 2.5 :(

Fixes: 81f60e1508f940574d3d59c8b9ab0e87c00bd6d2

lib/foreman_maintain/utils/backup.rb
@@ -291,7 +291,7 @@ module ForemanMaintain
         # I wanted to do `Socket.getifaddrs.map(&:name).uniq`,
         # but this has to work with Ruby 2.0, and Socket.getifaddrs is 2.1+
         errors = {}
-        system_interfaces = Dir.children('/sys/class/net')
+        system_interfaces = Dir.entries('/sys/class/net') - ['.', '..']
 
         proxy_config = metadata.fetch('proxy_config', {})
 

test/lib/utils/backup_test.rb
@@ -339,13 +339,13 @@ module ForemanMaintain
 
     it 'accepts backup without proxy config in the metadata' do
       backup = subject.new(katello_standard_pulp2)
-      Dir.stubs(:children).returns(%w[lo eth0])
+      Dir.stubs(:entries).returns(%w[. .. lo eth0])
       assert backup.validate_interfaces.empty?
     end
 
     it 'accepts backup with proxy config and disabled DHCP/DNS in the metadata' do
       backup = subject.new(katello_standard_pulp2)
-      Dir.stubs(:children).returns(%w[lo eth0])
+      Dir.stubs(:entries).returns(%w[. .. lo eth0])
       backup.stubs(:metadata).returns('proxy_config' =>
                                       { 'dhcp' => false,
                                         'dhcp_interface' => 'eth0',
@@ -356,7 +356,7 @@ module ForemanMaintain
 
     it 'accepts backup when DHCP/DNS configured interfaces are found on system' do
       backup = subject.new(katello_standard_pulp2)
-      Dir.stubs(:children).returns(%w[lo eth0])
+      Dir.stubs(:entries).returns(%w[. .. lo eth0])
       backup.stubs(:metadata).returns('proxy_config' =>
                                       { 'dhcp' => true,
                                         'dhcp_interface' => 'eth0',
@@ -367,7 +367,7 @@ module ForemanMaintain
 
     it 'rejects backup when DHCP/DNS configured interfaces are not found on system' do
       backup = subject.new(katello_standard_pulp2)
-      Dir.stubs(:children).returns(%w[lo eth1])
+      Dir.stubs(:entries).returns(%w[. .. lo eth1])
       backup.stubs(:metadata).returns('proxy_config' =>
                                       { 'dhcp' => true,
                                         'dhcp_interface' => 'eth0',