53a7460ef3d087d2ad132c0fe54c48058db70f8e
Amit Upadhye <upadhyeammit@gmail.com>
2021-12-02 17:57:31 +0530

    Refs #34066 - remove pulp2 from pulp_common concern

--- lib/foreman_maintain/concerns/pulp_common.rb
+++ lib/foreman_maintain/concerns/pulp_common.rb
@@ -6,16 +6,10 @@ module ForemanMaintain
       end
 
       def exclude_from_backup
-        # For pulp2:
-        # Exclude /var/lib/pulp/katello-export and /var/lib/pulp/cache
-        # since the tar is run from /var/lib/pulp, list subdir paths only
-        # For pulp3/pulpcore:
-        pulp2_dirs = %w[katello-export cache]
         # For pulp3/pulpcore:
         # Only need to backup media directory of /var/lib/pulp
         # All below directories and their contents are regenerated on installer run
-        pulpcore_dirs = %w[assets exports imports sync_imports tmp]
-        pulp2_dirs + pulpcore_dirs
+        %w[assets exports imports sync_imports tmp]
       end
     end
   end
