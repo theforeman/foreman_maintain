d1d480792367be31e3b4a35a4178556a28bbe3fb
Amit Upadhye <upadhyeammit@gmail.com>
2022-03-01 17:04:12 +0530

    Refs 34282 - ask user for nftables/iptables installation

--- definitions/procedures/maintenance_mode/enable_maintenance_mode.rb
+++ definitions/procedures/maintenance_mode/enable_maintenance_mode.rb
@@ -11,8 +11,38 @@ module Procedures::MaintenanceMode
       if feature(:instance).firewall
         feature(:instance).firewall.enable_maintenance_mode
       else
-        warn! 'Unable to find iptables or nftables!'
+        notify_and_ask_to_install_firewall_utility
       end
+    end
+
+    def notify_and_ask_to_install_firewall_utility
+      puts 'Unable to find nftables or iptables!'
+      question, pkg = question_and_pkg_name
+      answer = ask_decision(question, actions_msg: 'y(yes), q(quit)')
+      if answer == :yes
+        packages_action(:install, pkg)
+        feature(:instance).firewall.enable_maintenance_mode
+      end
+    end
+
+    def can_install_nft?
+      nft_kernel_version = Gem::Version.new('3.13')
+      installed_kernel_version = Gem::Version.new(execute!('uname -r').split('-').first)
+      installed_kernel_version >= nft_kernel_version
+    end
+
+    def question_and_pkg_name
+      question = 'Do you want to install missing netfilter utility '
+      pkg_to_install = []
+      if can_install_nft?
+        question << 'nftables?'
+        pkg_to_install << 'nftables'
+      else
+        question << 'iptables?'
+        pkg_to_install << 'iptables'
+      end
+      question << "\nand start maintenance mode?"
+      [question, pkg_to_install]
     end
   end
 end
