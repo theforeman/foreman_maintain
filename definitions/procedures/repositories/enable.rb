b519226f912051a2debae4db90fe06118af49027
Amit Upadhye <upadhyeammit@gmail.com>
2022-03-07 22:34:36 +0530

    Refs #33620 - use RHSM to enable RHSM repositories

--- definitions/procedures/repositories/enable.rb
+++ definitions/procedures/repositories/enable.rb
@@ -2,11 +2,17 @@ module Procedures::Repositories
   class Enable < ForemanMaintain::Procedure
     metadata do
       param :repos, 'Array of repositories to enable'
+      param :use_rhsm, 'Use RHSM to enable repository',
+            :flag => true, :default => false
       description 'Enable repositories'
     end
     def run
       with_spinner('Enabling repositories') do
-        repository_manager.enable_repos(@repos)
+        if @use_rhsm
+          repository_manager.rhsm_enable_repos(@repos)
+        else
+          repository_manager.enable_repos(@repos)
+        end
       end
     end
   end
