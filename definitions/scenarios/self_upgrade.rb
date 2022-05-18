835b5f6fd4773dd0278b3e7996840a24f8d33760
Amit Upadhye <upadhyeammit@gmail.com>
2022-05-18 15:55:10 +0530

    Refs #34937 - use yum cmd options to enable and update packages

--- definitions/scenarios/self_upgrade.rb
+++ definitions/scenarios/self_upgrade.rb
@@ -1,18 +1,6 @@
 module ForemanMaintain::Scenarios
   class SelfUpgradeBase < ForemanMaintain::Scenario
     include ForemanMaintain::Concerns::Downstream
-    def enabled_system_repos_id
-      repository_manager.enabled_repos.keys
-    end
-
-    def enable_repos(repo_ids = stored_enabled_repos_ids)
-      add_step(Procedures::Repositories::Enable.new(repos: repo_ids))
-    end
-
-    def disable_repos(repo_ids = stored_enabled_repos_ids)
-      add_step(Procedures::Repositories::Disable.new(repos: repo_ids))
-    end
-
     def target_version
       current_full_version = feature(:instance).downstream.current_version
       @target_version ||= current_full_version.bump
@@ -42,36 +30,6 @@ module ForemanMaintain::Scenarios
       else
         "satellite-maintenance-#{version}-for-rhel-#{el_major_version}-x86_64-rpms"
       end
-    end
-
-    def maintenance_repo_version
-      return '6' if current_version == '6.10'
-
-      current_version
-    end
-
-    def stored_enabled_repos_ids
-      @stored_enabled_repos_ids ||= begin
-        path = File.expand_path('enabled_repos.yml', ForemanMaintain.config.backup_dir)
-        @stored_enabled_repos_ids = File.file?(path) ? YAML.load(File.read(path)) : []
-      end
-    end
-
-    def all_maintenance_repos
-      repo_regex = if el7?
-                     /rhel-\d-server-satellite-maintenance-\d.\d-rpms/
-                   else
-                     /satellite-maintenance-\d.\d-for-rhel-\d-x86_64-rpms/
-                   end
-      stored_enabled_repos_ids.select { |id| !id.match(repo_regex).nil? }
-    end
-
-    def repos_ids_to_reenable
-      repos_ids_to_reenable = stored_enabled_repos_ids - all_maintenance_repos
-      if use_rhsm?
-        repos_ids_to_reenable << maintenance_repo(maintenance_repo_version)
-      end
-      repos_ids_to_reenable
     end
 
     def use_rhsm?
@@ -104,30 +62,11 @@ module ForemanMaintain::Scenarios
     def compose
       if check_min_version('foreman', '2.5') || check_min_version('foreman-proxy', '2.5')
         pkgs_to_update = %w[satellite-maintain rubygem-foreman_maintain]
-        add_step(Procedures::Repositories::BackupEnabledRepos.new)
-        disable_repos
-        add_step(Procedures::Repositories::Enable.new(repos: req_repos_to_update_pkgs,
-                                                      use_rhsm: use_rhsm?))
-        add_step(Procedures::Packages::Update.new(packages: pkgs_to_update, assumeyes: true))
-        disable_repos('*')
-        enable_repos(repos_ids_to_reenable)
-      end
-    end
-  end
-
-  class SelfUpgradeRescue < SelfUpgradeBase
-    metadata do
-      label :rescue_self_upgrade
-      description 'Disables all version specific maintenance repositories and,'\
-  		 "\nenables the repositories which were configured prior to self upgrade"
-      manual_detection
-      run_strategy :fail_slow
-    end
-
-    def compose
-      if check_min_version('foreman', '2.5') || check_min_version('foreman-proxy', '2.5')
-        disable_repos('*')
-        enable_repos(repos_ids_to_reenable)
+        yum_options = req_repos_to_update_pkgs.map do |id|
+          "--enablerepo=#{id}"
+        end
+        add_step(Procedures::Packages::Update.new(packages: pkgs_to_update, assumeyes: true,
+                                                  yum_options: yum_options))
       end
     end
   end
--- lib/foreman_maintain/cli/self_upgrade_command.rb
+++ lib/foreman_maintain/cli/self_upgrade_command.rb
@@ -5,17 +5,11 @@ module ForemanMaintain
              'Repository label from which packages should be updated.'\
              'This can be used when standard CDN repositories are unavailable.'
       def execute
-        run_scenario(upgrade_scenario, upgrade_rescue_scenario)
+        run_scenario(upgrade_scenario)
       end
 
       def upgrade_scenario
         Scenarios::SelfUpgrade.new(
-          maintenance_repo_label: maintenance_repo_label
-        )
-      end
-
-      def upgrade_rescue_scenario
-        Scenarios::SelfUpgradeRescue.new(
           maintenance_repo_label: maintenance_repo_label
         )
       end
