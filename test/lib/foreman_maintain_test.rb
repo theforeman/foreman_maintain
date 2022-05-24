cf64342201bb621203069f3a1748f55bf507c62d
Amit Upadhye <upadhyeammit@gmail.com>
2022-05-24 19:51:12 +0530

    Refs #34195 - Fix tests failures

--- test/lib/foreman_maintain_test.rb
+++ test/lib/foreman_maintain_test.rb
@@ -1,3 +1,4 @@
+
 describe ForemanMaintain do
   subject { ForemanMaintain }
 
@@ -8,22 +9,23 @@ describe ForemanMaintain do
     end
 
     it 'instantiates correct yum manager implementation' do
-      subject.stubs(:`).with('command -v yum').returns('/bin/yum')
+      subject.stubs(:find_pkg_manager).returns('yum')
       subject.package_manager.must_be_instance_of ForemanMaintain::PackageManager::Yum
     end
 
     it 'instantiates correct dnf manager implementation' do
-      subject.stubs(:`).with('command -v yum').returns('/bin/yum')
-      subject.stubs(:`).with('command -v dnf').returns('/bin/dnf')
+      subject.stubs(:find_pkg_manager).returns('yum')
+      subject.stubs(:find_pkg_manager).returns('dnf')
       subject.package_manager.must_be_instance_of ForemanMaintain::PackageManager::Dnf
     end
 
     it 'instantiates correct apt manager implementation' do
-      subject.stubs(:`).with('command -v apt').returns('/bin/apt')
+      subject.stubs(:find_pkg_manager).returns('apt')
       subject.package_manager.must_be_instance_of ForemanMaintain::PackageManager::Apt
     end
 
     it 'fail on unknown manager type' do
+      subject.stubs(:find_pkg_manager).returns(nil)
       err = proc { subject.package_manager }.must_raise Exception
       err.message.must_equal 'No supported package manager was found'
     end
