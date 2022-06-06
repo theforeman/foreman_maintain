cd2b3faebbac5c69f14e001c5078f948b80df933
Amit Upadhye <upadhyeammit@gmail.com>
2022-06-06 17:21:45 +0530

    Fixes #35021 - backup and restore all Postgresql dirs on Debian

--- definitions/procedures/backup/offline/foreman_db_debian.rb
+++ definitions/procedures/backup/offline/foreman_db_debian.rb
@@ -1,6 +1,6 @@
 module Procedures::Backup
   module Offline
-    class ForemanDB < ForemanMaintain::Procedure
+    class ForemanDBDebian < ForemanMaintain::Procedure
       metadata do
         description 'Backup Foreman DB offline'
         tags :backup
@@ -32,36 +32,13 @@ module Procedures::Backup
 
       def local_backup
         with_spinner("Collecting data from #{pg_data_dir}") do
-          if el?
-            do_backup
-          elsif pg_data_dir.size == 1
-            do_backup(pg_data_dir.first)
-          else
-            deb_local_backup
-          end
+          feature(:foreman_database).backup_local(
+            pg_backup_file,
+            :listed_incremental => File.join(@backup_dir, '.postgres.snar'),
+            :volume_size => @tar_volume_size,
+            :data_dir => pg_data_dir
+          )
         end
-      end
-
-      def deb_local_backup
-        # We take backup of first dir
-        first_dir = pg_data_dir.first
-        pg_data_dir.delete(first_dir)
-        do_backup(first_dir)
-
-        # We append the rest of the dirs to pgsql_data.tar file
-        pg_data_dir.each do |pg_dir|
-          do_backup(pg_dir, 'append')
-        end
-      end
-
-      def do_backup(pg_dir = pg_data_dir, cmd = 'create')
-        feature(:foreman_database).backup_local(
-          pg_backup_file,
-          :listed_incremental => File.join(@backup_dir, '.postgres.snar'),
-          :volume_size => @tar_volume_size,
-          :data_dir => pg_dir,
-          :command => cmd
-        )
       end
 
       def pg_backup_file
@@ -69,23 +46,6 @@ module Procedures::Backup
       end
 
       def pg_data_dir
-        if el?
-          pg_data_dir_el
-        else
-          pg_data_dirs_deb
-        end
-      end
-
-      def pg_data_dirs_deb
-        # The Debian based OSes support multiple installations of Postgresql
-        # There could be situations where Foreman db is either of these versions
-        # To be sure we backup the system correctly without missing anything
-        # we backup all of the Postgresql dirs
-        # Yet to implement the snapshot backup!
-        feature(:foreman_database).data_dir
-      end
-
-      def pg_data_dir_el
         return feature(:foreman_database).data_dir if @mount_dir.nil?
         mount_point = File.join(@mount_dir, 'pgsql')
         dir = feature(:foreman_database).find_base_directory(mount_point)
