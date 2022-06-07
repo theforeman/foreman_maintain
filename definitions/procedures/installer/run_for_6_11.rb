9b81dd05d9f544d1a9d328bad2cbea5ddd6a8b1e
Amit Upadhye <upadhyeammit@gmail.com>
2022-06-07 19:22:03 +0530

    Refs #35029 - refactor code and use candlepin_db_ssl and db_root_cert

--- definitions/procedures/installer/run_for_6_11.rb
+++ definitions/procedures/installer/run_for_6_11.rb
@@ -1,8 +1,8 @@
 module Procedures::Installer
   class RunFor6_11 < ForemanMaintain::Procedure
     metadata do
-      description 'Ask for Candlepin Database SSL CA certificate'\
-                  'to run installer in post upgrade'
+      description 'Run installer with Candlepin SSL CA'\
+                  ' when using external database with SSL'
       param :assumeyes, 'Do not ask for confirmation'
       manual_detection
     end
@@ -19,26 +19,20 @@ module Procedures::Installer
       !feature(:foreman_database).local?
     end
 
+    def installer_answers
+      @installer_answers ||= feature(:installer).answers
+    end
+
     def server_db_with_ssl?
-      answers = feature(:installer).answers
-      db_sslmode = answers.fetch('foreman')['db_sslmode']
-      db_sslmode == 'verify-full'
+      installer_answers.fetch('katello')['candlepin_db_ssl']
     end
 
     def extdb_and_ssl?
       ext_db? && server_db_with_ssl?
     end
 
-    def ask_for_ca_path
-      msg = 'The system is using external database over SSL protocol!'\
-			      "\nThe Satellite 6.11 has introduced new installer flag"\
-			      "\n--katello-candlepin-db-ssl-ca which is required to proceed further."\
-			      "\nRequest to provide path of Candlepin DB SSL CA:"
-      ask(msg)
-    end
-
     def run_installer_with_extra_option
-      ssl_ca_path = ask_for_ca_path
+      ssl_ca_path = installer_answers.fetch('foreman')['db_root_cert']
       spinner_msg = "Running installer with --katello-candlepin-db-ssl-ca #{ssl_ca_path} argument!"
       with_spinner(spinner_msg) do
         installer_args = feature(:installer).installer_arguments
