2591e6c3c6cab9fc49bd445cc3cf8a120d6857e1
Samir Jha <sjha4@ncsu.edu>
2022-07-22 19:42:54 +0000

    Fixes #35261 - Run procedure FixPulpcoreArtifactOwnership during 6.11 upgrade
    (cherry picked from commit 8dd7e214ed370542d0c514fee6abef71384f3c81)

--- definitions/scenarios/upgrade_to_capsule_6_11_z.rb
+++ definitions/scenarios/upgrade_to_capsule_6_11_z.rb
@@ -39,6 +39,7 @@ module Scenarios::Capsule_6_11_z
 
     def compose
       add_steps(find_procedures(:pre_migrations))
+      add_step(Procedures::Content::FixPulpcoreArtifactOwnership.new(:assumeyes => true))
       add_step(Procedures::Service::Stop.new)
     end
   end
