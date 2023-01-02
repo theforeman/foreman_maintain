theforeman/foreman_maintain

commit b0337ee30bf4c80dc161de8e4482f291e9990fdc

Fixes #35058 - upgrade scenario for Foreman and Katello nightly (#633)
* Fixes #35058 - upgrade scenario for Foreman and Katello nightly

definitions/features/instance.rb
@@ -60,8 +60,13 @@ class Features::Instance < ForemanMaintain::Feature
     end
   end
 
+  def foreman_server_ssl_ca
+    @foreman_server_ssl_ca ||= feature(:installer).answers['foreman']['server_ssl_ca']
+  end
+
   def server_connection
     net = Net::HTTP.new(ForemanMaintain.config.foreman_url, ForemanMaintain.config.foreman_port)
+    net.ca_file = foreman_server_ssl_ca
     net.use_ssl = true
     net
   end

definitions/features/capsule.rb
@@ -1,5 +1,6 @@
 class Features::Capsule < ForemanMaintain::Feature
   include ForemanMaintain::Concerns::Downstream
+  include ForemanMaintain::Concerns::Versions
 
   metadata do
     label :capsule

definitions/features/foreman_1_11_x.rb
@@ -1,9 +1,0 @@
-require 'features/foreman_1_7_x'
-
-class Features::Foreman_1_11_x < Features::Foreman_1_7_x
-  metadata do
-    confine do
-      check_min_version('foreman', '1.11')
-    end
-  end
-end

definitions/features/foreman_1_7_x.rb
@@ -1,9 +1,0 @@
-class Features::Foreman_1_7_x < ForemanMaintain::Feature
-  metadata do
-    label :foreman
-
-    confine do
-      check_min_version('foreman', '1.7')
-    end
-  end
-end

definitions/features/foreman_install.rb
@@ -1,0 +1,12 @@
+class Features::ForemanInstall < ForemanMaintain::Feature
+  include ForemanMaintain::Concerns::Upstream
+  include ForemanMaintain::Concerns::Versions
+
+  metadata do
+    label :foreman_install
+
+    confine do
+      !feature(:instance).downstream && !feature(:katello) && feature(:foreman_server)
+    end
+  end
+end

definitions/features/foreman_server.rb
@@ -1,6 +1,7 @@
 module ForemanMaintain
   module Features
     class ForemanServer < ForemanMaintain::Feature
+      include ForemanMaintain::Concerns::Versions
       metadata do
         label :foreman_server
         confine do
@@ -46,6 +47,10 @@ module ForemanMaintain
 
       def services_running?
         services.all?(&:running?)
+      end
+
+      def current_version
+        @current_version ||= package_version('foreman')
       end
     end
   end

definitions/features/installer.rb
@@ -92,9 +92,16 @@ class Features::Installer < ForemanMaintain::Feature
   end
 
   def run(arguments = '', exec_options = {})
-    out = execute!("LANG=en_US.utf-8 #{installer_command} #{arguments}".strip, exec_options)
+    out = execute!("#{installer_command} #{arguments}".strip, exec_options)
     @configuration = nil
     out
+  end
+
+  def run_with_status(arguments = '', exec_options = {})
+    cmd_with_arguments = "#{installer_command} #{arguments}".strip
+    cmd_status, out = execute_with_status(cmd_with_arguments, exec_options)
+    @configuration = nil
+    [cmd_status, out]
   end
 
   def upgrade(exec_options = {})
@@ -105,13 +112,15 @@ class Features::Installer < ForemanMaintain::Feature
     installer_args = ''
 
     if feature(:foreman_proxy) &&
-       feature(:foreman_proxy).with_content?
+       feature(:foreman_proxy).with_content? &&
+       check_max_version('foreman-installer', '3.4')
       installer_args += ' --disable-system-checks'
     end
 
-    unless check_min_version('foreman', '2.1') || check_min_version('foreman-proxy', '2.1')
+    unless check_min_version('foreman-installer', '2.1')
       installer_args += ' --upgrade' if can_upgrade?
     end
+
     installer_args
   end
 

definitions/features/katello.rb
@@ -1,4 +1,6 @@
 class Features::Katello < ForemanMaintain::Feature
+  include ForemanMaintain::Concerns::Versions
+
   metadata do
     label :katello
 

commit 508840d9d5811c1c3fe04cd54976a62f4d351634

Fixes #35892 - use server_ssl_chain to check certs
Fixes: b0337ee3

definitions/features/instance.rb
@@ -60,13 +60,13 @@ class Features::Instance < ForemanMaintain::Feature
     end
   end
 
-  def foreman_server_ssl_ca
-    @foreman_server_ssl_ca ||= feature(:installer).answers['foreman']['server_ssl_ca']
+  def server_connection_ca_file
+    @server_connection_ca_file ||= feature(:installer).answers['foreman']['server_ssl_chain']
   end
 
   def server_connection
     net = Net::HTTP.new(ForemanMaintain.config.foreman_url, ForemanMaintain.config.foreman_port)
-    net.ca_file = foreman_server_ssl_ca
+    net.ca_file = server_connection_ca_file
     net.use_ssl = true
     net
   end