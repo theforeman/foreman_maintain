theforeman/foreman_maintain

commit 1ae71c3ad827503eb370310a6b27232f80289351

Fixes #22886 - Add katello-restore (#169)

definitions/procedures/backup/online/pg_global_objects.rb
@@ -1,6 +1,8 @@
 module Procedures::Backup
   module Online
     class PgGlobalObjects < ForemanMaintain::Procedure
+      include ForemanMaintain::Concerns::SystemHelpers
+
       metadata do
         description 'Backup Postgres global objects online'
         tags :backup
@@ -11,7 +13,7 @@ module Procedures::Backup
       end
 
       def run
-        if feature(:foreman_database).local? || feature(:candlepin_database).local?
+        if feature(:instance).postgresql_local?
           local_db = feature(:foreman_database).local? ? :foreman_database : :candlepin_database
           feature(local_db).backup_global_objects(File.join(@backup_dir, 'pg_globals.dump'))
         else

definitions/procedures/restore/pg_global_objects.rb
@@ -1,0 +1,36 @@
+module Procedures::Restore
+  class PgGlobalObjects < ForemanMaintain::Procedure
+    include ForemanMaintain::Concerns::SystemHelpers
+
+    metadata do
+      description 'Restore any existing postgresql global objects from backup'
+
+      param :backup_dir,
+            'Path to backup directory',
+            :required => true
+
+      confine do
+        feature(:foreman_database) || feature(:candlepin_database)
+      end
+    end
+
+    def run
+      backup = ForemanMaintain::Utils::Backup.new(@backup_dir)
+      restore_global_objects(backup.file_map[:pg_globals][:path])
+    end
+
+    def restore_global_objects(pg_global_file)
+      if feature(:instance).postgresql_local?
+        with_spinner('') do |spinner|
+          feature(:service).handle_services(spinner, 'start', :only => ['postgresql'])
+
+          spinner.update('Restoring postgresql global objects')
+          local_db = feature(:foreman_database).local? ? :foreman_database : :candlepin_database
+          feature(local_db).restore_pg_globals(pg_global_file)
+        end
+      else
+        skip 'Restore of global objects is not supported for remote databases.'
+      end
+    end
+  end
+end

definitions/scenarios/restore.rb
@@ -1,0 +1,91 @@
+require 'foreman_maintain/utils/backup'
+
+module ForemanMaintain::Scenarios
+  class Restore < ForemanMaintain::Scenario
+    metadata do
+      description 'Restore backup'
+      param :backup_dir, 'Path to backup directory'
+      param :incremental_backup, 'Is the backup incremental?'
+      manual_detection
+    end
+
+    # rubocop:disable Metrics/MethodLength,Metrics/AbcSize
+    def compose
+      backup = ForemanMaintain::Utils::Backup.new(context.get(:backup_dir))
+
+      add_steps(find_checks(:root_user))
+      supported_version_check
+      add_steps_with_context(Checks::Restore::ValidateBackup,
+                             Procedures::Restore::Confirmation,
+                             Checks::Restore::ValidateHostname,
+                             Procedures::Selinux::SetFileSecurity,
+                             Procedures::Restore::Configs,
+                             Procedures::Restore::InstallerReset,
+                             Procedures::Service::Stop)
+      add_steps_with_context(Procedures::Restore::ExtractFiles) if backup.tar_backups_exist?
+      drop_dbs(backup)
+      if backup.sql_dump_files_exist? && feature(:instance).postgresql_local?
+        add_step(Procedures::Service::Start.new(:only => ['postgresql']))
+      end
+      restore_sql_dumps(backup)
+      if backup.sql_dump_files_exist? && feature(:instance).postgresql_local?
+        add_step(Procedures::Service::Stop.new(:only => ['postgresql']))
+      end
+      restore_mongo_dump(backup)
+      add_steps_with_context(Procedures::Pulp::Migrate,
+                             Procedures::Service::Start,
+                             Procedures::Service::DaemonReload)
+    end
+    # rubocop:enable Metrics/MethodLength,Metrics/AbcSize
+
+    def drop_dbs(backup)
+      if backup.file_map[:candlepin_dump][:present] ||
+         backup.file_map[:foreman_dump][:present]
+        add_steps_with_context(Procedures::Restore::DropDatabases)
+      end
+    end
+
+    def restore_sql_dumps(backup)
+      if backup.file_map[:pg_globals][:present]
+        add_steps_with_context(Procedures::Restore::PgGlobalObjects)
+      end
+      if backup.file_map[:candlepin_dump][:present]
+        add_steps_with_context(Procedures::Restore::CandlepinDump)
+      end
+      if backup.file_map[:foreman_dump][:present]
+        add_steps_with_context(Procedures::Restore::ForemanDump)
+      end
+    end
+
+    def restore_mongo_dump(backup)
+      if backup.file_map[:mongo_dump][:present]
+        add_steps_with_context(Procedures::Restore::MongoDump)
+      end
+    end
+
+    def supported_version_check
+      if feature(:downstream) && feature(:downstream).less_than_version?('6.3')
+        msg = 'ERROR: Restore subcommand is supported by Satellite 6.3+. ' \
+              'Please use katello-restore instead.'
+        abort(msg)
+      end
+    end
+
+    def set_context_mapping
+      context.map(:backup_dir,
+                  Checks::Restore::ValidateBackup => :backup_dir,
+                  Checks::Restore::ValidateHostname => :backup_dir,
+                  Procedures::Restore::Configs => :backup_dir,
+                  Procedures::Restore::DropDatabases => :backup_dir,
+                  Procedures::Restore::PgGlobalObjects => :backup_dir,
+                  Procedures::Restore::CandlepinDump => :backup_dir,
+                  Procedures::Restore::ForemanDump => :backup_dir,
+                  Procedures::Restore::ExtractFiles => :backup_dir,
+                  Procedures::Restore::MongoDump => :backup_dir)
+
+      context.map(:incremental_backup,
+                  Procedures::Selinux::SetFileSecurity => :incremental_backup,
+                  Procedures::Restore::InstallerReset => :incremental_backup)
+    end
+  end
+end

lib/foreman_maintain/concerns/base_database.rb
@@ -52,13 +52,34 @@ module ForemanMaintain
         execute!(dump_command(config) + " > #{file}", :hidden_patterns => [config['password']])
       end
 
+      def restore_dump(file, localdb, config = configuration)
+        if localdb
+          dump_cmd = "runuser - postgres -c 'pg_restore -C -d postgres #{file}'"
+          execute!(dump_cmd)
+        else
+          # TODO: figure out how to completely ignore errors. Currently this
+          # sometimes exits with 1 even though errors are ignored by pg_restore
+          dump_cmd = base_command(config, 'pg_restore') +
+                     ' --no-privileges --clean --disable-triggers -n public ' \
+                     "-d #{config['database']} #{file}"
+          execute!(dump_cmd, :hidden_patterns => [config['password']],
+                             :valid_exit_statuses => [0, 1])
+        end
+      end
+
+      def restore_pg_globals(pg_globals, config = configuration)
+        execute!(base_command(config, 'psql') + " -f #{pg_globals} postgres 2>/dev/null",
+                 :hidden_patterns => [config['password']])
+      end
+
       def backup_local(backup_file, extra_tar_options = {})
         dir = extra_tar_options.fetch(:data_dir, data_dir)
         FileUtils.cd(dir) do
           tar_options = {
             :archive => backup_file,
             :command => 'create',
-            :transform => 's,^,var/lib/pgsql/data/,S'
+            :transform => 's,^,var/lib/pgsql/data/,S',
+            :files => '*'
           }.merge(extra_tar_options)
           feature(:tar).run(tar_options)
         end
@@ -110,6 +131,19 @@ module ForemanMaintain
 
       def find_base_directory(directory)
         find_dir_containing_file(directory, 'postgresql.conf')
+      end
+
+      def dropdb(config = configuration)
+        if local?
+          execute!("runuser - postgres -c 'dropdb #{config['database']}'")
+        else
+          delete_statement = psql(<<-SQL)
+            select string_agg('drop table if exists \"' || tablename || '\" cascade;', '')
+            from pg_tables
+            where schemaname = 'public';
+          SQL
+          psql(delete_statement)
+        end
       end
 
       private

lib/foreman_maintain/utils/backup.rb
@@ -1,0 +1,225 @@
+require 'zlib'
+require 'rubygems/package'
+require 'yaml'
+
+module ForemanMaintain
+  module Utils
+    class Backup
+      include Concerns::SystemHelpers
+
+      attr_accessor :standard_files, :katello_online_files, :katello_offline_files,
+                    :foreman_online_files, :foreman_offline_files, :fpc_offline_files,
+                    :fpc_online_files
+
+      def initialize(backup_dir)
+        # fpc stands for foreman proxy w/ content
+        @backup_dir = backup_dir
+        @standard_files = ['config_files.tar.gz']
+        @katello_online_files = ['mongo_dump', 'candlepin.dump', 'foreman.dump']
+        @katello_offline_files = ['mongo_data.tar.gz', 'pgsql_data.tar.gz']
+        @foreman_online_files = ['foreman.dump']
+        @foreman_offline_files = ['pgsql_data.tar.gz']
+        @fpc_online_files = ['mongo_dump']
+        @fpc_offline_files = ['mongo_data.tar.gz']
+      end
+
+      def file_map
+        @file_map ||= {
+          :mongo_data => map_file(@backup_dir, 'mongo_data.tar.gz'),
+          :pgsql_data => map_file(@backup_dir, 'pgsql_data.tar.gz'),
+          :pulp_data => map_file(@backup_dir, 'pulp_data.tar'),
+          :foreman_dump => map_file(@backup_dir, 'foreman.dump'),
+          :candlepin_dump => map_file(@backup_dir, 'candlepin.dump'),
+          :mongo_dump => map_file(@backup_dir, 'mongo_dump'),
+          :config_files => map_file(@backup_dir, 'config_files.tar.gz'),
+          :pg_globals => map_file(@backup_dir, 'pg_globals.dump'),
+          :metadata => map_file(@backup_dir, 'metadata.yml')
+        }
+      end
+
+      def map_file(backup_dir, filename)
+        file_path = File.join(backup_dir, filename)
+        present = File.exist?(file_path)
+        {
+          :present => present,
+          :path => file_path
+        }
+      end
+
+      def present_files
+        present_files = file_map.select { |_k, v| v[:present] }
+        present_files.values.map { |f| File.basename(f[:path]) }
+      end
+
+      def valid_backup?
+        file_map[:config_files][:present] && check_backup
+      end
+
+      def check_backup
+        if feature(:instance).foreman_proxy_with_content?
+          valid_fpc_backup?
+        elsif feature(:katello)
+          valid_katello_backup?
+        else
+          valid_foreman_backup?
+        end
+      end
+
+      def valid_fpc_backup?
+        fpc_online_backup? || fpc_standard_backup? || fpc_logical_backup?
+      end
+
+      def valid_katello_backup?
+        katello_online_backup? || katello_standard_backup? || katello_logical_backup?
+      end
+
+      def valid_foreman_backup?
+        foreman_standard_backup? || foreman_online_backup? || foreman_logical_backup?
+      end
+
+      def katello_standard_backup?
+        file_map[:mongo_data][:present] &&
+          file_map[:pgsql_data][:present] &&
+          !(
+            file_map[:candlepin_dump][:present] ||
+            file_map[:foreman_dump][:present] ||
+            file_map[:mongo_dump][:present]
+          )
+      end
+
+      def katello_online_backup?
+        file_map[:candlepin_dump][:present] &&
+          file_map[:foreman_dump][:present] &&
+          file_map[:mongo_dump][:present] &&
+          !(
+            file_map[:mongo_data][:present] ||
+            file_map[:pgsql_data][:present]
+          )
+      end
+
+      def katello_logical_backup?
+        file_map[:mongo_dump][:present] &&
+          file_map[:mongo_data][:present] &&
+          file_map[:pgsql_data][:present] &&
+          file_map[:candlepin_dump][:present] &&
+          file_map[:foreman_dump][:present]
+      end
+
+      def fpc_standard_backup?
+        file_map[:mongo_data][:present] &&
+          !(
+            file_map[:pgsql_data][:present] ||
+            file_map[:candlepin_dump][:present] ||
+            file_map[:foreman_dump][:present] ||
+            file_map[:mongo_dump][:present]
+          )
+      end
+
+      def fpc_online_backup?
+        file_map[:mongo_dump][:present] &&
+          !(
+            file_map[:mongo_data][:present] ||
+            file_map[:pgsql_data][:present] ||
+            file_map[:candlepin_dump][:present] ||
+            file_map[:foreman_dump][:present]
+          )
+      end
+
+      def fpc_logical_backup?
+        file_map[:mongo_dump][:present] &&
+          file_map[:mongo_data][:present] &&
+          !(
+            file_map[:pgsql_data][:present] ||
+            file_map[:candlepin_dump][:present] ||
+            file_map[:foreman_dump][:present]
+          )
+      end
+
+      def foreman_standard_backup?
+        file_map[:pgsql_data][:present] &&
+          !(
+            file_map[:candlepin_dump][:present] ||
+            file_map[:foreman_dump][:present] ||
+            file_map[:mongo_data][:present] ||
+            file_map[:mongo_dump][:present]
+          )
+      end
+
+      def foreman_online_backup?
+        file_map[:foreman_dump][:present] &&
+          !(
+            file_map[:candlepin_dump][:present] ||
+            file_map[:pgsql_data][:present] ||
+            file_map[:mongo_data][:present] ||
+            file_map[:mongo_dump][:present]
+          )
+      end
+
+      def foreman_logical_backup?
+        file_map[:pgsql_data][:present] &&
+          file_map[:foreman_dump][:present] &&
+          !(
+            file_map[:candlepin_dump][:present] ||
+            file_map[:mongo_data][:present] ||
+            file_map[:mongo_dump][:present]
+          )
+      end
+
+      def validate_hostname?
+        # make sure that the system hostname is the same as the backup
+        config_tarball = file_map[:config_files][:path]
+        config_files = tarball_file_list(config_tarball)
+
+        # Incremental backups sometimes don't include httpd.conf. Since a "base" backup
+        # is restored before an incremental, we can assume that the hostname is checked
+        # during the base backup restore
+        if config_files.include?('etc/httpd/conf/httpd.conf')
+          tar_cmd = "tar zxf #{config_tarball} etc/httpd/conf/httpd.conf " \
+                    "--to-stdout | grep ServerName | awk {'print $2'} | tr -d '\"'"
+          backup_hostname = execute(tar_cmd).chomp
+          backup_hostname == hostname
+        else
+          true
+        end
+      end
+
+      def metadata
+        if file_map[:metadata][:present]
+          YAML.load_file(file_map[:metadata][:path])
+        else
+          {}
+        end
+      end
+
+      def pulp_tar_split?
+        File.exist?(File.join(@backup_dir, 'pulp_data.part0002'))
+      end
+
+      def tar_backups_exist?
+        file_map[:mongo_data][:present] ||
+          file_map[:pulp_data][:present] ||
+          file_map[:pgsql_data][:present]
+      end
+
+      def sql_dump_files_exist?
+        file_map[:foreman_dump][:present] ||
+          file_map[:candlepin_dump][:present]
+      end
+
+      private
+
+      def tarball_file_list(tarball)
+        # accepts tar.gz files only
+        file_list = []
+        File.open(tarball, 'rb') do |file|
+          ::Zlib::GzipReader.wrap(file) do |gz|
+            ::Gem::Package::TarReader.new(gz) do |tar|
+              tar.each { |entry| file_list << entry.full_name }
+            end
+          end
+        end
+        file_list
+      end
+    end
+  end
+end

definitions/checks/backup/directory_ready.rb
@@ -10,7 +10,7 @@ module Checks::Backup
 
     def run
       assert(File.directory?(@backup_dir), "Backup directory (#{@backup_dir}) does not exit.")
-      if local_psql_database?
+      if feature(:instance).postgresql_local?
         result = system("runuser - postgres -c 'test -w #{@backup_dir}'")
         assert(result, "Postgres user needs write access to the backup directory \n" \
           "Please allow the postgres user write access to #{@backup_dir}" \

definitions/checks/restore/validate_backup.rb
@@ -1,0 +1,77 @@
+require 'foreman_maintain/utils/backup'
+
+module Checks::Restore
+  class ValidateBackup < ForemanMaintain::Check
+    metadata do
+      description 'Validate backup has appropriate files'
+
+      param :backup_dir,
+            'Path to backup directory',
+            :required => true
+      manual_detection
+    end
+
+    def run
+      backup = ForemanMaintain::Utils::Backup.new(@backup_dir)
+      assert(backup.valid_backup?, valid_backup_message(backup))
+    end
+
+    def valid_backup_message(backup)
+      message = "\n"
+      message += "The given directory does not contain the required files or has too many files\n\n"
+      message += "All backup directories contain: #{backup.standard_files.join(', ')}\n"
+      message += required_files(backup)
+      message += 'Including pulp_data.tar is optional and '
+      message += "will restore pulp data to the filesystem if included.\n\n"
+      message += "Only the following files were found: #{backup.present_files.join(', ')}\n"
+      message
+    end
+
+    def required_files(backup)
+      message = ''
+      message += if feature(:instance).foreman_proxy_with_content?
+                   required_fpc_files(backup)
+                 elsif feature(:katello)
+                   required_katello_files(backup)
+                 else
+                   required_foreman_files(backup)
+                 end
+      message
+    end
+
+    def required_katello_files(backup)
+      backup_files_message(
+        backup.katello_online_files.join(', '),
+        backup.katello_offline_files.join(', '),
+        [backup.katello_online_files + backup.katello_offline_files].join(', ')
+      )
+    end
+
+    def required_fpc_files(backup)
+      backup_files_message(
+        backup.fpc_online_files.join(', '),
+        backup.fpc_offline_files.join(', '),
+        [backup.fpc_online_files + backup.fpc_offline_files].join(', ')
+      )
+    end
+
+    def required_foreman_files(backup)
+      backup_files_message(
+        backup.foreman_online_files.join(', '),
+        backup.foreman_offline_files.join(', '),
+        [backup.foreman_online_files + backup.foreman_offline_files].join(', ')
+      )
+    end
+
+    def backup_files_message(online_files, offline_files, logical_files)
+      message = ''
+      message += 'An online or remote database backup directory contains: '
+      message += "#{online_files}\n"
+      message += 'An offline backup directory contains: '
+      message += "#{offline_files}\n"
+      message += 'A logical backup directory contains: '
+      message += "#{logical_files}\n"
+      message
+    end
+  end
+end

definitions/checks/restore/validate_hostname.rb
@@ -1,0 +1,20 @@
+require 'foreman_maintain/utils/backup'
+
+module Checks::Restore
+  class ValidateHostname < ForemanMaintain::Check
+    metadata do
+      description 'Validate hostname is the same as backup'
+
+      param :backup_dir,
+            'Path to backup directory',
+            :required => true
+      manual_detection
+    end
+
+    def run
+      msg = 'The hostname in the backup does not match the hostname of the system'
+      backup = ForemanMaintain::Utils::Backup.new(@backup_dir)
+      assert(backup.validate_hostname?, msg)
+    end
+  end
+end

commit 2c3ccfd4103fe35d9dcb62265621d6b68ea8493a

Fixes #36152 - don't try to backup or restore pg_globals
We used to backup pg_globals as those contain roles (users) and should
allow us to restore the same on the new system.

However, the restore never has worked, as the code as introduced in
1ae71c3ad827503eb370310a6b27232f80289351 tried to restore the globals
using the "foreman_database" connection, so the "foreman" user which
means this user had to *exist* for the restore and more crucially would
need superuser privileges, which it never had.

In the logs, you can see that during a restore it actually throws
errors:

    DEBUG -- : Running command PGPASSWORD='[FILTERED]' psql -h localhost  -p 5432 -U foreman -f /backup/pg_globals.dump postgres 2>/dev/null with stdin nil
    DEBUG -- : output of the command:
    SET
    SET
    SET
    psql:/backup/pg_globals.dump:14: ERROR:  permission denied to create role
    psql:/backup/pg_globals.dump:15: ERROR:  must be superuser to alter superusers
    psql:/backup/pg_globals.dump:16: ERROR:  permission denied to create role
    psql:/backup/pg_globals.dump:17: ERROR:  must be superuser to alter superusers
    psql:/backup/pg_globals.dump:18: ERROR:  permission denied to create role
    psql:/backup/pg_globals.dump:19: ERROR:  must be superuser to alter superusers
    psql:/backup/pg_globals.dump:20: ERROR:  permission denied to create role
    psql:/backup/pg_globals.dump:21: ERROR:  must be superuser to alter superusers
    INFO -- : --- Execution step 'Restore any existing postgresql global objects from backup' finished ---

The code has been ported over from katello-restore [1] where it ran as
the "postgres" user, which *does* have superuser permissions and thus
worked.

The good thing is, we run the installer *before* we try to restore
things, and that already sets up all the users correctly for us, and
there is no need to try to restore pg_globals.

definitions/procedures/backup/online/pg_global_objects.rb
@@ -1,29 +1,0 @@
-module Procedures::Backup
-  module Online
-    class PgGlobalObjects < ForemanMaintain::Procedure
-      include ForemanMaintain::Concerns::SystemHelpers
-
-      metadata do
-        description 'Backup Postgres global objects online'
-        tags :backup
-        param :backup_dir, 'Directory where to backup to', :required => true
-        confine do
-          feature(:foreman_database) || feature(:candlepin_database)
-        end
-      end
-
-      def run
-        if feature(:instance).postgresql_local?
-          local_db = if feature(:instance).database_local?(:foreman_database)
-                       :foreman_database
-                     else
-                       :candlepin_database
-                     end
-          feature(local_db).backup_global_objects(File.join(@backup_dir, 'pg_globals.dump'))
-        else
-          skip 'Backup of global objects is not supported for remote databases.'
-        end
-      end
-    end
-  end
-end

definitions/procedures/restore/pg_global_objects.rb
@@ -1,36 +1,0 @@
-module Procedures::Restore
-  class PgGlobalObjects < ForemanMaintain::Procedure
-    include ForemanMaintain::Concerns::SystemHelpers
-
-    metadata do
-      description 'Restore any existing postgresql global objects from backup'
-
-      param :backup_dir,
-            'Path to backup directory',
-            :required => true
-
-      confine do
-        feature(:foreman_database) || feature(:candlepin_database)
-      end
-    end
-
-    def run
-      backup = ForemanMaintain::Utils::Backup.new(@backup_dir)
-      restore_global_objects(backup.file_map[:pg_globals][:path])
-    end
-
-    def restore_global_objects(pg_global_file)
-      if feature(:instance).postgresql_local?
-        with_spinner('') do |spinner|
-          feature(:service).handle_services(spinner, 'start', :only => ['postgresql'])
-
-          spinner.update('Restoring postgresql global objects')
-          local_db = feature(:foreman_database).local? ? :foreman_database : :candlepin_database
-          feature(local_db).restore_pg_globals(pg_global_file)
-        end
-      else
-        skip 'Restore of global objects is not supported for remote databases.'
-      end
-    end
-  end
-end

definitions/scenarios/backup.rb
@@ -58,7 +58,6 @@ module ForemanMaintain::Scenarios
                   Procedures::Backup::CompressData => :backup_dir,
                   Procedures::Backup::Pulp => :backup_dir,
                   Procedures::Backup::Online::Mongo => :backup_dir,
-                  Procedures::Backup::Online::PgGlobalObjects => :backup_dir,
                   Procedures::Backup::Online::CandlepinDB => :backup_dir,
                   Procedures::Backup::Online::ForemanDB => :backup_dir,
                   Procedures::Backup::Online::PulpcoreDB => :backup_dir,
@@ -160,9 +159,6 @@ module ForemanMaintain::Scenarios
     end
 
     def include_dumps
-      if feature(:instance).postgresql_local?
-        add_step_with_context(Procedures::Backup::Online::PgGlobalObjects)
-      end
       if feature(:instance).database_local?(:candlepin_database)
         add_step_with_context(Procedures::Backup::Online::CandlepinDB)
       end
@@ -217,7 +213,6 @@ module ForemanMaintain::Scenarios
       add_step_with_context(Procedures::Backup::Pulp, :ensure_unchanged => true)
       add_steps_with_context(
         Procedures::Backup::Online::Mongo,
-        Procedures::Backup::Online::PgGlobalObjects,
         Procedures::Backup::Online::CandlepinDB,
         Procedures::Backup::Online::ForemanDB,
         Procedures::Backup::Online::PulpcoreDB

definitions/scenarios/restore.rb
@@ -73,9 +73,6 @@ module ForemanMaintain::Scenarios
     end
 
     def restore_sql_dumps(backup)
-      if backup.file_map[:pg_globals][:present]
-        add_steps_with_context(Procedures::Restore::PgGlobalObjects)
-      end
       if backup.file_map[:candlepin_dump][:present]
         add_steps_with_context(Procedures::Restore::CandlepinDump)
       end
@@ -108,7 +105,6 @@ module ForemanMaintain::Scenarios
                   Checks::Restore::ValidateInterfaces => :backup_dir,
                   Procedures::Restore::Configs => :backup_dir,
                   Procedures::Restore::DropDatabases => :backup_dir,
-                  Procedures::Restore::PgGlobalObjects => :backup_dir,
                   Procedures::Restore::CandlepinDump => :backup_dir,
                   Procedures::Restore::ForemanDump => :backup_dir,
                   Procedures::Restore::PulpcoreDump => :backup_dir,

lib/foreman_maintain/concerns/base_database.rb
@@ -108,11 +108,6 @@ module ForemanMaintain
         end
       end
 
-      def restore_pg_globals(pg_globals, config = configuration)
-        execute!(base_command(config, 'psql') + " -f #{pg_globals} postgres 2>/dev/null",
-                 :hidden_patterns => [config['password']])
-      end
-
       def backup_local(backup_file, extra_tar_options = {})
         dir = extra_tar_options.fetch(:data_dir, data_dir)
         command = extra_tar_options.fetch(:command, 'create')
@@ -137,10 +132,6 @@ module ForemanMaintain
       # TODO: remove the backup file path tools from here. Lib Utils::Backup?
       def backup_dir
         @backup_dir ||= File.expand_path(ForemanMaintain.config.db_backup_dir)
-      end
-
-      def backup_global_objects(file)
-        execute!("runuser - postgres -c 'pg_dumpall -g > #{file}'")
       end
 
       def perform_backup(config = configuration)

lib/foreman_maintain/utils/backup.rb
@@ -42,7 +42,6 @@ module ForemanMaintain
           :candlepin_dump => map_file(@backup_dir, 'candlepin.dump'),
           :mongo_dump => map_file(@backup_dir, 'mongo_dump'),
           :config_files => map_file(@backup_dir, 'config_files.tar.gz'),
-          :pg_globals => map_file(@backup_dir, 'pg_globals.dump'),
           :metadata => map_file(@backup_dir, 'metadata.yml'),
           :pulpcore_dump => map_file(@backup_dir, 'pulpcore.dump')
         }