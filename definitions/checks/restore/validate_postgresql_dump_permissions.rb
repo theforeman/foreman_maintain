20ca7111a0430994fd0332d49e98e730c4203ab1
Evgeni Golov <evgeni@golov.de>
2023-03-23 11:51:22 +0100

    add check whether postgres user can read dump files

---
+++ definitions/checks/restore/validate_postgresql_dump_permissions.rb
@@ -0,0 +1,31 @@
+module Checks::Restore
+  class ValidatePostgresqlDumpPermissions < ForemanMaintain::Check
+    metadata do
+      description 'Validate permissions for PostgreSQL dumps'
+
+      param :backup_dir,
+        'Path to backup directory',
+        :required => true
+      manual_detection
+    end
+
+    def run
+      backup = ForemanMaintain::Utils::Backup.new(@backup_dir)
+      if feature(:instance).postgresql_local? && backup.online_backup?
+        errors = []
+        [:candlepin_dump, :foreman_dump, :pulpcore_dump].each do |dump|
+          next unless backup.file_map[dump][:present]
+
+          unless system("runuser - postgres -c 'test -r #{backup.file_map[dump][:path]}'")
+            errors << backup.file_map[dump][:path]
+          end
+        end
+
+        msg = "The 'postgres' user needs read access to the following files:\n"
+        msg << errors.join("\n")
+        assert(errors.empty?, msg)
+
+      end
+    end
+  end
+end
