a24b4c98f7443a7e40a45ba1f2e9c7ab7f5d2f6f
Evgeni Golov <evgeni@golov.de>
2023-04-12 10:56:24 +0200

    Fixes #33947 - only check directory permissions for backups with dumps
    offline backups are performed as root and don't need write
    permissions for the postgres user

--- definitions/checks/backup/directory_ready.rb
+++ definitions/checks/backup/directory_ready.rb
@@ -6,11 +6,13 @@ module Checks::Backup
       manual_detection
       param :backup_dir, 'Directory where to backup to', :required => true
       param :preserve_dir, 'Directory where to backup to', :flag => true, :default => false
+      param :postgres_access, 'Whether the postgres user needs access', :flag => true,
+        :default => false
     end
 
     def run
       assert(File.directory?(@backup_dir), "Backup directory (#{@backup_dir}) does not exist.")
-      if feature(:instance).postgresql_local?
+      if feature(:instance).postgresql_local? && @postgres_access
         result = system("runuser - postgres -c 'test -w #{@backup_dir}'")
         assert(result, "Postgres user needs write access to the backup directory \n" \
           "Please allow the postgres user write access to #{@backup_dir}" \
