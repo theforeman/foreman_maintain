theforeman/foreman_maintain

commit 3b30b83c480201936bb67f03f03c1fe95bfff0a5

Allow to define product support entity in features file

definitions/features/instance.rb
@@ -28,6 +28,14 @@ class Features::Instance < ForemanMaintain::Feature
     end
   end
 
+  def project_support_entity
+    if feature(:satellite)
+      'Red Hat Technical Support'
+    else
+      'Foreman Community Support'
+    end
+  end
+
   def database_remote?(feature)
     !!feature(feature) && !feature(feature).local?
   end

test/lib/reporter_test.rb
@@ -9,6 +9,14 @@ module ForemanMaintain
     def downstream
       feature(:satellite) || feature(:capsule)
     end
+
+    def project_support_entity
+      if feature(:satellite)
+        'Red Hat Technical Support'
+      else
+        'Foreman community support'
+      end
+    end
   end
 
   describe Reporter::CLIReporter do

lib/foreman_maintain/reporter/cli_reporter.rb
@@ -330,20 +330,20 @@ module ForemanMaintain
           whitelist_labels = steps_to_whitelist.map(&:label_dashed).join(',')
           unless whitelist_labels.empty?
             recommend << if scenario.detector.feature(:instance).downstream
-                           format(<<-MESSAGE.strip_heredoc, whitelist_labels)
+                           format(<<-MESSAGE.strip_heredoc)
               Resolve the failed steps and rerun the command.
 
               If the situation persists and, you are unclear what to do next,
-              contact Red Hat Technical Support.
+              contact #{scenario.detector.feature(:instance).project_support_entity}.
 
               In case the failures are false positives, use
-              --whitelist="%s"
+              --whitelist="#{whitelist_labels}"
                            MESSAGE
                          else
-                           format(<<-MESSAGE.strip_heredoc, whitelist_labels)
+                           format(<<-MESSAGE.strip_heredoc)
               Resolve the failed steps and rerun the command.
               In case the failures are false positives, use
-              --whitelist="%s"
+              --whitelist="#{whitelist_labels}"
                            MESSAGE
                          end
           end

commit eb2569745792fa6ced42a4a485826f9d68c84c9a

also use RH support for capsule setups
Fixes: 3b30b83c480201936bb67f03f03c1fe95bfff0a5

definitions/features/instance.rb
@@ -29,7 +29,7 @@ class Features::Instance < ForemanMaintain::Feature
   end
 
   def project_support_entity
-    if feature(:satellite)
+    if downstream
       'Red Hat Technical Support'
     else
       'Foreman Community Support'

test/lib/reporter_test.rb
@@ -11,7 +11,7 @@ module ForemanMaintain
     end
 
     def project_support_entity
-      if feature(:satellite)
+      if downstream
         'Red Hat Technical Support'
       else
         'Foreman community support'