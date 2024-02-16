a58650e05d73103f882495672e5370219d20e18d
Eric D. Helms <ericdhelms@gmail.com>
2024-02-16 16:32:01 -0500

    Remove old options from installer reset

--- definitions/procedures/restore/installer_reset.rb
+++ definitions/procedures/restore/installer_reset.rb
@@ -12,28 +12,12 @@ module Procedures::Restore
 
     def installer_cmd
       installer = "yes | #{feature(:installer).installer_command} "
-      installer << reset_option
+      installer << '-v --reset-data '
       if feature(:instance).foreman_proxy_with_content?
         installer << '--foreman-proxy-register-in-foreman false '
       end
 
-      # We always disable system checks to avoid unnecessary errors. The installer should have
-      # already ran since this is to be run on an existing system, which means installer checks
-      # has already been skipped
-      if feature(:foreman_proxy)&.with_content? &&
-         check_max_version('foreman-installer', '3.4')
-        installer << '--disable-system-checks '
-      end
       installer
-    end
-
-    def reset_option
-      if check_min_version('foreman', '2.2') || \
-         check_min_version('foreman-proxy', '2.2')
-        return '-v --reset-data '
-      end
-
-      '-v --reset '
     end
   end
 end
