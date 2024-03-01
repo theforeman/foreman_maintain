46ba4616fcbdeaec4d4faadbf95cfb868caa080d
Marek Hulan <mhulan@redhat.com>
2024-03-01 16:33:28 +0100

    Allow report generating in the foreman-maintain
    This extends the check so it can hold any arbitrary data. That is then
    used in the generate report command, that finds all checks tagged with
    report tag, executes them and gather their data for the final YAML
    creation.
    
    This commit also includes two examples of metrics that uses database as
    the Data source.

---
+++ lib/foreman_maintain/cli/report_command.rb
@@ -0,0 +1,19 @@
+module ForemanMaintain
+  module Cli
+    class ReportCommand < Base
+      extend Concerns::Finders
+
+      subcommand 'generate', 'Generates the usage report' do
+        def execute
+          scenario = run_scenario(Scenarios::Report::Generate.new(tags: :report)).first
+          # description can be used too
+          report_data = scenario.steps.map(&:data).reduce(&:merge)
+#require 'pry'
+#binding.pry
+          puts report_data.to_yaml
+          exit runner.exit_code
+        end
+      end
+    end
+  end
+end
