56e8b616b498ddcd9669b2a2f79669d38aba0452
Eric D. Helms <ericdhelms@gmail.com>
2024-03-27 10:46:40 -0400

    Refs #37275: Enable modules and download updates after enabling postgresql module

--- definitions/scenarios/upgrade_to_satellite_6_16.rb
+++ definitions/scenarios/upgrade_to_satellite_6_16.rb
@@ -39,17 +39,6 @@ module Scenarios::Satellite_6_16
     end
 
     def compose
-      add_step(Procedures::Repositories::Setup.new(:version => '6.16'))
-      if el8?
-        modules_to_switch = ['postgresql:13']
-        add_step(Procedures::Packages::SwitchModules.new(:module_names => modules_to_switch))
-        modules_to_enable = ["satellite:#{el_short_name}"]
-        add_step(Procedures::Packages::EnableModules.new(:module_names => modules_to_enable))
-      end
-      add_step(Procedures::Packages::Update.new(
-        :assumeyes => true,
-        :dnf_options => ['--downloadonly']
-      ))
       add_steps(find_procedures(:pre_migrations))
     end
   end
@@ -66,6 +55,17 @@ module Scenarios::Satellite_6_16
     end
 
     def compose
+      add_step(Procedures::Repositories::Setup.new(:version => '6.16'))
+      if el8?
+        modules_to_switch = ['postgresql:13']
+        add_step(Procedures::Packages::SwitchModules.new(:module_names => modules_to_switch))
+        modules_to_enable = ["satellite:#{el_short_name}"]
+        add_step(Procedures::Packages::EnableModules.new(:module_names => modules_to_enable))
+      end
+      add_step(Procedures::Packages::Update.new(
+        :assumeyes => true,
+        :dnf_options => ['--downloadonly']
+      ))
       add_step(Procedures::Service::Stop.new)
       add_step(Procedures::Packages::Update.new(:assumeyes => true, :clean_cache => false))
       add_step_with_context(Procedures::Installer::Run)
