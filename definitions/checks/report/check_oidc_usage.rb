a6877cf68f3fc9f6e686678ee6beed67df467459
Marek Hulan <mhulan@redhat.com>
2024-05-10 16:35:50 +0200

    Add check for OIDC/Keycloak use

---
+++ definitions/checks/report/check_oidc_usage.rb
@@ -0,0 +1,18 @@
+module Checks
+  module Report
+    class CheckOIDCUsage < ForemanMaintain::Check
+      metadata do
+        description 'Checks the use of Keycloak/OIDC'
+        tags :report
+      end
+
+      # Do you use OIDC/keycloak?
+      def run
+        oidc_issuer = feature(:foreman_database).query("SELECT value FROM settings WHERE name = 'oidc_issuer'").first
+        result = (oidc_issuer && YAML.load(oidc_issuer['value']).is_a?(String) && YAML.load(oidc_issuer['value']) != '')
+
+        self.data = { oidc_use: !!result }
+      end
+    end
+  end
+end
