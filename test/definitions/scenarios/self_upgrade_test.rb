ce4ca3d99dba23087bd86f7a25f5e55e06f94bf3
Eric D. Helms <ericdhelms@gmail.com>
2024-05-31 11:57:18 -0400

    Fail self-upgrade if current and target version are different

--- test/definitions/scenarios/self_upgrade_test.rb
+++ test/definitions/scenarios/self_upgrade_test.rb
@@ -1,29 +1,120 @@
 require 'test_helper'
+require_relative '../test_helper'
+require_relative '../../../definitions/scenarios/self_upgrade'
 
-module Scenarios
-  describe ForemanMaintain::Scenarios::SelfUpgradeBase do
-    include DefinitionsTestHelper
+describe ForemanMaintain::Scenarios::SelfUpgradeBase do
+  include ::DefinitionsTestHelper
 
-    describe 'with default params' do
-      let(:scenario) do
-        ForemanMaintain::Scenarios::SelfUpgradeBase.new
-      end
+  before do
+    File.stubs(:exist?).with('/etc/redhat-release').returns(true)
+  end
 
-      it 'computes the target version correctly coming from normal release 6.10.0' do
-        assume_satellite_present do |feature_class|
-          feature_class.any_instance.stubs(:current_version => version('6.10.0'))
-        end
+  let(:scenario) do
+    ForemanMaintain::Scenarios::SelfUpgradeBase.new
+  end
 
-        assert_equal '6.11', scenario.target_version
-      end
+  it 'computes the target version correctly for downstream' do
+    assume_satellite_present
 
-      it 'computes the target version correctly coming from an async release 6.11.1.1' do
-        assume_satellite_present do |feature_class|
-          feature_class.any_instance.stubs(:current_version => version('6.11.1.1'))
-        end
+    assert_equal '6.16', scenario.target_version
+  end
 
-        assert_equal '6.12', scenario.target_version
-      end
+  it 'returns target version correctly for upstream' do
+    assume_feature_present(:foreman_install)
+
+    assert_equal '3.11', scenario.target_version
+  end
+
+  it 'allow upgrade when versions are equal' do
+    assume_satellite_present
+    scenario.expects(:current_version).returns('6.16.0')
+
+    assert scenario.self_upgrade_allowed?
+  end
+
+  it 'allow upgrade when versions are equal for z-stream' do
+    assume_satellite_present
+    scenario.expects(:current_version).returns('6.16.4')
+
+    assert scenario.self_upgrade_allowed?
+  end
+
+  it 'allow upgrade when versions are off by 1' do
+    assume_satellite_present
+    scenario.expects(:current_version).twice.returns('6.15.8')
+
+    assert scenario.self_upgrade_allowed?
+  end
+
+  it 'does not allow upgrade when versions are off by 2 or more' do
+    assume_satellite_present
+    scenario.expects(:current_version).twice.returns('6.14.4')
+
+    refute scenario.self_upgrade_allowed?
+  end
+end
+
+describe ForemanMaintain::Scenarios::SelfUpgrade do
+  include ::DefinitionsTestHelper
+
+  before do
+    File.stubs(:exist?).with('/etc/redhat-release').returns(true)
+  end
+
+  it 'runs successfully for downstream Satellite' do
+    ForemanMaintain.
+      package_manager.
+      expects(:find_installed_package).
+      with('satellite', "%{VERSION}").
+      returns('6.16.0')
+    assume_feature_present(:satellite)
+    scenario = ForemanMaintain::Scenarios::SelfUpgrade.new
+    scenario.compose
+
+    assert run_step(scenario)
+  end
+
+  it 'runs successfully for downstream Capsule' do
+    ForemanMaintain.
+      package_manager.
+      expects(:find_installed_package).
+      with('satellite-capsule', "%{VERSION}").
+      returns('6.16.3')
+    assume_feature_present(:capsule)
+    scenario = ForemanMaintain::Scenarios::SelfUpgrade.new
+    scenario.compose
+
+    assert run_step(scenario)
+  end
+
+  it 'run successfully if current and target version are off by 1' do
+    ForemanMaintain.
+      package_manager.
+      expects(:find_installed_package).
+      with('satellite', "%{VERSION}").
+      returns('6.15.3')
+    assume_feature_present(:satellite)
+    scenario = ForemanMaintain::Scenarios::SelfUpgrade.new
+    scenario.compose
+
+    assert run_step(scenario)
+  end
+
+  it 'fails if versions are off 2 or more' do
+    ForemanMaintain.
+      package_manager.
+      expects(:find_installed_package).
+      with('satellite', "%{VERSION}").
+      returns('6.14.1')
+    assume_feature_present(:satellite)
+
+    msg = "foreman-maintain is too many versions ahead. The target " \
+          "version is 6.16 while the currently installed " \
+          "version is 6.14.1. Please rollback " \
+          "foreman-maintain to the proper version."
+    assert_raises(ForemanMaintain::Error::Warn, msg) do
+      scenario = ForemanMaintain::Scenarios::SelfUpgrade.new
+      scenario.compose
     end
   end
 end
