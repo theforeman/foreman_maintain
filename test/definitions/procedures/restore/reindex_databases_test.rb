a93f9d6a719a4066209515ea487026aa60c94f8e
Evgeni Golov <evgeni@golov.de>
2024-06-03 14:17:38 +0200

    refresh collation version when pulp_ansible is installed

--- test/definitions/procedures/restore/reindex_databases_test.rb
+++ test/definitions/procedures/restore/reindex_databases_test.rb
@@ -16,6 +16,19 @@ describe Procedures::Restore::ReindexDatabases do
     reindex_command = 'runuser - postgres -c "reindexdb -a"'
 
     subject.stubs(:'execute!').with(reindex_command).returns(0)
+    subject.stubs(:check_min_version).returns(false)
+    result = run_procedure(subject)
+    assert result.success?, 'the procedure was expected to succeed'
+  end
+
+  it 'reindexes all DBs if DB is local and pulp-ansible is present' do
+    reindex_command = 'runuser - postgres -c "reindexdb -a"'
+    collate_command = 'runuser -c \'echo "ALTER COLLATION pulp_ansible_semver REFRESH VERSION;"'\
+                       '| psql pulpcore\' postgres'
+
+    subject.stubs(:'execute!').with(reindex_command).returns(0)
+    subject.stubs(:check_min_version).with('python3.11-pulp-ansible', '0.20.0').returns(true)
+    subject.stubs(:'execute!').with(collate_command).returns(0)
     result = run_procedure(subject)
     assert result.success?, 'the procedure was expected to succeed'
   end
