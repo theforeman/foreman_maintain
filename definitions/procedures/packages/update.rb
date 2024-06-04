theforeman/foreman_maintain

commit 9ac8a20cda789c0d733396f0c92bf9b45d25901c

drop dnf_options from P::P::Update

definitions/procedures/packages/update.rb
@@ -6,7 +6,6 @@ module Procedures::Packages
       param :force, 'Do not skip if package is installed', :flag => true, :default => false
       param :warn_on_errors, 'Do not interrupt scenario on failure',
         :flag => true, :default => false
-      param :dnf_options, 'Extra dnf options if any', :array => true, :default => []
       param :download_only, 'Download and cache packages only', :flag => true, :default => false
       param :clean_cache, 'If true will cause a DNF cache clean', :flag => true, :default => true
     end
@@ -16,7 +15,6 @@ module Procedures::Packages
       package_manager.clean_cache(:assumeyes => assumeyes_val) if @clean_cache
       opts = {
         :assumeyes => assumeyes_val,
-        :options => @dnf_options,
         :download_only => @download_only,
       }
       packages_action(:update, @packages, opts)

lib/foreman_maintain/concerns/system_helpers.rb
@@ -108,7 +108,6 @@ module ForemanMaintain
           package_manager.update(
             packages,
             :assumeyes => options[:assumeyes],
-            :options => options[:options],
             :download_only => options[:download_only]
           )
         when :remove

lib/foreman_maintain/package_manager/apt.rb
@@ -19,12 +19,10 @@ module ForemanMaintain::PackageManager
       apt_action('remove', packages, :assumeyes => assumeyes)
     end
 
-    # rubocop:disable Lint/UnusedMethodArgument
-    def update(packages = [], assumeyes: false, options: [], download_only: false)
+    def update(packages = [], assumeyes: false, download_only: false)
       action = packages.any? ? '--only-upgrade install' : 'upgrade'
       apt_action(action, packages, :assumeyes => assumeyes, :download_only => download_only)
     end
-    # rubocop:enable Lint/UnusedMethodArgument
 
     def clean_cache(assumeyes: false)
       apt_action('clean', [], :assumeyes => assumeyes)

lib/foreman_maintain/package_manager/dnf.rb
@@ -59,12 +59,11 @@ module ForemanMaintain::PackageManager
       dnf_action('remove', packages, assumeyes: assumeyes)
     end
 
-    def update(packages = [], assumeyes: false, options: [], download_only: false)
+    def update(packages = [], assumeyes: false, download_only: false)
       dnf_action(
         'update',
         packages,
         assumeyes: assumeyes,
-        dnf_options: options,
         download_only: download_only
       )
     end

definitions/scenarios/update.rb
@@ -59,7 +59,7 @@ module Scenarios::Update
       add_steps(
         Procedures::Packages::Update.new(
           :assumeyes => true,
-          :dnf_options => ['--downloadonly']
+          :download_only => true
         ),
         Procedures::MaintenanceMode::EnableMaintenanceMode,
         Procedures::Crond::Stop,

commit 3dabb6b5a3470891c9bfa2845a1aeb2aeae798be

Fix self-upgrade by adding enabled-repos support to dnf
Fixes: 9ac8a20

definitions/procedures/packages/update.rb
@@ -8,6 +8,7 @@ module Procedures::Packages
         :flag => true, :default => false
       param :download_only, 'Download and cache packages only', :flag => true, :default => false
       param :clean_cache, 'If true will cause a DNF cache clean', :flag => true, :default => true
+      param :enabled_repos, 'List of repositories to enable', :array => true
     end
 
     def run
@@ -16,6 +17,7 @@ module Procedures::Packages
       opts = {
         :assumeyes => assumeyes_val,
         :download_only => @download_only,
+        :enabled_repos => @enabled_repos,
       }
       packages_action(:update, @packages, opts)
     rescue ForemanMaintain::Error::ExecutionError => e

definitions/scenarios/self_upgrade.rb
@@ -67,11 +67,8 @@ module ForemanMaintain::Scenarios
     def downstream_self_upgrade(pkgs_to_update)
       ForemanMaintain.enable_maintenance_module
 
-      dnf_options = req_repos_to_update_pkgs.map do |id|
-        "--enablerepo=#{id}"
-      end
       add_step(Procedures::Packages::Update.new(packages: pkgs_to_update, assumeyes: true,
-        dnf_options: dnf_options))
+        enabled_repos: req_repos_to_update_pkgs))
     end
 
     def upstream_self_upgrade(pkgs_to_update)

lib/foreman_maintain/concerns/system_helpers.rb
@@ -100,7 +100,7 @@ module ForemanMaintain
       end
 
       def packages_action(action, packages, options = {})
-        options.validate_options!(:assumeyes, :options, :download_only)
+        options.validate_options!(:assumeyes, :options, :download_only, :enabled_repos)
         case action
         when :install
           package_manager.install(packages, :assumeyes => options[:assumeyes])
@@ -108,7 +108,8 @@ module ForemanMaintain
           package_manager.update(
             packages,
             :assumeyes => options[:assumeyes],
-            :download_only => options[:download_only]
+            :download_only => options[:download_only],
+            :enabled_repos => options[:enabled_repos]
           )
         when :remove
           package_manager.remove(packages, :assumeyes => options[:assumeyes])

lib/foreman_maintain/package_manager/apt.rb
@@ -19,10 +19,12 @@ module ForemanMaintain::PackageManager
       apt_action('remove', packages, :assumeyes => assumeyes)
     end
 
-    def update(packages = [], assumeyes: false, download_only: false)
+    # rubocop:disable Lint/UnusedMethodArgument
+    def update(packages = [], assumeyes: false, download_only: false, enabled_repos: [])
       action = packages.any? ? '--only-upgrade install' : 'upgrade'
       apt_action(action, packages, :assumeyes => assumeyes, :download_only => download_only)
     end
+    # rubocop:enable Lint/UnusedMethodArgument
 
     def clean_cache(assumeyes: false)
       apt_action('clean', [], :assumeyes => assumeyes)

lib/foreman_maintain/package_manager/dnf.rb
@@ -59,12 +59,13 @@ module ForemanMaintain::PackageManager
       dnf_action('remove', packages, assumeyes: assumeyes)
     end
 
-    def update(packages = [], assumeyes: false, download_only: false)
+    def update(packages = [], assumeyes: false, download_only: false, enabled_repos: [])
       dnf_action(
         'update',
         packages,
         assumeyes: assumeyes,
-        download_only: download_only
+        download_only: download_only,
+        enabled_repos: enabled_repos
       )
     end
 
@@ -132,13 +133,17 @@ module ForemanMaintain::PackageManager
 
     private
 
-    # rubocop:disable Layout/LineLength, Metrics/ParameterLists
-    def dnf_action(action, packages, with_status: false, assumeyes: false, dnf_options: [], valid_exit_statuses: [0], download_only: false)
+    # rubocop:disable Layout/LineLength, Metrics/ParameterLists, Metrics/MethodLength
+    def dnf_action(action, packages, with_status: false, assumeyes: false, dnf_options: [], valid_exit_statuses: [0], download_only: false, enabled_repos: [])
       packages = [packages].flatten(1)
 
       dnf_options << '-y' if assumeyes
       dnf_options << '--downloadonly' if download_only
       dnf_options << '--disableplugin=foreman-protector'
+
+      enabled_repos.map do |id|
+        dnf_options << "--enablerepo=#{id}"
+      end
 
       command = ['dnf', dnf_options.join(' '), action]
 
@@ -158,7 +163,7 @@ module ForemanMaintain::PackageManager
         )
       end
     end
-    # rubocop:enable Layout/LineLength, Metrics/ParameterLists
+    # rubocop:enable Layout/LineLength, Metrics/ParameterLists, Metrics/MethodLength
 
     def protector_config
       File.exist?(protector_config_file) ? File.read(protector_config_file) : ''