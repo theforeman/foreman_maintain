38f674a097a12b8655e677a9d6aac34b82dfbc5b
Evgeni Golov <evgeni@golov.de>
2024-06-17 11:08:45 +0200

    add "no running tasks" check for Pulpcore

---
+++ test/definitions/checks/pulpcore/no_running_tasks_test.rb
@@ -0,0 +1,50 @@
+require 'test_helper'
+
+describe Checks::Pulpcore::NoRunningTasks do
+  include DefinitionsTestHelper
+
+  context 'with default params' do
+    subject do
+      Checks::Pulpcore::NoRunningTasks.new
+    end
+
+    it 'passes when not active tasks are present' do
+      assume_feature_present(:pulpcore, :running_tasks => [])
+      result = run_check(subject)
+      assert result.success?, 'Check expected to succeed'
+    end
+
+    it 'fails when running/paused tasks are present' do
+      assume_feature_present(:pulpcore, :running_tasks => ['a_task'])
+      result = run_check(subject)
+      assert result.fail?, 'Check expected to fail'
+      msg = 'There are 1 active task(s) in the system.'
+      msg += "\nPlease wait for these to complete."
+      assert_match msg, result.output
+      assert_empty subject.next_steps.map(&:class)
+    end
+  end
+
+  context 'with wait_for_tasks=>true' do
+    subject do
+      Checks::Pulpcore::NoRunningTasks.new(:wait_for_tasks => true)
+    end
+
+    it 'passes when not active tasks are present' do
+      assume_feature_present(:pulpcore, :running_tasks => [])
+      result = run_check(subject)
+      assert result.success?, 'Check expected to succeed'
+    end
+
+    it 'fails when running/paused tasks are present' do
+      assume_feature_present(:pulpcore, :running_tasks => ['a_task'])
+      result = run_check(subject)
+      assert result.fail?, 'Check expected to fail'
+      msg = 'There are 1 active task(s) in the system.'
+      msg += "\nPlease wait for these to complete."
+      assert_match msg, result.output
+      assert_equal [Procedures::Pulpcore::WaitForTasks],
+        subject.next_steps.map(&:class)
+    end
+  end
+end
