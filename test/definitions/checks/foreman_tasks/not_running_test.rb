da67ffd092f66c1347567a1f4298ed9dedc3a3c2
Evgeni Golov <evgeni@golov.de>
2024-06-17 11:10:22 +0200

    make wait_for_tasks configurable in NotRunning foreman tasks check

--- test/definitions/checks/foreman_tasks/not_running_test.rb
+++ test/definitions/checks/foreman_tasks/not_running_test.rb
@@ -3,25 +3,49 @@ require 'test_helper'
 describe Checks::ForemanTasks::NotRunning do
   include DefinitionsTestHelper
 
-  subject do
-    Checks::ForemanTasks::NotRunning.new
+  context 'with default params' do
+    subject do
+      Checks::ForemanTasks::NotRunning.new
+    end
+
+    it 'passes when not active tasks are present' do
+      assume_feature_present(:foreman_tasks, :running_tasks_count => 0)
+      result = run_check(subject)
+      assert result.success?, 'Check expected to succeed'
+    end
+
+    it 'fails when running/paused tasks are present' do
+      assume_feature_present(:foreman_tasks, :running_tasks_count => 5)
+      result = run_check(subject)
+      assert result.fail?, 'Check expected to fail'
+      msg = 'There are 5 active task(s) in the system.'
+      msg += "\nPlease wait for these to complete or cancel them from the Monitor tab."
+      assert_match msg, result.output
+      assert_equal [Procedures::ForemanTasks::FetchTasksStatus,
+                    Procedures::ForemanTasks::UiInvestigate],
+        subject.next_steps.map(&:class)
+    end
   end
 
-  it 'passes when not active tasks are present' do
-    assume_feature_present(:foreman_tasks, :running_tasks_count => 0)
-    result = run_check(subject)
-    assert result.success?, 'Check expected to succeed'
-  end
+  context 'with wait_for_tasks=>false' do
+    subject do
+      Checks::ForemanTasks::NotRunning.new(:wait_for_tasks => false)
+    end
 
-  it 'fails when running/paused tasks are present' do
-    assume_feature_present(:foreman_tasks, :running_tasks_count => 5)
-    result = run_check(subject)
-    assert result.fail?, 'Check expected to fail'
-    msg = 'There are 5 active task(s) in the system.'
-    msg += "\nPlease wait for these to complete or cancel them from the Monitor tab."
-    assert_match msg, result.output
-    assert_equal [Procedures::ForemanTasks::FetchTasksStatus,
-                  Procedures::ForemanTasks::UiInvestigate],
-      subject.next_steps.map(&:class)
+    it 'passes when not active tasks are present' do
+      assume_feature_present(:foreman_tasks, :running_tasks_count => 0)
+      result = run_check(subject)
+      assert result.success?, 'Check expected to succeed'
+    end
+
+    it 'fails when running/paused tasks are present' do
+      assume_feature_present(:foreman_tasks, :running_tasks_count => 5)
+      result = run_check(subject)
+      assert result.fail?, 'Check expected to fail'
+      msg = 'There are 5 active task(s) in the system.'
+      msg += "\nPlease wait for these to complete or cancel them from the Monitor tab."
+      assert_match msg, result.output
+      assert_empty subject.next_steps.map(&:class)
+    end
   end
 end
