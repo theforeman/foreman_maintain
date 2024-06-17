76070aaec633044c654b49ddd10c20a511b158a8
Evgeni Golov <evgeni@golov.de>
2024-06-17 11:10:22 +0200

    make wait_for_tasks configurable in NotRunning foreman tasks check

--- definitions/checks/foreman_tasks/not_running.rb
+++ definitions/checks/foreman_tasks/not_running.rb
@@ -6,17 +6,17 @@ module Checks::ForemanTasks
       tags :pre_upgrade
       after :foreman_tasks_not_paused
       before :check_old_foreman_tasks
+      param :wait_for_tasks,
+        'Wait for tasks to finish or fail directly',
+        :required => false,
+        :default => true
     end
 
     def run
       task_count = feature(:foreman_tasks).running_tasks_count
       assert(task_count == 0,
         failure_message(task_count),
-        :next_steps =>
-          [Procedures::ForemanTasks::FetchTasksStatus.new(:state => 'running'),
-           Procedures::ForemanTasks::UiInvestigate.new(
-             'search_query' => search_query_for_running_tasks
-           )])
+        :next_steps => calculate_next_steps)
     end
 
     private
@@ -29,6 +29,19 @@ module Checks::ForemanTasks
     def failure_message(task_count)
       "There are #{task_count} active task(s) in the system." \
       "\nPlease wait for these to complete or cancel them from the Monitor tab."
+    end
+
+    def calculate_next_steps
+      steps = []
+      if @wait_for_tasks
+        steps << Procedures::ForemanTasks::FetchTasksStatus.new(:state => 'running')
+        unless assumeyes?
+          steps << Procedures::ForemanTasks::UiInvestigate.new(
+            'search_query' => search_query_for_running_tasks
+          )
+        end
+      end
+      steps
     end
   end
 end
