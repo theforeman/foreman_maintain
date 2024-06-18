theforeman/foreman_maintain

commit f0e78f70b57fd357ba5c302709f23e45602d5b73

Fixes #36056 - Don't enable maintenance mode during offline backup

definitions/scenarios/backup.rb
@@ -81,15 +81,13 @@ module ForemanMaintain::Scenarios
       include_dumps if include_db_dumps?
       add_step_with_context(Procedures::ForemanProxy::Features, :load_only => true)
       add_steps_with_context(
-        find_procedures(:maintenance_mode_on),
         Procedures::Service::Stop,
         Procedures::Backup::ConfigFiles,
         Procedures::Backup::Pulp,
         Procedures::Backup::Offline::CandlepinDB,
         Procedures::Backup::Offline::ForemanDB,
         Procedures::Backup::Offline::PulpcoreDB,
-        Procedures::Service::Start,
-        find_procedures(:maintenance_mode_off)
+        Procedures::Service::Start
       )
     end
 

commit fd750bd9b5cfea8de9c65072413b53ef64f1e473

Don't disable maintenance mode in BackupRescue scenario
We don't enable maintenance mode anymore for backups, so we don't need
to disable it.

Fixes: f0e78f70b57fd357ba5c302709f23e45602d5b73

definitions/scenarios/backup.rb
@@ -136,7 +136,6 @@ module ForemanMaintain::Scenarios
     def compose
       if strategy == :offline
         add_step_with_context(Procedures::Service::Start)
-        add_steps_with_context(find_procedures(:maintenance_mode_off))
       end
       add_step_with_context(Procedures::Backup::Clean)
     end