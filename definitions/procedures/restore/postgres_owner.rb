theforeman/foreman_maintain

commit 1ae71c3ad827503eb370310a6b27232f80289351

Fixes #22886 - Add katello-restore (#169)

definitions/procedures/restore/postgres_owner.rb
@@ -1,0 +1,18 @@
+module Procedures::Restore
+  class PostgresOwner < ForemanMaintain::Procedure
+    include ForemanMaintain::Concerns::SystemHelpers
+    metadata do
+      description 'Make postgres owner of backup directory'
+
+      param :backup_dir,
+            'Path to backup directory',
+            :required => true
+    end
+
+    def run
+      if feature(:instance).foreman_proxy_with_content?
+        FileUtils.chown(nil, 'postgres', @backup_dir)
+      end
+    end
+  end
+end

definitions/checks/backup/directory_ready.rb
@@ -10,7 +10,7 @@ module Checks::Backup
 
     def run
       assert(File.directory?(@backup_dir), "Backup directory (#{@backup_dir}) does not exit.")
-      if local_psql_database?
+      if feature(:instance).postgresql_local?
         result = system("runuser - postgres -c 'test -w #{@backup_dir}'")
         assert(result, "Postgres user needs write access to the backup directory \n" \
           "Please allow the postgres user write access to #{@backup_dir}" \

definitions/checks/restore/validate_backup.rb
@@ -1,0 +1,77 @@
+require 'foreman_maintain/utils/backup'
+
+module Checks::Restore
+  class ValidateBackup < ForemanMaintain::Check
+    metadata do
+      description 'Validate backup has appropriate files'
+
+      param :backup_dir,
+            'Path to backup directory',
+            :required => true
+      manual_detection
+    end
+
+    def run
+      backup = ForemanMaintain::Utils::Backup.new(@backup_dir)
+      assert(backup.valid_backup?, valid_backup_message(backup))
+    end
+
+    def valid_backup_message(backup)
+      message = "\n"
+      message += "The given directory does not contain the required files or has too many files\n\n"
+      message += "All backup directories contain: #{backup.standard_files.join(', ')}\n"
+      message += required_files(backup)
+      message += 'Including pulp_data.tar is optional and '
+      message += "will restore pulp data to the filesystem if included.\n\n"
+      message += "Only the following files were found: #{backup.present_files.join(', ')}\n"
+      message
+    end
+
+    def required_files(backup)
+      message = ''
+      message += if feature(:instance).foreman_proxy_with_content?
+                   required_fpc_files(backup)
+                 elsif feature(:katello)
+                   required_katello_files(backup)
+                 else
+                   required_foreman_files(backup)
+                 end
+      message
+    end
+
+    def required_katello_files(backup)
+      backup_files_message(
+        backup.katello_online_files.join(', '),
+        backup.katello_offline_files.join(', '),
+        [backup.katello_online_files + backup.katello_offline_files].join(', ')
+      )
+    end
+
+    def required_fpc_files(backup)
+      backup_files_message(
+        backup.fpc_online_files.join(', '),
+        backup.fpc_offline_files.join(', '),
+        [backup.fpc_online_files + backup.fpc_offline_files].join(', ')
+      )
+    end
+
+    def required_foreman_files(backup)
+      backup_files_message(
+        backup.foreman_online_files.join(', '),
+        backup.foreman_offline_files.join(', '),
+        [backup.foreman_online_files + backup.foreman_offline_files].join(', ')
+      )
+    end
+
+    def backup_files_message(online_files, offline_files, logical_files)
+      message = ''
+      message += 'An online or remote database backup directory contains: '
+      message += "#{online_files}\n"
+      message += 'An offline backup directory contains: '
+      message += "#{offline_files}\n"
+      message += 'A logical backup directory contains: '
+      message += "#{logical_files}\n"
+      message
+    end
+  end
+end

definitions/checks/restore/validate_hostname.rb
@@ -1,0 +1,20 @@
+require 'foreman_maintain/utils/backup'
+
+module Checks::Restore
+  class ValidateHostname < ForemanMaintain::Check
+    metadata do
+      description 'Validate hostname is the same as backup'
+
+      param :backup_dir,
+            'Path to backup directory',
+            :required => true
+      manual_detection
+    end
+
+    def run
+      msg = 'The hostname in the backup does not match the hostname of the system'
+      backup = ForemanMaintain::Utils::Backup.new(@backup_dir)
+      assert(backup.validate_hostname?, msg)
+    end
+  end
+end

definitions/features/foreman_proxy.rb
@@ -17,6 +17,10 @@ class Features::ForemanProxy < ForemanMaintain::Feature
     dhcp_req_pass? && !syntax_error_exists?
   end
 
+  def with_content?
+    !!feature(:pulp)
+  end
+
   def services
     {
       'foreman-proxy'            => 20,

definitions/features/instance.rb
@@ -40,4 +40,8 @@ class Features::Instance < ForemanMaintain::Feature
   def postgresql_local?
     database_local?(:candlepin_database) || database_local?(:foreman_database)
   end
+
+  def foreman_proxy_with_content?
+    feature(:foreman_proxy) && feature(:foreman_proxy).with_content? && !feature(:katello)
+  end
 end

definitions/features/katello.rb
@@ -7,7 +7,6 @@ class Features::Katello < ForemanMaintain::Feature
     end
   end
 
-  # TODO: refactor to new features?
   def data_dirs
     @dirs ||= ['/var/lib/pulp', '/var/lib/mongodb', '/var/lib/pgsql']
   end

definitions/features/mongo.rb
@@ -87,6 +87,12 @@ class Features::Mongo < ForemanMaintain::Feature
              :hidden_patterns => [config['password']].compact)
   end
 
+  def restore(dir, config = configuration)
+    cmd = base_command(core.restore_command, config,
+                       "-d #{config['name']} #{File.join(dir, config['name'])}")
+    execute!(cmd, :hidden_patterns => [config['password']].compact)
+  end
+
   def dropdb(config = configuration)
     execute!(mongo_command("--eval 'db.dropDatabase()'", config),
              :hidden_patterns => [config['password']].compact)
@@ -114,7 +120,8 @@ class Features::Mongo < ForemanMaintain::Feature
         :archive => backup_file,
         :command => 'create',
         :exclude => ['mongod.lock'],
-        :transform => 's,^,var/lib/mongodb/,S'
+        :transform => 's,^,var/lib/mongodb/,S',
+        :files => '*'
       }.merge(extra_tar_options)
       feature(:tar).run(tar_options)
     end

commit 71cf8db78a9b04eda10da23ec7dd7e4f71ecdec4

drop unused PostgresOwner procedure
Fixes: 1ae71c3ad827503eb370310a6b27232f80289351

definitions/procedures/restore/postgres_owner.rb
@@ -1,18 +1,0 @@
-module Procedures::Restore
-  class PostgresOwner < ForemanMaintain::Procedure
-    include ForemanMaintain::Concerns::SystemHelpers
-    metadata do
-      description 'Make postgres owner of backup directory'
-
-      param :backup_dir,
-        'Path to backup directory',
-        :required => true
-    end
-
-    def run
-      if feature(:instance).foreman_proxy_with_content?
-        FileUtils.chown(nil, 'postgres', @backup_dir)
-      end
-    end
-  end
-end