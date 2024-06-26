theforeman/foreman_maintain

commit 041af1efe8ebef5682a04bf8ba0643f3bd995ca5

Combine Satellite and Capsule into a single scenario

definitions/scenarios/satellite_upgrade.rb
@@ -1,11 +1,10 @@
-module Scenarios::Satellite_6_16
+module Scenarios::Satellite
   class Abstract < ForemanMaintain::Scenario
     def self.upgrade_metadata(&block)
       metadata do
         tags :upgrade_scenario
         confine do
-          feature(:satellite) &&
-            (feature(:satellite).current_minor_version == '6.15' || \
+          (feature(:downstream).current_minor_version == '6.15' || \
             ForemanMaintain.upgrade_in_progress == '6.16')
         end
         instance_eval(&block)
@@ -19,7 +18,7 @@ module Scenarios::Satellite_6_16
 
   class PreUpgradeCheck < Abstract
     upgrade_metadata do
-      description 'Checks before upgrading to Satellite 6.16'
+      description 'Checks before upgrading'
       tags :pre_upgrade_checks
       run_strategy :fail_slow
     end
@@ -29,14 +28,14 @@ module Scenarios::Satellite_6_16
       add_steps(find_checks(:pre_upgrade))
       add_step(Checks::CheckIpv6Disable)
       add_step(Checks::Disk::AvailableSpacePostgresql13)
-      add_step(Checks::Repositories::Validate.new(:version => '6.16'))
+      add_step(Checks::Repositories::Validate.new(:version => target_version))
       add_step(Checks::CheckOrganizationContentAccessMode)
     end
   end
 
   class PreMigrations < Abstract
     upgrade_metadata do
-      description 'Procedures before migrating to Satellite 6.16'
+      description 'Procedures before migrating'
       tags :pre_migrations
     end
 
@@ -47,7 +46,7 @@ module Scenarios::Satellite_6_16
 
   class Migrations < Abstract
     upgrade_metadata do
-      description 'Migration scripts to Satellite 6.16'
+      description 'Migration scripts'
       tags :migrations
       run_strategy :fail_fast
     end
@@ -57,7 +56,7 @@ module Scenarios::Satellite_6_16
     end
 
     def compose
-      add_step(Procedures::Repositories::Setup.new(:version => '6.16'))
+      add_step(Procedures::Repositories::Setup.new(:version => target_version))
       if el8?
         modules_to_switch = ['postgresql:13']
         add_step(Procedures::Packages::SwitchModules.new(:module_names => modules_to_switch))
@@ -77,7 +76,7 @@ module Scenarios::Satellite_6_16
 
   class PostMigrations < Abstract
     upgrade_metadata do
-      description 'Procedures after migrating to Satellite 6.16'
+      description 'Procedures after migrating'
       tags :post_migrations
     end
 
@@ -90,7 +89,7 @@ module Scenarios::Satellite_6_16
 
   class PostUpgradeChecks < Abstract
     upgrade_metadata do
-      description 'Checks after upgrading to Satellite 6.16'
+      description 'Checks after upgrading'
       tags :post_upgrade_checks
       run_strategy :fail_slow
     end

definitions/procedures/installer/upgrade_rake_task.rb
@@ -2,12 +2,13 @@ module Procedures::Installer
   class UpgradeRakeTask < ForemanMaintain::Procedure
     metadata do
       description 'Execute upgrade:run rake task'
+      confine do
+        feature(:satellite)
+      end
     end
 
     def run
-      # only run this in the Satellite scenario, as in others
-      # the installer runs this rake task for us already
-      execute!('foreman-rake upgrade:run') if feature(:satellite)
+      execute!('foreman-rake upgrade:run')
     end
   end
 end

definitions/scenarios/upgrade_to_capsule_6_16.rb
@@ -1,102 +1,0 @@
-module Scenarios::Capsule_6_16
-  class Abstract < ForemanMaintain::Scenario
-    def self.upgrade_metadata(&block)
-      metadata do
-        tags :upgrade_scenario
-        confine do
-          feature(:capsule) &&
-            (feature(:capsule).current_minor_version == '6.15' || \
-            ForemanMaintain.upgrade_in_progress == '6.16')
-        end
-        instance_eval(&block)
-      end
-    end
-
-    def target_version
-      '6.16'
-    end
-  end
-
-  class PreUpgradeCheck < Abstract
-    upgrade_metadata do
-      description 'Checks before upgrading to Capsule 6.16'
-      tags :pre_upgrade_checks
-      run_strategy :fail_slow
-    end
-
-    def compose
-      add_steps(find_checks(:default))
-      add_steps(find_checks(:pre_upgrade))
-      add_step(Checks::CheckIpv6Disable)
-      add_step(Checks::Disk::AvailableSpacePostgresql13)
-      add_step(Checks::Repositories::Validate.new(:version => '6.16'))
-    end
-  end
-
-  class PreMigrations < Abstract
-    upgrade_metadata do
-      description 'Procedures before migrating to Capsule 6.16'
-      tags :pre_migrations
-    end
-
-    def compose
-      add_steps(find_procedures(:pre_migrations))
-    end
-  end
-
-  class Migrations < Abstract
-    upgrade_metadata do
-      description 'Migration scripts to Capsule 6.16'
-      tags :migrations
-    end
-
-    def set_context_mapping
-      context.map(:assumeyes, Procedures::Installer::Run => :assumeyes)
-    end
-
-    def compose
-      add_step(Procedures::Repositories::Setup.new(:version => '6.16'))
-      if el8?
-        modules_to_switch = ['postgresql:13']
-        add_step(Procedures::Packages::SwitchModules.new(:module_names => modules_to_switch))
-        modules_to_enable = ["satellite-capsule:#{el_short_name}"]
-        add_step(Procedures::Packages::EnableModules.new(:module_names => modules_to_enable))
-      end
-      add_step(Procedures::Packages::Update.new(
-        :assumeyes => true,
-        :download_only => true
-      ))
-      add_step(Procedures::Service::Stop.new)
-      add_step(Procedures::Packages::Update.new(:assumeyes => true, :clean_cache => false))
-      add_step_with_context(Procedures::Installer::Run)
-    end
-  end
-
-  class PostMigrations < Abstract
-    upgrade_metadata do
-      description 'Procedures after migrating to Capsule 6.16'
-      tags :post_migrations
-    end
-
-    def compose
-      add_step(Procedures::RefreshFeatures)
-      add_step(Procedures::Service::Start.new)
-      add_steps(find_procedures(:post_migrations))
-    end
-  end
-
-  class PostUpgradeChecks < Abstract
-    upgrade_metadata do
-      description 'Checks after upgrading to Capsule 6.16'
-      tags :post_upgrade_checks
-      run_strategy :fail_slow
-    end
-
-    def compose
-      add_steps(find_checks(:default))
-      add_steps(find_checks(:post_upgrade))
-      add_step(Procedures::Packages::CheckForReboot)
-      add_step(Procedures::Pulpcore::ContainerHandleImageMetadata)
-    end
-  end
-end

commit 45d5556494e626ccb56fc4a20d998ae57723d050

enable the correct module during the capsule upgrade
Fixes: 041af1efe8ebef5682a04bf8ba0643f3bd995ca5

definitions/features/capsule.rb
@@ -18,4 +18,8 @@ class Features::Capsule < ForemanMaintain::Feature
   def package_name
     'satellite-capsule'
   end
+
+  def module_name
+    'satellite-capsule'
+  end
 end

definitions/features/satellite.rb
@@ -22,6 +22,10 @@ class Features::Satellite < ForemanMaintain::Feature
     'satellite'
   end
 
+  def module_name
+    'satellite'
+  end
+
   private
 
   def version_from_source

definitions/scenarios/satellite_upgrade.rb
@@ -60,7 +60,7 @@ module Scenarios::Satellite
       if el8?
         modules_to_switch = ['postgresql:13']
         add_step(Procedures::Packages::SwitchModules.new(:module_names => modules_to_switch))
-        modules_to_enable = ["satellite:#{el_short_name}"]
+        modules_to_enable = ["#{feature(:instance).downstream.module_name}:#{el_short_name}"]
         add_step(Procedures::Packages::EnableModules.new(:module_names => modules_to_enable))
       end
       add_step(Procedures::Packages::Update.new(

test/definitions/scenarios/satellite_upgrade_test.rb
@@ -1,0 +1,39 @@
+require 'test_helper'
+
+module Scenarios
+  describe Scenarios::Satellite::Migrations do
+    include DefinitionsTestHelper
+
+    let(:scenario) do
+      Scenarios::Satellite::Migrations.new
+    end
+
+    it 'composes all steps for Satellite on EL8' do
+      assume_feature_present(:satellite)
+      Scenarios::Satellite::Migrations.any_instance.stubs(:el_major_version).returns(8)
+      assert_scenario_has_step(scenario, Procedures::Packages::EnableModules) do |step|
+        assert_equal(['satellite:el8'], step.options['module_names'])
+      end
+    end
+
+    it 'composes all steps for Satellite on EL9' do
+      assume_feature_present(:satellite)
+      Scenarios::Satellite::Migrations.any_instance.stubs(:el_major_version).returns(9)
+      refute_scenario_has_step(scenario, Procedures::Packages::EnableModules)
+    end
+
+    it 'composes all steps for Capsule on EL8' do
+      assume_feature_present(:capsule)
+      Scenarios::Satellite::Migrations.any_instance.stubs(:el_major_version).returns(8)
+      assert_scenario_has_step(scenario, Procedures::Packages::EnableModules) do |step|
+        assert_equal(['satellite-capsule:el8'], step.options['module_names'])
+      end
+    end
+
+    it 'composes all steps for Capsule on EL9' do
+      assume_feature_present(:capsule)
+      Scenarios::Satellite::Migrations.any_instance.stubs(:el_major_version).returns(9)
+      refute_scenario_has_step(scenario, Procedures::Packages::EnableModules)
+    end
+  end
+end