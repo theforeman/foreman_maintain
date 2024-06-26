7fc550da442500b1960368e278a3094e884bcff6
Evgeni Golov <evgeni@golov.de>
2024-06-26 11:13:38 +0200

    always use pg_dump for DB backups
    when restoring incremental backups, you can run in the situation where
    the on-disk representation of the database changes between the restore
    of the base and the increment, leading to a broken database.
    
    by using logical DB dumps, we ensure that the restore always work at the
    cost that increments are slightly bigger as they now always include a
    full DB dump
    
    this also allows simplification of the code as we now only have one type
    of DB backup to support

--- definitions/procedures/backup/offline/candlepin_db.rb
+++ definitions/procedures/backup/offline/candlepin_db.rb
@@ -6,47 +6,14 @@ module Procedures::Backup
         tags :backup
         label :backup_offline_candlepin_db
         for_feature :candlepin_database
-        preparation_steps { Checks::Candlepin::DBUp.new unless feature(:candlepin_database).local? }
+        preparation_steps { Checks::Candlepin::DBUp.new }
         param :backup_dir, 'Directory where to backup to', :required => true
-        param :tar_volume_size, 'Size of tar volume (indicates splitting)'
       end
 
       def run
-        if feature(:candlepin_database).local?
-          if File.exist?(pg_backup_file)
-            puts 'Already done'
-          else
-            local_backup
-          end
-        else
-          puts "Backup of #{pg_data_dir} is not supported for remote databases." \
-            ' Doing postgres dump instead...'
-          with_spinner('Getting Candlepin DB dump') do
-            feature(:candlepin_database).dump_db(File.join(@backup_dir, 'candlepin.dump'))
-          end
+        with_spinner('Getting Candlepin DB dump') do
+          feature(:candlepin_database).dump_db(File.join(@backup_dir, 'candlepin.dump'))
         end
-      end
-
-      private
-
-      def local_backup
-        with_spinner("Collecting data from #{pg_data_dir}") do
-          feature(:candlepin_database).backup_local(
-            pg_backup_file,
-            :listed_incremental => File.join(@backup_dir, '.postgres.snar'),
-            :volume_size => @tar_volume_size,
-            :data_dir => pg_data_dir,
-            :restore_dir => feature(:candlepin_database).data_dir
-          )
-        end
-      end
-
-      def pg_backup_file
-        File.join(@backup_dir, 'pgsql_data.tar')
-      end
-
-      def pg_data_dir
-        feature(:candlepin_database).data_dir
       end
     end
   end
--- test/definitions/checks/backup/incremental_parent_type_test.rb
+++ test/definitions/checks/backup/incremental_parent_type_test.rb
@@ -15,42 +15,9 @@ describe Checks::Backup::IncrementalParentType do
     end
   end
 
-  context 'for offline backup with local PostreSQL tarballs' do
+  context 'for offline backup with PostgreSQL dumps' do
     subject do
-      Checks::Backup::IncrementalParentType.new(:incremental_dir => '.', :online_backup => false,
-        :sql_tar => true)
-    end
-
-    it 'passes when existing backup is offline with tarballs' do
-      ForemanMaintain::Utils::Backup.any_instance.expects(:online_backup?).returns(false)
-      ForemanMaintain::Utils::Backup.any_instance.expects(:sql_tar_files_exist?).returns(true)
-      result = run_check(subject)
-      assert result.success?, 'Check expected to succeed'
-    end
-
-    it 'fails when existing backup is online' do
-      ForemanMaintain::Utils::Backup.any_instance.expects(:online_backup?).returns(true)
-      result = run_check(subject)
-      refute result.success?, 'Check expected to fail'
-      expected = 'The existing backup is an online backup, but an offline backup was requested.'
-      assert_equal expected, result.output
-    end
-
-    it 'fails when existing backup uses dumps' do
-      ForemanMaintain::Utils::Backup.any_instance.expects(:online_backup?).returns(false)
-      ForemanMaintain::Utils::Backup.any_instance.expects(:sql_tar_files_exist?).returns(false)
-      result = run_check(subject)
-      refute result.success?, 'Check expected to fail'
-      expected = 'The existing backup has PostgreSQL as a dump, '\
-        'but the new one will have a tarball.'
-      assert_equal expected, result.output
-    end
-  end
-
-  context 'for offline backup with remote PostgreSQL dumps' do
-    subject do
-      Checks::Backup::IncrementalParentType.new(:incremental_dir => '.', :online_backup => false,
-        :sql_tar => false)
+      Checks::Backup::IncrementalParentType.new(:incremental_dir => '.', :online_backup => false)
     end
 
     it 'passes when existing backup is offline' do
