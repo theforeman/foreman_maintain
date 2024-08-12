0c1dfc0cde0666f709d062f23e54ae8d4de74b85
ianballou <ianballou67@gmail.com>
2024-08-12 19:41:10 +0000

    Fix rubocop errors

--- test/definitions/checks/restore/validate_postgresql_dump_permissions_test.rb
+++ test/definitions/checks/restore/validate_postgresql_dump_permissions_test.rb
@@ -12,7 +12,8 @@ describe Checks::Restore::ValidatePostgresqlDumpPermissions do
       :foreman_dump => { :present => true, :path => '/nonexistant/foreman.dump' },
       :candlepin_dump => { :present => true, :path => '/nonexistant/candlepin.dump' },
       :pulpcore_dump => { :present => true, :path => '/nonexistant/pulpcore.dump' },
-      :container_gateway_dump => { :present => true, :path => '/nonexistant/container_gateway.dump' }
+      :container_gateway_dump =>
+        { :present => true, :path => '/nonexistant/container_gateway.dump' },
     }
     ForemanMaintain::Utils::Backup.any_instance.stubs(:file_map).returns(file_map)
   end
@@ -54,7 +55,8 @@ describe Checks::Restore::ValidatePostgresqlDumpPermissions do
     result = run_check(subject)
     refute result.success?, 'Check expected to fail'
     expected = "The 'postgres' user needs read access to the following files:\n" \
-      "/nonexistant/candlepin.dump\n/nonexistant/foreman.dump\n/nonexistant/pulpcore.dump\n/nonexistant/container_gateway.dump"
+      "/nonexistant/candlepin.dump\n/nonexistant/foreman.dump\n" \
+      "/nonexistant/pulpcore.dump\n/nonexistant/container_gateway.dump"
     assert_equal result.output, expected
   end
 
@@ -67,7 +69,8 @@ describe Checks::Restore::ValidatePostgresqlDumpPermissions do
       returns(true)
     subject.stubs(:system).with("runuser - postgres -c 'test -r /nonexistant/foreman.dump'").
       returns(true)
-    subject.stubs(:system).with("runuser - postgres -c 'test -r /nonexistant/container_gateway.dump'").
+    subject.stubs(:system).
+      with("runuser - postgres -c 'test -r /nonexistant/container_gateway.dump'").
       returns(true)
     result = run_check(subject)
     refute result.success?, 'Check expected to fail'
