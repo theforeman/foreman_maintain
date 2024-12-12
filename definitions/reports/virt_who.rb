83d4ebe315a2394c52a7fce2cafaadd340839c64
Adam Ruzicka <aruzicka@redhat.com>
2024-12-12 10:45:29 +0100

    Address review comments

--- definitions/reports/virt_who.rb
+++ definitions/reports/virt_who.rb
@@ -1,18 +1,22 @@
 module Checks
-    module Report
-      class Virtwho < ForemanMaintain::Report
-        metadata do
-          description 'Check if virt-who is being used and what hypervisor types are present'
-        end
-  
-        def run
-          count = sql_count("SELECT COUNT(*) FROM foreman_virt_who_configure_configs")
-          hypervisor_types = feature(:foreman_database).query("SELECT hypervisor_type FROM foreman_virt_who_configure_configs")
-          self.data = { "virt_who_configurations": count,
-                        "Hypervisor types": hypervisor_types
-                      }
+  module Report
+    class Virtwho < ForemanMaintain::Report
+      metadata do
+        description 'Check if virt-who is being used and what hypervisor types are present'
+      end
+
+      HYPERVISOR_TYPES = %w[ahv esx fakevirt hyperv kubevirt libvirtd].freeze
+
+      def run
+        self.data = {}
+        data['foreman_virt_who_configure_configurations_count'] =
+          sql_count('foreman_virt_who_configure_configs')
+
+        HYPERVISOR_TYPES.each do |type|
+          data["foreman_virt_who_configure_#{type}_count"] =
+            sql_count("foreman_virt_who_configure_configs WHERE hypervisor_type = '#{type}'")
         end
       end
     end
   end
-  
+end
