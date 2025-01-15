58f330a7af49ccdcd5644fbdca77eace6dc64188
Adam Ruzicka <aruzicka@redhat.com>
2025-01-15 04:56:56 -0500

    Optionally save generated report to file

--- lib/foreman_maintain/cli/report_command.rb
+++ lib/foreman_maintain/cli/report_command.rb
@@ -3,6 +3,7 @@ module ForemanMaintain
     class ReportCommand < Base
       extend Concerns::Finders
 
+      option '--output', 'FILE', 'Output the generate report into FILE'
       subcommand 'generate', 'Generates the usage reports' do
         def execute
           scenario = run_scenario(Scenarios::Report::Generate.new({}, [:reports])).first
@@ -10,7 +11,12 @@ module ForemanMaintain
           # description can be used too
           report_data = scenario.steps.map(&:data).compact.reduce(&:merge).transform_keys(&:to_s)
           report_data['version'] = 1
-          puts report_data.to_yaml
+          yaml = report_data.to_yaml
+          if @output
+            File.write(@output, yaml)
+          else
+            puts yaml
+          end
           exit runner.exit_code
         end
       end
