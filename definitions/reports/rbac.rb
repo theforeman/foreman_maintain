f254e9534ff1e652bfaaa2c444b3b60f5275f5bd
Adam Ruzicka <aruzicka@redhat.com>
2025-01-28 08:38:24 -0500

    Make rubocop happy about rbac report

--- definitions/reports/rbac.rb
+++ definitions/reports/rbac.rb
@@ -4,56 +4,65 @@ module Reports
       description 'Checks the RBAC use'
     end
 
+    TAXONOMY_TYPES = %w[Organization Location].freeze
+
     # How many users do you have in the system?
     # How many non-admin users do you have?
     # How many custom roles did you create and assigned to users?
-    # rubocop:disable Layout/LineLength
-    # rubocop:disable Metrics/MethodLength
-    # rubocop:disable Metrics/AbcSize
     def run
-      result = {}
+      self.data = {}
 
-      count = sql_count("users" +
+      data.merge!(user_counts)
+      data.merge!(custom_roles_counts)
+      data.merge!(taxonomy_counts)
+      data.merge!(taxonomy_ignore_type_uses)
+    end
+
+    def user_counts
+      users = sql_count("users" +
                         " INNER JOIN auth_sources ON auth_sources.id = users.auth_source_id" +
                         " WHERE auth_sources.name != 'Hidden'")
-      result["users_count"] = count
 
-      count = sql_count("users" +
-        " LEFT OUTER JOIN cached_usergroup_members ON cached_usergroup_members.user_id = users.id" +
+      non_admin_users = sql_count("users" +
+        " LEFT OUTER JOIN cached_usergroup_members" +
+        "   ON cached_usergroup_members.user_id = users.id" +
         " LEFT OUTER JOIN usergroups ON usergroups.id = cached_usergroup_members.usergroup_id" +
         " INNER JOIN auth_sources ON auth_sources.id = users.auth_source_id" +
-        " WHERE ((users.admin = FALSE OR users.admin IS NULL) AND (usergroups.admin = FALSE OR usergroups.admin IS NULL))" +
+        " WHERE ((users.admin = FALSE OR users.admin IS NULL)" +
+        "        AND (usergroups.admin = FALSE OR usergroups.admin IS NULL))" +
         " AND auth_sources.name != 'Hidden'")
-      result["non_admin_users_count"] = count
 
-      role_ids = feature(:foreman_database).query("SELECT id FROM roles WHERE roles.builtin != 2 AND roles.origin IS NULL")
-      result["custom_roles_count"] = role_ids.size
-      role_ids = role_ids.flat_map(&:values)
-      count = sql_count("cached_user_roles WHERE cached_user_roles.role_id IN (#{role_ids.join(',')})")
-      result["assigned_custom_roles_count"] = count
-
-      count = sql_count("taxonomies" +
-                          " WHERE taxonomies.type = 'Organization'")
-      result["organizations_count"] = count
-
-      count = sql_count("taxonomies" +
-                          " WHERE taxonomies.type = 'Location'")
-      result["locations_count"] = count
-
-      count = sql_count("taxonomies" +
-                          " WHERE taxonomies.type = 'Organization'" +
-                          " AND taxonomies.ignore_types IS NOT NULL")
-      result["organization_ignore_types_used"] = count > 0
-
-      count = sql_count("taxonomies" +
-                          " WHERE taxonomies.type = 'Location'" +
-                          " AND taxonomies.ignore_types IS NOT NULL")
-      result["location_ignore_types_used"] = count > 0
-
-      self.data = result
+      { 'users_count' => users, 'non_admin_users_count' => non_admin_users }
     end
-    # rubocop:enable Layout/LineLength
-    # rubocop:enable Metrics/MethodLength
-    # rubocop:enable Metrics/AbcSize
+
+    def custom_roles_counts
+      role_ids = query("SELECT id FROM roles WHERE roles.builtin != 2 AND roles.origin IS NULL")
+      roles_count = role_ids.size
+      role_ids = role_ids.flat_map(&:values)
+      assigned_count = if role_ids.empty?
+                         0
+                       else
+                         sql = "cached_user_roles" +
+                               "WHERE cached_user_roles.role_id IN (#{role_ids.join(',')})"
+                         sql_count(sql)
+                       end
+
+      { 'custom_roles_count' => roles_count, 'assigned_custom_roles_count' => assigned_count }
+    end
+
+    def taxonomy_counts
+      TAXONOMY_TYPES.to_h do |t|
+        ["#{t.downcase}s_count", sql_count("taxonomies WHERE type = '#{t}'")]
+      end
+    end
+
+    def taxonomy_ignore_type_uses
+      TAXONOMY_TYPES.to_h do |t|
+        count = sql_count("taxonomies" +
+                          " WHERE type = '#{t}'" +
+                          "   AND ignore_types IS NOT NULL")
+        ["#{t.downcase}_ignore_types_used", count.positive?]
+      end
+    end
   end
 end
