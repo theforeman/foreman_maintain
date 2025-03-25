39cc1d6aa39c944c6be8e79198aa1a998d815ab4
Ian Ballou <ianballou67@gmail.com>
2025-03-25 17:52:01 -0400

    DRY up the content.rb report

--- definitions/reports/content.rb
+++ definitions/reports/content.rb
@@ -11,67 +11,43 @@ module Checks
       def run
         data_field('custom_library_yum_repositories_count') { custom_library_yum_repositories }
         data_field('redhat_library_yum_repositories_count') { redhat_library_yum_repositories }
-        data_field('library_debian_repositories_count') { library_debian_repositories }
-        data_field('library_container_repositories_count') { library_container_repositories }
-        data_field('library_file_repositories_count') { library_file_repositories }
-        data_field('library_python_repositories_count') { library_python_repositories }
+        data_field('library_debian_repositories_count') { library_repositories('deb') }
+        data_field('library_container_repositories_count') { library_repositories('docker') }
+        data_field('library_file_repositories_count') { library_repositories('file') }
+        data_field('library_python_repositories_count') { library_repositories('python') }
         data_field('library_ansible_collection_repositories_count') do
-          library_ansible_collection_repositories
+          library_repositories('ansible_collection')
         end
-        data_field('library_ostree_repositories_count') { library_ostree_repositories }
+        data_field('library_ostree_repositories_count') { library_repositories('ostree') }
       end
 
       def custom_library_yum_repositories
-        query =
-          query(
-            <<-SQL
-              SELECT count(*) as yum_custom_count FROM "katello_root_repositories"
-                WHERE "katello_root_repositories"."id" NOT IN
-                (SELECT "katello_root_repositories"."id" FROM "katello_root_repositories" INNER JOIN "katello_products"
-                  ON "katello_products"."id" = "katello_root_repositories"."product_id" INNER JOIN "katello_providers"
-                  ON "katello_providers"."id" = "katello_products"."provider_id" WHERE "katello_providers"."provider_type" = 'Red Hat')
-                AND "katello_root_repositories"."content_type" = 'yum'
-            SQL
-          )
-        query.first['yum_custom_count'].to_i
+        query_snippet =
+          <<-SQL
+            "katello_root_repositories"
+              WHERE "katello_root_repositories"."id" NOT IN
+              (SELECT "katello_root_repositories"."id" FROM "katello_root_repositories" INNER JOIN "katello_products"
+                ON "katello_products"."id" = "katello_root_repositories"."product_id" INNER JOIN "katello_providers"
+                ON "katello_providers"."id" = "katello_products"."provider_id" WHERE "katello_providers"."provider_type" = 'Red Hat')
+              AND "katello_root_repositories"."content_type" = 'yum'
+          SQL
+        sql_count(query_snippet)
       end
 
       def redhat_library_yum_repositories
-        query =
-          query(
-            <<-SQL
-              SELECT count(*) as yum_rh_count FROM "katello_root_repositories"
-                INNER JOIN "katello_products" ON "katello_products"."id" = "katello_root_repositories"."product_id"
-                INNER JOIN "katello_providers" ON "katello_providers"."id" = "katello_products"."provider_id"
-                  WHERE "katello_providers"."provider_type" = 'Red Hat'
-                AND "katello_root_repositories"."content_type" = 'yum'
-            SQL
-          )
-        query.first['yum_rh_count'].to_i
+        query_snippet =
+          <<-SQL
+            "katello_root_repositories"
+              INNER JOIN "katello_products" ON "katello_products"."id" = "katello_root_repositories"."product_id"
+              INNER JOIN "katello_providers" ON "katello_providers"."id" = "katello_products"."provider_id"
+                WHERE "katello_providers"."provider_type" = 'Red Hat'
+              AND "katello_root_repositories"."content_type" = 'yum'
+          SQL
+        sql_count(query_snippet)
       end
 
-      def library_container_repositories
-        sql_count("katello_root_repositories WHERE content_type = 'docker'")
-      end
-
-      def library_ostree_repositories
-        sql_count("katello_root_repositories WHERE content_type = 'ostree'")
-      end
-
-      def library_ansible_collection_repositories
-        sql_count("katello_root_repositories WHERE content_type = 'ansible_collection'")
-      end
-
-      def library_file_repositories
-        sql_count("katello_root_repositories WHERE content_type = 'file'")
-      end
-
-      def library_python_repositories
-        sql_count("katello_root_repositories WHERE content_type = 'python'")
-      end
-
-      def library_debian_repositories
-        sql_count("katello_root_repositories WHERE content_type = 'deb'")
+      def library_repositories(content_type)
+        sql_count("katello_root_repositories WHERE content_type = '#{content_type}'")
       end
     end
   end
