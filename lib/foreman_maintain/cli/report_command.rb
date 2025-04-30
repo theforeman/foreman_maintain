e879b9e0d14bcca575dab12d78d8d1f55e6edabd
Adam Ruzicka <aruzicka@redhat.com>
2025-04-30 11:29:57 +0200

    Make rubocop happy

--- lib/foreman_maintain/cli/report_command.rb
+++ lib/foreman_maintain/cli/report_command.rb
@@ -37,10 +37,10 @@ module ForemanMaintain
       subcommand 'condense', 'Condense the report' do
         def execute
           data = if fresh_enough?(@input, @max_age)
-            YAML.load_file(@input)
-          else
-            generate_report
-          end
+                   YAML.load_file(@input)
+                 else
+                   generate_report
+                 end
 
           report = condense_report(data)
           report = prefix_keys(report)
@@ -49,8 +49,9 @@ module ForemanMaintain
 
         def condense_report(data)
           result = {}
-          result['advisor_on_prem_remediations_count'] = data['advisor_on_prem_remediations_count'] || 0
-          result['rhel_ai_workload_host_count'] = data['rhel_ai_workload_host_count'] || 0
+          %w[advisor_on_prem_remediations_count rhel_ai_workload_host_count].each do |key|
+            result[key] = data[key] || 0
+          end
           result.merge!(aggregate_host_count(data))
           result.merge!(aggregate_image_mode_host_count(data))
           result.merge!(aggregate_networking_metrics(data))
@@ -67,7 +68,9 @@ module ForemanMaintain
           rh_count = data['hosts_by_family_count|Redhat'] || 0
           result['host_rhel_count'] = rhel_count
           result['host_redhat_count'] = rh_count - rhel_count
-          result['host_other_count'] = data.select { |k, _| k.start_with?('hosts_by_os_count') }.values.sum - rhel_count - rh_count
+          result['host_other_count'] = data.select do |k, _|
+            k.start_with?('hosts_by_os_count')
+          end.values.sum - rhel_count - rh_count
           result
         end
 
@@ -77,20 +80,36 @@ module ForemanMaintain
         end
 
         def aggregate_networking_metrics(data)
-          ipv6 = data.values_at('subnet_ipv6_count', 'hosts_with_ipv6only_interface_count', 'foreman_interfaces_ipv6only_count').map { |v| v || 0 }.any?(&:positive?)
-
+          ipv6 = any_positive?(data, %w[subnet_ipv6_count hosts_with_ipv6only_interface_count
+                                        foreman_interfaces_ipv6only_count])
           # Deployment is considered to run in dualstack mode if:
           # - Foreman has both ipv6 and ipv4 addresses on a single interface
           # - or if any host in Foreman has both ipv6 and ipv4 addresses on a single interface
-          dualstack = data.values_at('hosts_with_dualstack_interface_count', 'foreman_interfaces_dualstack_count').map { |v| v || 0 }.any?(&:positive?)
+          dualstack = any_positive?(data, %w[hosts_with_dualstack_interface_count
+                                             foreman_interfaces_dualstack_count])
+
           # - or if there are both ipv4 and ipv6 subnets defined
-          dualstack |= data.values_at('subnet_ipv4_count', 'subnet_ipv6_count').map { |v| v || 0 }.all?(&:positive?)
-          # - or if any host in Foreman has an interface with only an ipv4 address as well as another interface with ipv6 address
-          dualstack |= data.values_at('hosts_with_ipv4only_interface_count', 'hosts_with_ipv6only_interface_count').map { |v| v || 0 }.all?(&:positive?)
-          # - Foreman has an interface with only an ipv4 address as well as another interface with ipv6 address
-          dualstack |= data.values_at('foreman_interfaces_ipv4only_count', 'foreman_interfaces_ipv6only_count').map { |v| v || 0 }.all?(&:positive?)
+          dualstack |= all_positive?(data, %w[subnet_ipv4_count subnet_ipv6_count])
+
+          # - or if any host in Foreman has an interface with only an ipv4 address
+          #   as well as another interface with ipv6 address
+          dualstack |= all_positive?(data, %w[hosts_with_ipv4only_interface_count
+                                              hosts_with_ipv6only_interface_count])
+
+          # - or if Foreman has an interface with only an ipv4 address
+          #   as well as another interface with ipv6 address
+          dualstack |= all_positive?(data,
+            %w[foreman_interfaces_ipv4only_count foreman_interfaces_ipv6only_count])
 
           { 'use_dualstack' => dualstack, 'use_ipv6' => ipv6 }
+        end
+
+        def all_positive?(source, keys)
+          source.values_at(*keys).map { |x| x || 0 }.all?(&:positive?)
+        end
+
+        def any_positive?(source, keys)
+          source.values_at(*keys).map { |x| x || 0 }.any?(&:positive?)
         end
 
         def prefix_keys(data)
@@ -98,7 +117,7 @@ module ForemanMaintain
         end
 
         def fresh_enough?(input, max_age)
-          @input && File.exists?(input) &&
+          @input && File.exist?(input) &&
             (@max_age.nil? || (Time.now - File.stat(input).mtime <= 60 * 60 * max_age.to_i))
         end
       end
