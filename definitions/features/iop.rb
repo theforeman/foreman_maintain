c93ba5366503331e413d7c90b563c7f62fbe684c
Evgeni Golov <evgeni@golov.de>
2025-06-30 15:37:58 +0200

    update list of iop services

--- definitions/features/iop.rb
+++ definitions/features/iop.rb
@@ -20,13 +20,20 @@ class Features::Iop < ForemanMaintain::Feature
       system_service('iop-core-gateway', 20),
       system_service('iop-core-host-inventory-migrate', 20),
       system_service('iop-core-host-inventory', 20),
-      system_service('iop-core-host-inventory-web', 20),
+      system_service('iop-core-host-inventory-api', 20),
       system_service('iop-core-ingress', 20),
       system_service('iop-core-kafka', 20),
       system_service('iop-core-puptoo', 20),
       system_service('iop-core-yuptoo', 20),
       system_service('iop-service-vmaas-reposcan', 20),
       system_service('iop-service-vmaas-webapp-go', 20),
+      system_service('iop-service-vuln-manager', 20),
+      system_service('iop-service-vuln-taskomatic', 20),
+      system_service('iop-service-vuln-grouper', 20),
+      system_service('iop-service-vuln-listener', 20),
+      system_service('iop-service-vuln-evaluator-recalc', 20),
+      system_service('iop-service-vuln-evaluator-upload', 20),
+      system_service('iop-service-vuln-vmaas-sync', 20),
     ]
   end
 end
