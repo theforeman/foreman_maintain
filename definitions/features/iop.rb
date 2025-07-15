037dc69047a0d1438c5489c55f3c35e57de02539
Evgeni Golov <evgeni@golov.de>
2025-07-15 07:42:13 +0200

    add new iop services

--- definitions/features/iop.rb
+++ definitions/features/iop.rb
@@ -15,25 +15,32 @@ class Features::Iop < ForemanMaintain::Feature
     ]
   end
 
+  # rubocop:disable Metrics/MethodLength
   def services
     [
+      system_service('iop-core-engine', 20),
       system_service('iop-core-gateway', 20),
-      system_service('iop-core-host-inventory-migrate', 20),
       system_service('iop-core-host-inventory', 20),
       system_service('iop-core-host-inventory-api', 20),
+      system_service('iop-core-host-inventory-migrate', 20),
       system_service('iop-core-ingress', 20),
       system_service('iop-core-kafka', 20),
       system_service('iop-core-puptoo', 20),
       system_service('iop-core-yuptoo', 20),
+      system_service('iop-service-advisor-backend-api', 20),
+      system_service('iop-service-advisor-backend-service', 20),
+      system_service('iop-service-remediations-api', 20),
       system_service('iop-service-vmaas-reposcan', 20),
       system_service('iop-service-vmaas-webapp-go', 20),
-      system_service('iop-service-vuln-manager', 20),
-      system_service('iop-service-vuln-taskomatic', 20),
-      system_service('iop-service-vuln-grouper', 20),
-      system_service('iop-service-vuln-listener', 20),
+      system_service('iop-service-vuln-dbupgrade', 20),
       system_service('iop-service-vuln-evaluator-recalc', 20),
       system_service('iop-service-vuln-evaluator-upload', 20),
+      system_service('iop-service-vuln-grouper', 20),
+      system_service('iop-service-vuln-listener', 20),
+      system_service('iop-service-vuln-manager', 20),
+      system_service('iop-service-vuln-taskomatic', 20),
       system_service('iop-service-vuln-vmaas-sync', 20),
     ]
   end
+  # rubocop:enable Metrics/MethodLength
 end
