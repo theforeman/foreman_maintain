bb3aaf00b009b63bd191e70aff1bd407fdc4aac5
Evgeni Golov <evgeni@golov.de>
2025-07-17 11:17:28 +0200

    also upgrade containers during upgrade

--- definitions/scenarios/satellite_upgrade.rb
+++ definitions/scenarios/satellite_upgrade.rb
@@ -103,6 +103,7 @@ module Scenarios::Satellite
           :assumeyes => true,
           :download_only => true
         ),
+        Procedures::Iop::Update,
         Procedures::Service::Stop,
         Procedures::Packages::Update.new(:assumeyes => true, :clean_cache => false),
       )
