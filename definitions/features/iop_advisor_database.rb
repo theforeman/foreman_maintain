theforeman/foreman_maintain

commit 9fad99038eca358152c3e124c42ae2db8f2af8bd

add iop database definition

definitions/features/iop_advisor_database.rb
@@ -1,0 +1,45 @@
+class Features::IopAdvisorDatabase < ForemanMaintain::Feature
+  include ForemanMaintain::Concerns::BaseDatabase
+
+  metadata do
+    label :iop_advisor_database
+
+    confine do
+      File.exist?('/etc/containers/networks/iop-core-network.json') ||
+        File.exist?('/etc/containers/systemd/iop-core.network')
+    end
+  end
+
+  def configuration
+    @configuration || load_configuration
+  end
+
+  def services
+    [
+      system_service('postgresql', 10, :component => 'iop',
+        :db_feature => feature(:iop_advisor_database)),
+    ]
+  end
+
+  private
+
+  def load_configuration
+    podman_command = "podman exec iop-service-advisor-api bash -c 'env |grep DB_'"
+    podman_result = execute!(podman_command, merge_stderr: false).lines.map do |l|
+      l.strip.split('=')
+    end.to_h
+
+    db_host = if podman_result['ADVISOR_DB_HOST'].start_with?('/var/run/postgresql')
+                'localhost'
+              else
+                podman_result['ADVISOR_DB_HOST']
+              end
+    @configuration = {
+      'host' => db_host,
+      'port' => podman_result['ADVISOR_DB_PORT'],
+      'database' => podman_result['ADVISOR_DB_NAME'],
+      'password' => podman_result['ADVISOR_DB_PASSWORD'],
+      'username' => podman_result['ADVISOR_DB_USER'],
+    }
+  end
+end

definitions/checks/iop_advisor/db_up.rb
@@ -1,0 +1,33 @@
+module Checks
+  module IopAdvisor
+    class DBUp < ForemanMaintain::Check
+      metadata do
+        description 'Make sure IoP Advisor DB is up'
+        label :iop_advisor_db_up
+        for_feature :iop_advisor
+      end
+
+      def run
+        status = false
+        if feature(:iop_advisor_database).psql_cmd_available?
+          with_spinner('Checking connection to the IoP Advisor DB') do
+            status = feature(:iop_advisor_database).ping
+          end
+          assert(status, 'IoP Advisor DB is not responding. ' \
+            'It needs to be up and running to perform the following steps',
+            :next_steps => start_pgsql)
+        else
+          feature(:iop_advisor_database).raise_psql_missing_error
+        end
+      end
+
+      def start_pgsql
+        if feature(:iop_advisor_database).local?
+          [Procedures::Service::Start.new(:only => 'postgresql')]
+        else
+          [] # there is nothing we can do for remote db
+        end
+      end
+    end
+  end
+end

definitions/checks/iop_inventory/db_up.rb
@@ -1,0 +1,33 @@
+module Checks
+  module IopInventory
+    class DBUp < ForemanMaintain::Check
+      metadata do
+        description 'Make sure IoP Inventory DB is up'
+        label :iop_inventory_db_up
+        for_feature :iop_inventory
+      end
+
+      def run
+        status = false
+        if feature(:iop_inventory_database).psql_cmd_available?
+          with_spinner('Checking connection to the IoP Inventory DB') do
+            status = feature(:iop_inventory_database).ping
+          end
+          assert(status, 'IoP Inventory DB is not responding. ' \
+            'It needs to be up and running to perform the following steps',
+            :next_steps => start_pgsql)
+        else
+          feature(:iop_inventory_database).raise_psql_missing_error
+        end
+      end
+
+      def start_pgsql
+        if feature(:iop_inventory_database).local?
+          [Procedures::Service::Start.new(:only => 'postgresql')]
+        else
+          [] # there is nothing we can do for remote db
+        end
+      end
+    end
+  end
+end

definitions/checks/iop_remediations/db_up.rb
@@ -1,0 +1,33 @@
+module Checks
+  module IopRemediations
+    class DBUp < ForemanMaintain::Check
+      metadata do
+        description 'Make sure IoP Remediations DB is up'
+        label :iop_remediations_db_up
+        for_feature :iop_remediations
+      end
+
+      def run
+        status = false
+        if feature(:iop_remediations_database).psql_cmd_available?
+          with_spinner('Checking connection to the IoP Remediations DB') do
+            status = feature(:iop_remediations_database).ping
+          end
+          assert(status, 'IoP Remediations DB is not responding. ' \
+            'It needs to be up and running to perform the following steps',
+            :next_steps => start_pgsql)
+        else
+          feature(:iop_remediations_database).raise_psql_missing_error
+        end
+      end
+
+      def start_pgsql
+        if feature(:iop_remediations_database).local?
+          [Procedures::Service::Start.new(:only => 'postgresql')]
+        else
+          [] # there is nothing we can do for remote db
+        end
+      end
+    end
+  end
+end

definitions/checks/iop_vmaas/db_up.rb
@@ -1,0 +1,33 @@
+module Checks
+  module IopVmaas
+    class DBUp < ForemanMaintain::Check
+      metadata do
+        description 'Make sure IoP Vmaas DB is up'
+        label :iop_vmaas_db_up
+        for_feature :iop_vmaas
+      end
+
+      def run
+        status = false
+        if feature(:iop_vmaas_database).psql_cmd_available?
+          with_spinner('Checking connection to the IoP Vmaas DB') do
+            status = feature(:iop_vmaas_database).ping
+          end
+          assert(status, 'IoP Vmaas DB is not responding. ' \
+            'It needs to be up and running to perform the following steps',
+            :next_steps => start_pgsql)
+        else
+          feature(:iop_vmaas_database).raise_psql_missing_error
+        end
+      end
+
+      def start_pgsql
+        if feature(:iop_vmaas_database).local?
+          [Procedures::Service::Start.new(:only => 'postgresql')]
+        else
+          [] # there is nothing we can do for remote db
+        end
+      end
+    end
+  end
+end

definitions/features/iop_inventory_database.rb
@@ -1,0 +1,45 @@
+class Features::IopInventoryDatabase < ForemanMaintain::Feature
+  include ForemanMaintain::Concerns::BaseDatabase
+
+  metadata do
+    label :iop_inventory_database
+
+    confine do
+      File.exist?('/etc/containers/networks/iop-core-network.json') ||
+        File.exist?('/etc/containers/systemd/iop-core.network')
+    end
+  end
+
+  def configuration
+    @configuration || load_configuration
+  end
+
+  def services
+    [
+      system_service('postgresql', 10, :component => 'iop',
+        :db_feature => feature(:iop_inventory_database)),
+    ]
+  end
+
+  private
+
+  def load_configuration
+    podman_command = "podman exec iop-core-host-inventory bash -c 'env |grep INVENTORY_DB_'"
+    podman_result = execute!(podman_command, merge_stderr: false).lines.map do |l|
+      l.strip.split('=')
+    end.to_h
+
+    db_host = if podman_result['INVENTORY_DB_HOST'].start_with?('/var/run/postgresql')
+                'localhost'
+              else
+                podman_result['INVENTORY_DB_HOST']
+              end
+    @configuration = {
+      'host' => db_host,
+      'port' => podman_result['INVENTORY_DB_PORT'],
+      'database' => podman_result['INVENTORY_DB_NAME'],
+      'password' => podman_result['INVENTORY_DB_PASS'],
+      'username' => podman_result['INVENTORY_DB_USER'],
+    }
+  end
+end

definitions/features/iop_remediations_database.rb
@@ -1,0 +1,45 @@
+class Features::IopRemediationsDatabase < ForemanMaintain::Feature
+  include ForemanMaintain::Concerns::BaseDatabase
+
+  metadata do
+    label :iop_remediations_database
+
+    confine do
+      File.exist?('/etc/containers/networks/iop-core-network.json') ||
+        File.exist?('/etc/containers/systemd/iop-core.network')
+    end
+  end
+
+  def configuration
+    @configuration || load_configuration
+  end
+
+  def services
+    [
+      system_service('postgresql', 10, :component => 'iop',
+        :db_feature => feature(:iop_remediations_database)),
+    ]
+  end
+
+  private
+
+  def load_configuration
+    podman_command = "podman exec iop-service-remediations-api bash -c 'env |grep DB_'"
+    podman_result = execute!(podman_command, merge_stderr: false).lines.map do |l|
+      l.strip.split('=')
+    end.to_h
+
+    db_host = if podman_result['DB_HOST'].start_with?('/var/run/postgresql')
+                'localhost'
+              else
+                podman_result['DB_HOST']
+              end
+    @configuration = {
+      'host' => db_host,
+      'port' => podman_result['DB_PORT'],
+      'database' => podman_result['DB_DATABASE'],
+      'password' => podman_result['DB_PASSWORD'],
+      'username' => podman_result['DB_USERNAME'],
+    }
+  end
+end

definitions/features/iop_vmaas_database.rb
@@ -1,0 +1,45 @@
+class Features::IopVmaasDatabase < ForemanMaintain::Feature
+  include ForemanMaintain::Concerns::BaseDatabase
+
+  metadata do
+    label :iop_vmaas_database
+
+    confine do
+      File.exist?('/etc/containers/networks/iop-core-network.json') ||
+        File.exist?('/etc/containers/systemd/iop-core.network')
+    end
+  end
+
+  def configuration
+    @configuration || load_configuration
+  end
+
+  def services
+    [
+      system_service('postgresql', 10, :component => 'iop',
+        :db_feature => feature(:iop_vmaas_database)),
+    ]
+  end
+
+  private
+
+  def load_configuration
+    podman_command = "podman exec iop-service-vmaas-reposcan bash -c 'env |grep POSTGRESQL_'"
+    podman_result = execute!(podman_command, merge_stderr: false).lines.map do |l|
+      l.strip.split('=')
+    end.to_h
+
+    db_host = if podman_result['POSTGRESQL_HOST'].start_with?('/var/run/postgresql')
+                'localhost'
+              else
+                podman_result['POSTGRESQL_HOST']
+              end
+    @configuration = {
+      'host' => db_host,
+      'port' => podman_result['POSTGRESQL_PORT'],
+      'database' => podman_result['POSTGRESQL_DATABASE'],
+      'password' => podman_result['POSTGRESQL_PASSWORD'],
+      'username' => podman_result['POSTGRESQL_USER'],
+    }
+  end
+end

commit 66c011f160935b8ca253ec8ce7976717d2973162

correct container name of advisor backend
Fixes: 9fad99038eca358152c3e124c42ae2db8f2af8bd

definitions/features/iop_advisor_database.rb
@@ -24,7 +24,7 @@ class Features::IopAdvisorDatabase < ForemanMaintain::Feature
   private
 
   def load_configuration
-    podman_command = "podman exec iop-service-advisor-api bash -c 'env |grep DB_'"
+    podman_command = "podman exec iop-service-advisor-backend-api bash -c 'env |grep DB_'"
     podman_result = execute!(podman_command, merge_stderr: false).lines.map do |l|
       l.strip.split('=')
     end.to_h