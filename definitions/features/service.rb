theforeman/foreman_maintain

commit 5c2a705e18a9ec26d5461cdd7e1b211cd06d9971

Drop old Satellite version conditionals

definitions/features/service.rb
@@ -8,11 +8,7 @@ class Features::Service < ForemanMaintain::Feature
     # { :only => ["httpd"] }
     # { :exclude => ["pulp-workers", "tomcat"] }
     # { :include => ["crond"] }
-    if feature(:instance).downstream&.less_than_version?('6.3')
-      use_katello_service(action, options)
-    else
-      use_system_service(action, options, spinner)
-    end
+    use_system_service(action, options, spinner)
   end
 
   def existing_services
@@ -180,44 +176,7 @@ class Features::Service < ForemanMaintain::Feature
     end
   end
 
-  def use_katello_service(action, options)
-    if %w[enable disable].include?(action)
-      raise 'Service enable and disable are only supported in Satellite 6.3+'
-    end
-
-    command = "katello-service #{action} "
-
-    # katello-service in 6.1 does not support --only
-    if feature(:instance).downstream.less_than_version?('6.2')
-      excluded_services = exclude_services_only(options)
-      command += "--exclude #{excluded_services.join(',')}" if excluded_services.any?
-    else
-      command += katello_service_filters(options)
-    end
-
-    run_katello_service(command)
-  end
-
-  def run_katello_service(command)
-    puts "Services are handled by katello-service in Satellite versions 6.2 and earlier. \n" \
-         "Flags --brief or --failing will be ignored if present. \n"\
-         "Similarly, services that are not listed by katello-service will get ignored. \n"\
-         "Redirecting to: \n#{command}\n"
-    puts execute(command)
-  end
-
   def exclude_services_only(options)
     existing_services - filtered_services(options)
-  end
-
-  def katello_service_filters(options)
-    filters = ''
-    if options[:exclude]&.any?
-      filters += "--exclude #{options[:exclude].join(',')}"
-    end
-    if options[:only]&.any?
-      filters += "--only #{options[:only].join(',')}"
-    end
-    filters
   end
 end

definitions/checks/candlepin/validate_db.rb
@@ -11,14 +11,7 @@ module Checks::Candlepin
 
     def run
       result, result_msg = feature(:candlepin_database).execute_cpdb_validate_cmd
-      next_steps = []
-      if feature(:satellite) && feature(:satellite).current_minor_version == '6.2'
-        next_steps.concat(
-          [Procedures::Candlepin::DeleteOrphanedRecordsFromEnvContent.new,
-           Procedures::KnowledgeBaseArticle.new(:doc => 'fix_cpdb_validate_failure')]
-        )
-      end
-      assert(result == 0, result_msg, :next_steps => next_steps)
+      assert(result == 0, result_msg)
     end
   end
 end

definitions/checks/check_for_newer_packages.rb
@@ -7,7 +7,7 @@ class Checks::CheckForNewerPackages < ForemanMaintain::Check
       'package names to validate',
       :required => true
     param :manual_confirmation_version,
-      'Version of satellite (6.9) to ask the user if they are on the latest minor release of.',
+      'Version of satellite to ask the user if they are on the latest minor release of.',
       :required => false
     manual_detection
   end

definitions/checks/foreman/check_https_proxies.rb
@@ -1,34 +1,0 @@
-module Checks
-  class CheckHttpsProxies < ForemanMaintain::Check
-    metadata do
-      label :https_proxies
-      for_feature :foreman_database
-      description 'Check for HTTPS proxies from the database'
-      manual_detection
-    end
-
-    def run
-      https_proxies = find_https_proxies
-      unless https_proxies.empty?
-        https_proxy_names = https_proxies.map { |proxy| proxy['name'] }
-        question = "Syncing repositories through an 'HTTP Proxy' that uses the HTTPS\n"\
-                    "protocol is not supported directly with Satellite 6.10.\n"\
-                    "The following proxies use HTTPS: #{https_proxy_names.join(', ')}.\n"\
-                    "For a suggested solution see https://access.redhat.com/solutions/6414991\n"\
-                    'Continue upgrade?'
-        answer = ask_decision(question, actions_msg: 'y(yes), q(quit)')
-        abort! if answer != :yes
-      end
-    end
-
-    def find_https_proxies
-      feature(:foreman_database).query(self.class.query_to_get_https_proxies)
-    end
-
-    def self.query_to_get_https_proxies
-      <<-SQL
-        SELECT \"http_proxies\".* FROM \"http_proxies\" WHERE (http_proxies.url ilike 'https://%')
-      SQL
-    end
-  end
-end

definitions/checks/pulpcore/group_ownership_check.rb
@@ -1,18 +1,0 @@
-module Checks
-  module Pulpcore
-    class GroupOwnershipCheck < ForemanMaintain::Check
-      metadata do
-        description 'Check the group owner of /var/lib/pulp/content directory'
-        label :group_ownership_check_of_pulp_content
-        manual_detection
-      end
-
-      def run
-        group_id = File.stat('/var/lib/pulp/content/').gid
-        if Etc.getgrgid(group_id).name != 'pulp'
-          fail! "Please run 'foreman-maintain prep-6.10-upgrade' prior to upgrading."
-        end
-      end
-    end
-  end
-end

definitions/checks/puppet/provide_upgrade_guide.rb
@@ -1,35 +1,0 @@
-module Checks::Puppet
-  class ProvideUpgradeGuide < ForemanMaintain::Check
-    metadata do
-      description 'Verify puppet and provide upgrade guide for it'
-      tags :puppet_upgrade_guide
-      confine do
-        feature(:instance).downstream &&
-          feature(:instance).downstream.current_minor_version == '6.3' &&
-          find_package('puppet')
-      end
-      manual_detection
-    end
-
-    def run
-      with_spinner('Verifying puppet version before upgrade') do |spinner|
-        puppet_package = find_package('puppet')
-        spinner.update "current puppet version: #{puppet_package}"
-        curr_sat_version = feature(:instance).downstream.current_minor_version
-        assert(
-          (puppet_package !~ /puppet-3/),
-          'Before continuing with upgrade, please make sure you finish puppet upgrade.',
-          :next_steps => [
-            Procedures::KnowledgeBaseArticle.new(:doc => doc_ids_by_sat_versions[curr_sat_version]),
-          ]
-        )
-      end
-    end
-
-    private
-
-    def doc_ids_by_sat_versions
-      { '6.3' => 'upgrade_puppet_guide_for_sat63' }
-    end
-  end
-end

definitions/checks/puppet/warn_about_puppet_removal.rb
@@ -1,35 +1,0 @@
-module Checks::Puppet
-  class WarnAboutPuppetRemoval < ForemanMaintain::Check
-    metadata do
-      description 'Warn about Puppet content removal prior to 6.10 upgrade'
-      label :warn_before_puppet_removal
-      confine do
-        feature(:instance).downstream &&
-          feature(:instance).downstream.current_minor_version == '6.9'
-      end
-    end
-
-    def run
-      message = "Puppet repositories found!\n"\
-                "Upgrading to 6.10 will delete the Puppet repositories\n"\
-                "if any of the following conditions are met,\n"\
-                "1. Any Puppet repositories in Library lifecycle environment.\n"\
-                "2. Unpublished content view's Puppet repositories.\n\n"\
-                "Note: Any Puppet content that is in use by hosts will not be deleted.\n"\
-                "After the upgrade, hosts will continue to use the Puppet modules,\n"\
-                'but future Puppet content management '\
-                "must be handled outside of the Satellite!\n\n"\
-                'Do you want to proceed?'
-      if puppet_repos_available?
-        answer = ask_decision(message, actions_msg: 'y(yes), n(no)')
-        exit 0 unless answer == :yes
-      end
-    end
-
-    def puppet_repos_available?
-      hammer_cmd = '--output json repository list --fields name --content-type puppet'
-      puppet_repos = feature(:hammer).run(hammer_cmd)
-      !JSON.parse(puppet_repos).empty?
-    end
-  end
-end

definitions/checks/remote_execution/verify_settings_file_already_exists.rb
@@ -1,42 +1,0 @@
-module Checks::RemoteExecution
-  class VerifySettingsFileAlreadyExists < ForemanMaintain::Check
-    metadata do
-      description 'Check to verify remote_execution_ssh settings already exist'
-
-      confine do
-        feature(:instance).downstream &&
-          feature(:instance).downstream.current_minor_version == '6.2' &&
-          find_package(proxy_plugin_name('dynflow_core')) &&
-          file_exists?('/etc/smart_proxy_dynflow_core')
-      end
-    end
-
-    def run
-      if file_exists?(settingd_dir_path)
-        symlinks = find_symlinks(settingd_dir_path)
-        assert(
-          symlinks.empty?,
-          failure_msg(settingd_dir_path, symlinks),
-          :next_steps => [
-            Procedures::RemoteExecution::RemoveExistingSettingsd.new(
-              :dirpath => settingd_dir_path
-            ),
-          ]
-        )
-      end
-    end
-
-    private
-
-    def failure_msg(dir_path, symlinks)
-      'Settings related to remote_execution_ssh are already present'\
-      " under #{dir_path} and " \
-      "\nit would conflict with the installer from the next version." \
-      "\nsymlinks available - #{symlinks.join(', ')}"
-    end
-
-    def settingd_dir_path
-      '/etc/smart_proxy_dynflow_core/settings.d'
-    end
-  end
-end

commit 79936dd27aeec55a9f4134f18ed0e1e8a61652b4

Drop unused exclude_service_only
Fixes: 5c2a705e18a9ec26d5461cdd7e1b211cd06d9971

definitions/features/service.rb
@@ -175,8 +175,4 @@ class Features::Service < ForemanMaintain::Feature
       action
     end
   end
-
-  def exclude_services_only(options)
-    existing_services - filtered_services(options)
-  end
 end