theforeman/foreman_maintain

commit 5c2a705e18a9ec26d5461cdd7e1b211cd06d9971

Drop old Satellite version conditionals

definitions/features/service.rb
@@ -8,11 +8,7 @@ class Features::Service < ForemanMaintain::Feature
     # { :only => ["httpd"] }
     # { :exclude => ["pulp-workers", "tomcat"] }
     # { :include => ["crond"] }
-    if feature(:instance).downstream&.less_than_version?('6.3')
-      use_katello_service(action, options)
-    else
-      use_system_service(action, options, spinner)
-    end
+    use_system_service(action, options, spinner)
   end
 
   def existing_services
@@ -180,44 +176,7 @@ class Features::Service < ForemanMaintain::Feature
     end
   end
 
-  def use_katello_service(action, options)
-    if %w[enable disable].include?(action)
-      raise 'Service enable and disable are only supported in Satellite 6.3+'
-    end
-
-    command = "katello-service #{action} "
-
-    # katello-service in 6.1 does not support --only
-    if feature(:instance).downstream.less_than_version?('6.2')
-      excluded_services = exclude_services_only(options)
-      command += "--exclude #{excluded_services.join(',')}" if excluded_services.any?
-    else
-      command += katello_service_filters(options)
-    end
-
-    run_katello_service(command)
-  end
-
-  def run_katello_service(command)
-    puts "Services are handled by katello-service in Satellite versions 6.2 and earlier. \n" \
-         "Flags --brief or --failing will be ignored if present. \n"\
-         "Similarly, services that are not listed by katello-service will get ignored. \n"\
-         "Redirecting to: \n#{command}\n"
-    puts execute(command)
-  end
-
   def exclude_services_only(options)
     existing_services - filtered_services(options)
-  end
-
-  def katello_service_filters(options)
-    filters = ''
-    if options[:exclude]&.any?
-      filters += "--exclude #{options[:exclude].join(',')}"
-    end
-    if options[:only]&.any?
-      filters += "--only #{options[:only].join(',')}"
-    end
-    filters
   end
 end

definitions/scenarios/restore.rb
@@ -16,7 +16,6 @@ module ForemanMaintain::Scenarios
       backup = ForemanMaintain::Utils::Backup.new(context.get(:backup_dir))
 
       add_steps(find_checks(:root_user))
-      supported_version_check
       add_steps_with_context(Checks::Restore::ValidateBackup,
         Checks::Restore::ValidateHostname,
         Checks::Restore::ValidateInterfaces,
@@ -80,14 +79,6 @@ module ForemanMaintain::Scenarios
       end
       if feature(:pulpcore) && backup.file_map[:pulpcore_dump][:present]
         add_steps_with_context(Procedures::Restore::PulpcoreDump)
-      end
-    end
-
-    def supported_version_check
-      if feature(:instance).downstream&.less_than_version?('6.3')
-        msg = 'ERROR: Restore subcommand is supported by Satellite 6.3+. ' \
-              'Please use katello-restore instead.'
-        abort(msg)
       end
     end
 

definitions/checks/candlepin/validate_db.rb
@@ -11,14 +11,7 @@ module Checks::Candlepin
 
     def run
       result, result_msg = feature(:candlepin_database).execute_cpdb_validate_cmd
-      next_steps = []
-      if feature(:satellite) && feature(:satellite).current_minor_version == '6.2'
-        next_steps.concat(
-          [Procedures::Candlepin::DeleteOrphanedRecordsFromEnvContent.new,
-           Procedures::KnowledgeBaseArticle.new(:doc => 'fix_cpdb_validate_failure')]
-        )
-      end
-      assert(result == 0, result_msg, :next_steps => next_steps)
+      assert(result == 0, result_msg)
     end
   end
 end

definitions/checks/check_for_newer_packages.rb
@@ -7,7 +7,7 @@ class Checks::CheckForNewerPackages < ForemanMaintain::Check
       'package names to validate',
       :required => true
     param :manual_confirmation_version,
-      'Version of satellite (6.9) to ask the user if they are on the latest minor release of.',
+      'Version of satellite to ask the user if they are on the latest minor release of.',
       :required => false
     manual_detection
   end

definitions/checks/foreman/check_https_proxies.rb
@@ -1,34 +1,0 @@
-module Checks
-  class CheckHttpsProxies < ForemanMaintain::Check
-    metadata do
-      label :https_proxies
-      for_feature :foreman_database
-      description 'Check for HTTPS proxies from the database'
-      manual_detection
-    end
-
-    def run
-      https_proxies = find_https_proxies
-      unless https_proxies.empty?
-        https_proxy_names = https_proxies.map { |proxy| proxy['name'] }
-        question = "Syncing repositories through an 'HTTP Proxy' that uses the HTTPS\n"\
-                    "protocol is not supported directly with Satellite 6.10.\n"\
-                    "The following proxies use HTTPS: #{https_proxy_names.join(', ')}.\n"\
-                    "For a suggested solution see https://access.redhat.com/solutions/6414991\n"\
-                    'Continue upgrade?'
-        answer = ask_decision(question, actions_msg: 'y(yes), q(quit)')
-        abort! if answer != :yes
-      end
-    end
-
-    def find_https_proxies
-      feature(:foreman_database).query(self.class.query_to_get_https_proxies)
-    end
-
-    def self.query_to_get_https_proxies
-      <<-SQL
-        SELECT \"http_proxies\".* FROM \"http_proxies\" WHERE (http_proxies.url ilike 'https://%')
-      SQL
-    end
-  end
-end

definitions/checks/pulpcore/group_ownership_check.rb
@@ -1,18 +1,0 @@
-module Checks
-  module Pulpcore
-    class GroupOwnershipCheck < ForemanMaintain::Check
-      metadata do
-        description 'Check the group owner of /var/lib/pulp/content directory'
-        label :group_ownership_check_of_pulp_content
-        manual_detection
-      end
-
-      def run
-        group_id = File.stat('/var/lib/pulp/content/').gid
-        if Etc.getgrgid(group_id).name != 'pulp'
-          fail! "Please run 'foreman-maintain prep-6.10-upgrade' prior to upgrading."
-        end
-      end
-    end
-  end
-end

definitions/checks/puppet/provide_upgrade_guide.rb
@@ -1,35 +1,0 @@
-module Checks::Puppet
-  class ProvideUpgradeGuide < ForemanMaintain::Check
-    metadata do
-      description 'Verify puppet and provide upgrade guide for it'
-      tags :puppet_upgrade_guide
-      confine do
-        feature(:instance).downstream &&
-          feature(:instance).downstream.current_minor_version == '6.3' &&
-          find_package('puppet')
-      end
-      manual_detection
-    end
-
-    def run
-      with_spinner('Verifying puppet version before upgrade') do |spinner|
-        puppet_package = find_package('puppet')
-        spinner.update "current puppet version: #{puppet_package}"
-        curr_sat_version = feature(:instance).downstream.current_minor_version
-        assert(
-          (puppet_package !~ /puppet-3/),
-          'Before continuing with upgrade, please make sure you finish puppet upgrade.',
-          :next_steps => [
-            Procedures::KnowledgeBaseArticle.new(:doc => doc_ids_by_sat_versions[curr_sat_version]),
-          ]
-        )
-      end
-    end
-
-    private
-
-    def doc_ids_by_sat_versions
-      { '6.3' => 'upgrade_puppet_guide_for_sat63' }
-    end
-  end
-end

definitions/checks/puppet/warn_about_puppet_removal.rb
@@ -1,35 +1,0 @@
-module Checks::Puppet
-  class WarnAboutPuppetRemoval < ForemanMaintain::Check
-    metadata do
-      description 'Warn about Puppet content removal prior to 6.10 upgrade'
-      label :warn_before_puppet_removal
-      confine do
-        feature(:instance).downstream &&
-          feature(:instance).downstream.current_minor_version == '6.9'
-      end
-    end
-
-    def run
-      message = "Puppet repositories found!\n"\
-                "Upgrading to 6.10 will delete the Puppet repositories\n"\
-                "if any of the following conditions are met,\n"\
-                "1. Any Puppet repositories in Library lifecycle environment.\n"\
-                "2. Unpublished content view's Puppet repositories.\n\n"\
-                "Note: Any Puppet content that is in use by hosts will not be deleted.\n"\
-                "After the upgrade, hosts will continue to use the Puppet modules,\n"\
-                'but future Puppet content management '\
-                "must be handled outside of the Satellite!\n\n"\
-                'Do you want to proceed?'
-      if puppet_repos_available?
-        answer = ask_decision(message, actions_msg: 'y(yes), n(no)')
-        exit 0 unless answer == :yes
-      end
-    end
-
-    def puppet_repos_available?
-      hammer_cmd = '--output json repository list --fields name --content-type puppet'
-      puppet_repos = feature(:hammer).run(hammer_cmd)
-      !JSON.parse(puppet_repos).empty?
-    end
-  end
-end

commit bbf77f89ce24bdb81e1fe5ff96250ec13cb67a38

add support for timer management (#1016)
* Drop unused exclude_service_only

Fixes: 5c2a705e18a9ec26d5461cdd7e1b211cd06d9971

* Use uniq(&:to_s) as we don't need Ruby 1.8.7 anymore

Fixes: ccbe9b2ca7c8f3c87d15d78b4802840252d82286

* Drop unused unit_file_available?

Fixes: 079baecc4417f6d218c93ab9df0217e8672ffaaf

* add timers feature

* define iop timers

* start/stop timers when needed

* only start/stop timers if there are any

* refactor common systemd stuff into own concern

definitions/features/iop.rb
@@ -59,6 +59,12 @@ class Features::Iop < ForemanMaintain::Feature
   end
   # rubocop:enable Metrics/MethodLength
 
+  def timers
+    [
+      system_service('iop-service-vuln-vmaas-sync.timer', 20),
+    ]
+  end
+
   def container_base
     if feature(:instance).downstream
       'registry.redhat.io/satellite'

definitions/features/service.rb
@@ -1,4 +1,7 @@
+require 'foreman_maintain/concerns/systemd'
+
 class Features::Service < ForemanMaintain::Feature
+  include ForemanMaintain::Concerns::Systemd
   metadata do
     label :service
   end
@@ -13,10 +16,7 @@ class Features::Service < ForemanMaintain::Feature
 
   def existing_services
     ForemanMaintain.available_features.flat_map(&:services).
-      sort.
-      inject([]) do |pool, service| # uniq(&:to_s) for ruby 1.8.7
-        (pool.last.nil? || !pool.last.matches?(service)) ? pool << service : pool
-      end.
+      sort.uniq(&:to_s).
       select(&:exist?)
   end
 
@@ -30,26 +30,11 @@ class Features::Service < ForemanMaintain::Feature
     Hash[services.sort_by { |k, _| k.to_i }.reverse]
   end
 
-  def action_noun(action)
-    action_word_modified(action) + 'ing'
-  end
-
-  def action_past_tense(action)
-    action_word_modified(action) + 'ed'
-  end
-
   def filter_disabled_services!(action, service_list)
     if %w[start stop restart status].include?(action)
       service_list.select! { |service| !service.respond_to?(:enabled?) || service.enabled? }
     end
     service_list
-  end
-
-  def unit_file_available?(name)
-    cmd = "systemctl --no-legend --no-pager list-unit-files --type=service #{name} |\
-           grep --word-regexp --quiet #{name}"
-    exit_status, = execute_with_status(cmd)
-    exit_status == 0
   end
 
   private
@@ -96,28 +81,6 @@ class Features::Service < ForemanMaintain::Feature
     services_and_statuses.map! { |service, status| [service, status.value] }
   end
 
-  def format_status(output, exit_code, options)
-    status = ''
-    if !options[:failing] || exit_code > 0
-      if options[:brief]
-        status += format_brief_status(exit_code)
-      elsif !(output.nil? || output.empty?)
-        status += "\n" + output
-      end
-    end
-    status
-  end
-
-  def format_brief_status(exit_code)
-    result = (exit_code == 0) ? reporter.status_label(:success) : reporter.status_label(:fail)
-    padding = reporter.max_length - reporter.last_line.to_s.length - 30
-    "#{' ' * padding} #{result}"
-  end
-
-  def allowed_action?(action)
-    %w[start stop restart status enable disable].include?(action)
-  end
-
   def extend_service_list_with_sockets(service_list, options)
     return service_list unless options[:include_sockets]
 
@@ -161,22 +124,5 @@ class Features::Service < ForemanMaintain::Feature
 
     service_list.concat(unregistered_service_list)
     service_list
-  end
-
-  def action_word_modified(action)
-    case action
-    when 'status'
-      'display'
-    when 'enable', 'disable'
-      action.chomp('e')
-    when 'stop'
-      action + 'p'
-    else
-      action
-    end
-  end
-
-  def exclude_services_only(options)
-    existing_services - filtered_services(options)
   end
 end

definitions/features/timer.rb
@@ -1,0 +1,96 @@
+require 'foreman_maintain/concerns/systemd'
+
+class Features::Timer < ForemanMaintain::Feature
+  include ForemanMaintain::Concerns::Systemd
+  metadata do
+    label :timer
+  end
+
+  def handle_timers(spinner, action, options = {})
+    # options is used to handle "exclude" and "only" i.e.
+    # { :only => ["httpd"] }
+    # { :exclude => ["pulp-workers", "tomcat"] }
+    use_system_timer(action, options, spinner)
+  end
+
+  def existing_timers
+    ForemanMaintain.available_features.flat_map(&:timers).
+      sort.uniq(&:to_s).
+      select(&:exist?)
+  end
+
+  def filtered_timers(options, action = '')
+    timers = filter_timers(existing_timers, options, action)
+
+    raise 'No timers found matching your parameters' unless timers.any?
+    return timers unless options[:reverse]
+
+    Hash[timers.sort_by { |k, _| k.to_i }.reverse]
+  end
+
+  def filter_disabled_timers!(action, timer_list)
+    if %w[start stop restart status].include?(action)
+      timer_list.select! { |timer| !timer.respond_to?(:enabled?) || timer.enabled? }
+    end
+    timer_list
+  end
+
+  private
+
+  def use_system_timer(action, options, spinner)
+    options[:reverse] = action == 'stop'
+    raise 'Unsupported action detected' unless allowed_action?(action)
+
+    status, failed_timers = run_action_on_timers(action, options, spinner)
+
+    spinner.update("All timers #{action_past_tense(action)}")
+    if action == 'status'
+      raise "Some timers are not running (#{failed_timers.join(', ')})" if status > 0
+
+      spinner.update('All timers are running')
+    end
+  end
+
+  def run_action_on_timers(action, options, spinner)
+    status = 0
+    failed_timers = []
+    filtered_timers(options, action).each_value do |group|
+      fork_threads_for_timers(action, group, spinner).each do |timer, status_and_output|
+        spinner.update("#{action_noun(action)} #{timer}") if action == 'status'
+        item_status, output = status_and_output
+        formatted = format_status(output, item_status, options)
+        puts formatted unless formatted.empty?
+
+        if item_status > 0
+          status = item_status
+          failed_timers << timer
+        end
+      end
+    end
+    [status, failed_timers]
+  end
+
+  def fork_threads_for_timers(action, timers, spinner)
+    timers_and_statuses = []
+    timers.each do |timer|
+      spinner.update("#{action_noun(action)} #{timer}") if action != 'status'
+      timers_and_statuses << [timer, Thread.new { timer.send(action.to_sym) }]
+    end
+    timers_and_statuses.map! { |timer, status| [timer, status.value] }
+  end
+
+  def filter_timers(timer_list, options, action)
+    if options[:only]&.any?
+      timer_list = timer_list.select do |timer|
+        options[:only].any? { |opt| timer.matches?(opt) }
+      end
+    end
+
+    if options[:exclude]&.any?
+      timer_list = timer_list.reject { |timer| options[:exclude].include?(timer.name) }
+    end
+
+    timer_list = filter_disabled_timers!(action, timer_list)
+    timer_list.group_by(&:priority).to_h
+  end
+end

definitions/procedures/timer/start.rb
@@ -1,0 +1,20 @@
+module Procedures::Timer
+  class Start < ForemanMaintain::Procedure
+    metadata do
+      description 'Start systemd timers'
+
+      for_feature :timer
+      confine do
+        feature(:timer)&.existing_timers&.any?
+      end
+
+      tags :post_migrations
+    end
+
+    def run
+      with_spinner('Starting systemd timers') do |spinner|
+        feature(:timer).handle_timers(spinner, 'start')
+      end
+    end
+  end
+end

definitions/procedures/timer/stop.rb
@@ -1,0 +1,20 @@
+module Procedures::Timer
+  class Stop < ForemanMaintain::Procedure
+    metadata do
+      description 'Stop systemd timers'
+
+      for_feature :timer
+      confine do
+        feature(:timer)&.existing_timers&.any?
+      end
+
+      tags :pre_migrations
+    end
+
+    def run
+      with_spinner('Stopping systemd timers') do |spinner|
+        feature(:timer).handle_timers(spinner, 'stop')
+      end
+    end
+  end
+end

definitions/scenarios/foreman_upgrade.rb
@@ -72,6 +72,7 @@ module Scenarios::Foreman
       add_steps(
         Procedures::MaintenanceMode::EnableMaintenanceMode,
         Procedures::Crond::Stop,
+        Procedures::Timer::Stop,
         Procedures::SyncPlans::Disable
       )
     end
@@ -113,6 +114,7 @@ module Scenarios::Foreman
         Procedures::RefreshFeatures,
         Procedures::Service::Start,
         Procedures::Crond::Start,
+        Procedures::Timer::Start,
         Procedures::SyncPlans::Enable,
         Procedures::MaintenanceMode::DisableMaintenanceMode
       )

definitions/scenarios/maintenance_mode.rb
@@ -10,6 +10,7 @@ module ForemanMaintain::Scenarios
     def compose
       add_step(Procedures::MaintenanceMode::EnableMaintenanceMode)
       add_step(Procedures::Crond::Stop)
+      add_step(Procedures::Timer::Stop)
       add_step(Procedures::SyncPlans::Disable)
     end
   end
@@ -25,6 +26,7 @@ module ForemanMaintain::Scenarios
     def compose
       add_step(Procedures::SyncPlans::Enable)
       add_step(Procedures::Crond::Start)
+      add_step(Procedures::Timer::Start)
       add_step(Procedures::MaintenanceMode::DisableMaintenanceMode)
     end
   end

definitions/scenarios/restore.rb
@@ -29,6 +29,7 @@ module ForemanMaintain::Scenarios
         Procedures::Restore::RequiredPackages,
         Procedures::Restore::Configs)
       add_step_with_context(Procedures::Crond::Stop)
+      add_step_with_context(Procedures::Timer::Stop)
       unless backup.incremental?
         add_steps_with_context(Procedures::Restore::InstallerReset)
       end
@@ -49,6 +50,7 @@ module ForemanMaintain::Scenarios
       add_step(Procedures::Installer::Run.new(:assumeyes => true))
       add_step_with_context(Procedures::Installer::UpgradeRakeTask)
       add_step_with_context(Procedures::Crond::Start)
+      add_step_with_context(Procedures::Timer::Start)
     end
     # rubocop:enable Metrics/MethodLength,Metrics/AbcSize
 
@@ -119,6 +121,7 @@ module ForemanMaintain::Scenarios
 
     def compose
       add_step_with_context(Procedures::Crond::Stop)
+      add_step_with_context(Procedures::Timer::Stop)
     end
   end
 end

definitions/scenarios/satellite_upgrade.rb
@@ -74,6 +74,7 @@ module Scenarios::Satellite
       add_steps(
         Procedures::MaintenanceMode::EnableMaintenanceMode,
         Procedures::Crond::Stop,
+        Procedures::Timer::Stop,
         Procedures::SyncPlans::Disable,
       )
     end
@@ -117,6 +118,7 @@ module Scenarios::Satellite
         Procedures::RefreshFeatures,
         Procedures::Service::Start,
         Procedures::Crond::Start,
+        Procedures::Timer::Start,
         Procedures::SyncPlans::Enable,
         Procedures::MaintenanceMode::DisableMaintenanceMode,
         Procedures::Iop::ImagePrune,

definitions/scenarios/update.rb
@@ -72,6 +72,7 @@ module Scenarios::Update
         Procedures::Iop::Update.new(:version => feature(:instance).current_major_version),
         Procedures::MaintenanceMode::EnableMaintenanceMode,
         Procedures::Crond::Stop,
+        Procedures::Timer::Stop,
         Procedures::SyncPlans::Disable
       )
     end
@@ -105,6 +106,7 @@ module Scenarios::Update
         Procedures::RefreshFeatures,
         Procedures::Service::Start,
         Procedures::Crond::Start,
+        Procedures::Timer::Start,
         Procedures::SyncPlans::Enable,
         Procedures::MaintenanceMode::DisableMaintenanceMode,
         Procedures::Iop::ImagePrune,

lib/foreman_maintain/concerns/systemd.rb
@@ -1,0 +1,50 @@
+module ForemanMaintain
+  module Concerns
+    module Systemd
+      def action_noun(action)
+        action_word_modified(action) + 'ing'
+      end
+
+      def action_past_tense(action)
+        action_word_modified(action) + 'ed'
+      end
+
+      private
+
+      def format_status(output, exit_code, options)
+        status = ''
+        if !options[:failing] || exit_code > 0
+          if options[:brief]
+            status += format_brief_status(exit_code)
+          elsif !(output.nil? || output.empty?)
+            status += "\n" + output
+          end
+        end
+        status
+      end
+
+      def format_brief_status(exit_code)
+        result = (exit_code == 0) ? reporter.status_label(:success) : reporter.status_label(:fail)
+        padding = reporter.max_length - reporter.last_line.to_s.length - 30
+        "#{' ' * padding} #{result}"
+      end
+
+      def allowed_action?(action)
+        %w[start stop restart status enable disable].include?(action)
+      end
+
+      def action_word_modified(action)
+        case action
+        when 'status'
+          'display'
+        when 'enable', 'disable'
+          action.chomp('e')
+        when 'stop'
+          action + 'p'
+        else
+          action
+        end
+      end
+    end
+  end
+end

lib/foreman_maintain/feature.rb
@@ -25,6 +25,15 @@ module ForemanMaintain
       []
     end
 
+    # Override method with list of applicable timers for feature.
+    # Services have a number for priority in order to ensure
+    # they are started and stopped in the correct order.
+    # example:
+    # [ system_service('foo', 10), system_service('bar', 20) ]
+    def timers
+      []
+    end
+
     # Override to generate additional feature instances that can't be
     # autodetected directly
     def additional_features

test/definitions/scenarios/capsule_upgrade_test.rb
@@ -50,6 +50,7 @@ describe "capsule upgrade scenarios" do
         scenario,
         Procedures::MaintenanceMode::EnableMaintenanceMode,
         Procedures::Crond::Stop,
+        Procedures::Timer::Stop,
       )
     end
   end
@@ -88,6 +89,7 @@ describe "capsule upgrade scenarios" do
         Procedures::RefreshFeatures,
         Procedures::Service::Start,
         Procedures::Crond::Start,
+        Procedures::Timer::Start,
         Procedures::MaintenanceMode::DisableMaintenanceMode,
       )
     end

test/definitions/scenarios/foreman_upgrade_test.rb
@@ -45,6 +45,7 @@ describe "foreman upgrade scenarios" do
         scenario,
         Procedures::MaintenanceMode::EnableMaintenanceMode,
         Procedures::Crond::Stop,
+        Procedures::Timer::Stop,
       )
     end
   end
@@ -82,6 +83,7 @@ describe "foreman upgrade scenarios" do
         Procedures::Service::Start,
         Procedures::MaintenanceMode::DisableMaintenanceMode,
         Procedures::Crond::Start,
+        Procedures::Timer::Start,
       )
     end
   end

test/definitions/scenarios/katello_upgrade_test.rb
@@ -59,6 +59,7 @@ describe "katello upgrade scenarios" do
         scenario,
         Procedures::MaintenanceMode::EnableMaintenanceMode,
         Procedures::Crond::Stop,
+        Procedures::Timer::Stop,
         Procedures::SyncPlans::Disable,
       )
     end
@@ -96,6 +97,7 @@ describe "katello upgrade scenarios" do
         Procedures::RefreshFeatures,
         Procedures::Service::Start,
         Procedures::Crond::Start,
+        Procedures::Timer::Start,
         Procedures::SyncPlans::Enable,
         Procedures::MaintenanceMode::DisableMaintenanceMode,
       )

test/definitions/scenarios/maintenance_mode_test.rb
@@ -13,6 +13,7 @@ describe "maintenance mode scenarios" do
         scenario,
         Procedures::MaintenanceMode::EnableMaintenanceMode,
         Procedures::Crond::Stop,
+        Procedures::Timer::Stop,
         Procedures::SyncPlans::Disable,
       )
     end
@@ -28,6 +29,7 @@ describe "maintenance mode scenarios" do
         scenario,
         Procedures::MaintenanceMode::DisableMaintenanceMode,
         Procedures::Crond::Start,
+        Procedures::Timer::Start,
         Procedures::SyncPlans::Enable,
       )
     end

test/definitions/scenarios/satellite_upgrade_test.rb
@@ -108,6 +108,7 @@ describe "satellite upgrade scenarios" do
         scenario,
         Procedures::MaintenanceMode::EnableMaintenanceMode,
         Procedures::Crond::Stop,
+        Procedures::Timer::Stop,
         Procedures::SyncPlans::Disable,
       )
     end
@@ -148,6 +149,7 @@ describe "satellite upgrade scenarios" do
         Procedures::RefreshFeatures,
         Procedures::Service::Start,
         Procedures::Crond::Start,
+        Procedures::Timer::Start,
         Procedures::SyncPlans::Enable,
         Procedures::MaintenanceMode::DisableMaintenanceMode,
       )

test/definitions/scenarios/update_test.rb
@@ -77,6 +77,7 @@ describe "update scenarios" do
           Procedures::Packages::Update,
           Procedures::MaintenanceMode::EnableMaintenanceMode,
           Procedures::Crond::Stop,
+          Procedures::Timer::Stop,
           Procedures::SyncPlans::Disable,
         )
       end
@@ -113,6 +114,7 @@ describe "update scenarios" do
           Procedures::RefreshFeatures,
           Procedures::Service::Start,
           Procedures::Crond::Start,
+          Procedures::Timer::Start,
           Procedures::SyncPlans::Enable,
           Procedures::MaintenanceMode::DisableMaintenanceMode,
         )
@@ -196,6 +198,7 @@ describe "update scenarios" do
           Procedures::Packages::Update,
           Procedures::MaintenanceMode::EnableMaintenanceMode,
           Procedures::Crond::Stop,
+          Procedures::Timer::Stop,
           Procedures::SyncPlans::Disable,
         )
       end
@@ -232,6 +235,7 @@ describe "update scenarios" do
           Procedures::RefreshFeatures,
           Procedures::Service::Start,
           Procedures::Crond::Start,
+          Procedures::Timer::Start,
           Procedures::SyncPlans::Enable,
           Procedures::MaintenanceMode::DisableMaintenanceMode,
         )
@@ -315,6 +319,7 @@ describe "update scenarios" do
           Procedures::Packages::Update,
           Procedures::MaintenanceMode::EnableMaintenanceMode,
           Procedures::Crond::Stop,
+          Procedures::Timer::Stop,
           Procedures::SyncPlans::Disable,
         )
       end
@@ -351,6 +356,7 @@ describe "update scenarios" do
           Procedures::RefreshFeatures,
           Procedures::Service::Start,
           Procedures::Crond::Start,
+          Procedures::Timer::Start,
           Procedures::SyncPlans::Enable,
           Procedures::MaintenanceMode::DisableMaintenanceMode,
         )