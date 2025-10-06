theforeman/foreman_maintain

commit 7cf8b888d20c4f1c2fed76aa10660294d3906dd9

Update IoP detection based on the iop::ensure Installer answer

definitions/features/iop.rb
@@ -3,8 +3,7 @@ class Features::Iop < ForemanMaintain::Feature
     label :iop
 
     confine do
-      File.exist?('/etc/containers/networks/iop-core-network.json') ||
-        File.exist?('/etc/containers/systemd/iop-core.network')
+      feature(:installer)&.answers&.dig('iop', 'ensure') == 'present'
     end
   end
 

definitions/reports/iop_remediations.rb
@@ -11,7 +11,7 @@ module Reports
 
     def iop_remediations_enabled
       if @iop_enabled.nil?
-        @iop_enabled = feature(:installer)&.answers&.fetch('iop', false) != false
+        @iop_enabled = feature(:installer)&.answers&.dig('iop', 'ensure') == 'present'
       end
       @iop_enabled
     end

definitions/features/iop_advisor_database.rb
@@ -5,8 +5,7 @@ class Features::IopAdvisorDatabase < ForemanMaintain::Feature
     label :iop_advisor_database
 
     confine do
-      File.exist?('/etc/containers/networks/iop-core-network.json') ||
-        File.exist?('/etc/containers/systemd/iop-core.network')
+      feature(:iop)
     end
   end
 

definitions/features/iop_inventory_database.rb
@@ -5,8 +5,7 @@ class Features::IopInventoryDatabase < ForemanMaintain::Feature
     label :iop_inventory_database
 
     confine do
-      File.exist?('/etc/containers/networks/iop-core-network.json') ||
-        File.exist?('/etc/containers/systemd/iop-core.network')
+      feature(:iop)
     end
   end
 

definitions/features/iop_remediations_database.rb
@@ -5,8 +5,7 @@ class Features::IopRemediationsDatabase < ForemanMaintain::Feature
     label :iop_remediations_database
 
     confine do
-      File.exist?('/etc/containers/networks/iop-core-network.json') ||
-        File.exist?('/etc/containers/systemd/iop-core.network')
+      feature(:iop)
     end
   end
 

definitions/features/iop_vmaas_database.rb
@@ -5,8 +5,7 @@ class Features::IopVmaasDatabase < ForemanMaintain::Feature
     label :iop_vmaas_database
 
     confine do
-      File.exist?('/etc/containers/networks/iop-core-network.json') ||
-        File.exist?('/etc/containers/systemd/iop-core.network')
+      feature(:iop)
     end
   end
 

commit d178771edc3d1065ef08e6b58bda61741e775e64

Correct IoP feature detection, installer answers can be false
Fixes: 7cf8b888d20c4f1c2fed76aa10660294d3906dd9

definitions/features/iop.rb
@@ -3,7 +3,8 @@ class Features::Iop < ForemanMaintain::Feature
     label :iop
 
     confine do
-      feature(:installer)&.answers&.dig('iop', 'ensure') == 'present'
+      feature(:installer)&.answers&.fetch('iop', nil).is_a?(Hash) and
+        feature(:installer)&.answers&.dig('iop', 'ensure') == 'present'
     end
   end
 

definitions/reports/iop_remediations.rb
@@ -11,7 +11,7 @@ module Reports
 
     def iop_remediations_enabled
       if @iop_enabled.nil?
-        @iop_enabled = feature(:installer)&.answers&.dig('iop', 'ensure') == 'present'
+        @iop_enabled = !!feature(:iop)
       end
       @iop_enabled
     end