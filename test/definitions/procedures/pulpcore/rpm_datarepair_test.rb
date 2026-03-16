6ec104d70f304beb96963f7ee50bdeb50749166f
Quinn James <qj@quinnjam.es>
2026-03-16 15:36:09 +0000

    Run rpm-datarepair 4007 on upgrade
    (cherry picked from commit 1806d4f2082e0265aa43766d3f12398d3fbf3b88)

--- test/definitions/procedures/pulpcore/rpm_datarepair_test.rb
+++ test/definitions/procedures/pulpcore/rpm_datarepair_test.rb
@@ -5,15 +5,31 @@ describe Procedures::Pulpcore::RpmDatarepair do
 
   subject { Procedures::Pulpcore::RpmDatarepair.new }
 
-  it 'runs pulpcore-manager rpm-datarepair 4073' do
+  it 'runs rpm-datarepair 4007 when handler exists' do
     assume_feature_present(:pulpcore)
     assume_feature_present(:pulpcore_database, :services => [])
     assume_feature_present(:service)
 
-    subject.expects(:execute!).with(
+    # Mock successful execution of 4007
+    subject.expects(:execute_with_status).with(
       'PULP_SETTINGS=/etc/pulp/settings.py runuser -u pulp -- ' \
-      'pulpcore-manager rpm-datarepair 4073'
-    ).once
+      'pulpcore-manager rpm-datarepair 4007'
+    ).returns([0, ''])
+
+    result = run_procedure(subject)
+    assert result.success?
+  end
+
+  it 'gracefully skips rpm-datarepair 4007 if handler does not exist' do
+    assume_feature_present(:pulpcore)
+    assume_feature_present(:pulpcore_database, :services => [])
+    assume_feature_present(:service)
+
+    # Mock 4007 handler not existing
+    subject.expects(:execute_with_status).with(
+      'PULP_SETTINGS=/etc/pulp/settings.py runuser -u pulp -- ' \
+      'pulpcore-manager rpm-datarepair 4007'
+    ).returns([2, "CommandError: Unknown issue: '4007'"])
 
     result = run_procedure(subject)
     assert result.success?
