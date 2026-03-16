b923da720df50d60b17ec4abc1c875d622c475ce
Quinn James <qj@quinnjam.es>
2026-03-16 15:36:09 +0000

    Run rpm-datarepair 4007 on upgrade
    (cherry picked from commit 1806d4f2082e0265aa43766d3f12398d3fbf3b88)

--- definitions/procedures/pulpcore/rpm_datarepair.rb
+++ definitions/procedures/pulpcore/rpm_datarepair.rb
@@ -3,14 +3,20 @@ module Procedures::Pulpcore
     include ForemanMaintain::Concerns::PulpCommon
 
     metadata do
-      description 'Rename ContentArtifact relative_paths to match `{N-V-R.A.rpm}`'
+      description 'Run Pulp RPM data repair commands'
       for_feature :pulpcore
     end
 
     def run
-      with_spinner('Running pulpcore-manager rpm-datarepair 4073') do
-        # Assumption: services are already started
-        execute!(pulpcore_manager('rpm-datarepair 4073'))
+      # Fix package_signing_fingerprint empty strings (SAT-42632 / PULP-1263)
+      # Do not fail if unavailable
+      with_spinner('Running pulpcore-manager rpm-datarepair 4007') do |spinner|
+        exit_status, output = execute_with_status(pulpcore_manager('rpm-datarepair 4007'))
+        if exit_status != 0 && output.include?("Unknown issue: '4007'")
+          spinner.update('Skipped pulpcore-manager rpm-datarepair 4007, not available')
+        elsif exit_status != 0
+          fail!(output)
+        end
       end
     end
   end
